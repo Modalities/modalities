c67c81fddb3b81c0ffbf4852c19fc6dfd7586cdf
Luzian Hahn <145655920+luzian-hahn@users.noreply.github.com>
2023-11-03 09:45:09 +0100

    test: test for index wout local ressources

--- tests/dataset_loader/test_large_file_lines_reader.py
+++ tests/dataset_loader/test_large_file_lines_reader.py
@@ -10,13 +10,17 @@ _ROOT_DIR = Path(__file__).parent.parent.parent
 
 
 def test_index_creation(tmpdir):
-    larger_test_data_path = Path("/home/shared/openwebtext/head20000_openwebtext2_en.jsonl")
-    indexer = IndexGenerator(larger_test_data_path)
+    dummy_data_path = Path(tmpdir, "dummy_data.txt")
+    # encode this as bytes. Using strings results into index shifting, if non-ascii chars are included.
+    dummy_text = "This is just\na dummy text\nwith newline chars\nand other\\n randøm\nchars...".encode("utf8")
+    dummy_data_path.write_bytes(dummy_text)
+    indexer = IndexGenerator(dummy_data_path)
     dummy_dst_path = Path(tmpdir, "index.pkl")
     indexer.run(dummy_dst_path)
 
     index = pickle.loads(dummy_dst_path.read_bytes())
-    assert index[:5] == [(0, 477), (478, 3797), (4276, 1730), (6007, 11180), (17188, 4886)]
+    assert len(index) == dummy_text.count(b"\n") + 1
+    assert dummy_text.split(b"\n") == [dummy_text[offset : offset + length] for offset, length in index]
 
 
 def test_large_file_lines_reader(tmpdir):
