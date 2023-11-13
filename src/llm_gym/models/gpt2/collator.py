52c10e3ad923d3d595aa181b041435eaf496667d
Mehdi Ali <33023925+mali-git@users.noreply.github.com>
2023-11-13 22:25:47 +0100

    refactor: remove HF collator

--- src/llm_gym/models/gpt2/collator.py
+++ src/llm_gym/models/gpt2/collator.py
@@ -12,37 +12,18 @@ from llm_gym.batch import DatasetBatch
 
 class GPT2LLMCollator:
 
-    def __init__(self, target_publication_key: str, tokenizer_file_path: str, pad_to_multiple_of: int = 8):
+    def __init__(self, sample_key: str, target_key: str):
         self.device: torch.device = field(default_factory=lambda: torch.device("cpu"))
-        self.target_publication_key = target_publication_key
-        tokenizer = GPT2TokenizerFast(tokenizer_file=tokenizer_file_path)  # "trained_wiki_tokenizer/tokenizer.json"
-        tokenizer.pad_token = tokenizer.eos_token
-        self.data_collator = DataCollatorForLanguageModeling(tokenizer=tokenizer,
-                                                             mlm=False,
-                                                             pad_to_multiple_of=pad_to_multiple_of)
+        self.sample_key = sample_key
+        self.target_key = target_key
 
-    def __call__(self, batch: List[torch.Tensor]) -> DatasetBatch:
+    def __call__(self, batch: List[Dict[str, torch.Tensor]]) -> DatasetBatch:
         """
         :param batch: batch format [no_samples, height, width, channels]
         :return:
         """
-        collated_batch = self.data_collator(batch)
-        samples = {"input_ids": collated_batch["input_ids"],
-                   "attention_mask": collated_batch["attention_mask"]}
-        targets = {self.target_publication_key: collated_batch["labels"],
-                   "attention_mask": collated_batch["attention_mask"]}
+        sample_tensor = torch.stack([torch.tensor(d[self.sample_key]) for d in batch])
+        samples = {self.sample_key: sample_tensor[:, :-1]}
+        targets = {self.target_key: sample_tensor[:, 1:]}
+
         return DatasetBatch(targets=targets, samples=samples)
-
-
-class LMWikiBookCorpusDatasetFactory:
-
-    @staticmethod
-    def construct(dataset_folder_path: str) -> Dict[str, Union[Dataset, DatasetDict]]:
-        dataset_dict = {}
-        if dataset_folder_path is None:
-            raise DatasetNotFoundError("Dataset path not specified")
-        splits = ["train", "test", "validation"]
-        for split_name in splits:
-            wiki_dataset = load_from_disk(os.path.join(dataset_folder_path, split_name))
-            dataset_dict[split_name] = wiki_dataset
-        return dataset_dict
