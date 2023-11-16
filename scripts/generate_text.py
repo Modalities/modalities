28e3e4ed34c42f5e19bc413d1cd69b7ebb8362d6
Viktor Haag <viktor.haag@iis.fraunhofer.de>
2023-11-16 11:49:56 +0100

    fix: :bug: fix generate text script

--- scripts/generate_text.py
+++ scripts/generate_text.py
@@ -1,11 +1,18 @@
 #!/usr/bin/env python3
 
 import argparse
+import os
+import sys
 from collections import OrderedDict
 from pathlib import Path
 
 import torch
-from transformers import GPT2Config, GPT2LMHeadModel, GPT2TokenizerFast, pipeline
+from torch.nn import functional as F
+from transformers import GPT2TokenizerFast
+
+from llm_gym.__main__ import load_app_config_dict
+from llm_gym.config.config import AppConfig
+from llm_gym.resolver_register import ResolverRegister
 
 chat_prefix = """
 This is a converstation between a user and a helpful bot, which answers the user's questsions as good as possible.
@@ -45,40 +52,67 @@ bot: There are five oceans - the Atlantic, Pacific, Indian, Arctic and Antarctic
 chat_prompt_template = """user: {prompt}
 bot: """
 
+
+def generate(
+    model: torch.nn.Module,
+    tokenizer: GPT2TokenizerFast,
+    context: str,
+    seq_len: int,
+    max_new_tokens: int,
+    temperature: float = 1.0,
+):
+    in_batch = tokenizer([context])
+    in_batch["input_ids"] = torch.Tensor(in_batch["input_ids"]).to(torch.int64)
+
+    for _ in range(max_new_tokens):
+        in_batch["input_ids"] = (
+            in_batch["input_ids"] if in_batch["input_ids"].size(1) <= seq_len else in_batch["input_ids"][:, -seq_len:]
+        )
+        logits = model.forward(in_batch)["logits"]
+        logits = logits[:, -1, :] / temperature
+        probs = F.softmax(logits, dim=-1)
+        idx_next = torch.multinomial(probs, num_samples=1)
+        in_batch["input_ids"] = torch.cat((in_batch["input_ids"], idx_next), dim=1)
+        print(tokenizer.decode(idx_next[0]), end="")
+        sys.stdout.flush()
+
+    return in_batch["input_ids"]
+
+
 if __name__ == "__main__":
+    os.environ["LOCAL_RANK"] = "1"
+    os.environ["RANK"] = "1"
+    os.environ["WORLD_SIZE"] = "1"
+
     parser = argparse.ArgumentParser()
-    parser.add_argument("--model-path", type=str, help="path to model.bin")
+    parser.add_argument("model_path", type=str, help="path to model.bin")
+    parser.add_argument("config_path", type=str, help="path to config.yaml")
     parser.add_argument("--chat", action="store_true", help="activate 'chat' mode")
-    parser.add_argument("--gpt", type=str, default="gpt2", help="set architecture")
     args = parser.parse_args()
 
-    gpt_version = args.gpt
-    config = GPT2Config.from_pretrained(gpt_version, output_hidden_stages=False)
-    tokenizer = GPT2TokenizerFast.from_pretrained(gpt_version)
-    if args.model_path is not None:
-        path = Path(args.model_path)
-        state_dict = torch.load(path)
-        s_d = OrderedDict({(k[6:], v) for k, v in state_dict.items()})
-        print(f"using {args.model_path}")
-        model = GPT2LMHeadModel(config)
-        model.load_state_dict(s_d)
-    else:
-        print(f"using pretrained {args.gpt} model from huggingface trained by OpenAI...")
-        model = GPT2LMHeadModel.from_pretrained(gpt_version, config=config)
-    generator = pipeline("text-generation", model=model, tokenizer=tokenizer)
+    tokenizer = GPT2TokenizerFast(tokenizer_file="./data/tokenizer/tokenizer.json")
+    path = Path(args.model_path)
+    state_dict = torch.load(path)
+    s_d = OrderedDict({(k[6:], v) for k, v in state_dict.items()})
+    print(f"using {args.model_path}")
+
+    config_dict = load_app_config_dict(args.config_path)
+    config = AppConfig.model_validate(config_dict)
+    resolvers = ResolverRegister(config=config)
+    model: torch.nn.Module = resolvers.build_component_by_config(config=config.model)
+    model.load_state_dict(state_dict)
+    model.eval()
 
     while True:
         try:
             print("-" * 50)
             if args.chat is True:
                 prompt = input("enter question> ")
-                ret = generator(
-                    chat_prompt_template.format(prompt=prompt).strip(), prefix=chat_prefix, max_new_tokens=100
-                )
+                ret = generate(model, tokenizer, prompt, config.data.sequence_len, 100)
             else:
                 prompt = input("enter prompt> ")
-                ret = generator(prompt.strip(), max_new_tokens=200)
-            print(ret[0]["generated_text"], "\n")
+                ret = generate(model, tokenizer, prompt, config.data.sequence_len, 100)
+            print("\n")
         except KeyboardInterrupt:
             print("closing app...")
             break
