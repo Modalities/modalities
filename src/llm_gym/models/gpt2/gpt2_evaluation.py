38f11d5017673506e848d243a58f2ba506a9fb4b
rrutmann <97447451+rrutmann@users.noreply.github.com>
2023-11-20 15:47:27 +0100

    feat(evaluation): Evaluation on lm-evaluation-harness now works with our custom models
    - Add default for prediction_publication_key
    - Convert dictionary config to GPTConfig
    - Use empty json representation for None config

--- src/llm_gym/models/gpt2/gpt2_evaluation.py
+++ src/llm_gym/models/gpt2/gpt2_evaluation.py
@@ -1,30 +1,36 @@
 
 import json
 from transformers import PretrainedConfig
-from src.llm_gym.models.gpt2.gpt2_model import GPTConfig, GPT2LLM
+from llm_gym.models.gpt2.gpt2_model import GPTConfig, GPT2LLM
 from transformers import PreTrainedModel
 from typing import Dict
 import torch
 
 
 class PretrainedGPTConfig(PretrainedConfig):
-    model_type = "gpt2"
+    model_type = "llm_gym_gpt2"
 
     def __init__(
             self,
             config: GPTConfig = None,
             **kwargs
     ):
+        if type(config) == dict:
+            config = GPTConfig(**config)
         self.config = config
+
         super().__init__(**kwargs)
 
     def to_json_string(self, use_diff: bool = True) -> str:
-        json_dict = self.config.__dict__.copy()
-        json_dict["attention"] = {
-            "attention_type": self.config.attention.attention_type.value,
-            "scaling_factor": self.config.attention.scaling_factor
-        }
-        json_dict["model_type"] = self.model_type
+        if self.config:
+            json_dict = {"config": self.config.__dict__.copy()}
+            json_dict["config"]["attention"] = {
+                "attention_type": self.config.attention.attention_type.value,
+                "scaling_factor": self.config.attention.scaling_factor
+            }
+            json_dict["model_type"] = self.model_type
+        else:
+            json_dict = {}
         return json.dumps(json_dict)
 
 
@@ -33,7 +39,7 @@ class PretrainedGPTModel(PreTrainedModel):
 
     def __init__(self,
                  config: PretrainedGPTConfig,
-                 prediction_publication_key: str,  # todo get this from AppConfig.model...
+                 prediction_publication_key: str = "logits",
                  ):
         super().__init__(config)
         self.prediction_publication_key = prediction_publication_key
