3b773e26982993f224c4110911f32a79434e874f
Luzian Hahn <145655920+luzian-hahn@users.noreply.github.com>
2023-11-28 17:43:43 +0100

    test: introduce dummy_config obj fixture instead of path

--- tests/conftest.py
+++ tests/conftest.py
@@ -2,20 +2,27 @@ from pathlib import Path
 
 import pytest
 
+from llm_gym.__main__ import load_app_config_dict
+from llm_gym.config.config import AppConfig
+
 _ROOT_DIR = Path(__file__).parents[1]
 
 
 @pytest.fixture
-def dummy_config_path() -> Path:
-    return _ROOT_DIR / Path("config_files/config_lorem_ipsum.yaml")
+def dummy_config(monkeypatch) -> AppConfig:
+    monkeypatch.setenv("RANK", "0")
+    monkeypatch.setenv("LOCAL_RANK", "0")
+    monkeypatch.setenv("WORLD_SIZE", "1")
+    dummy_config_path = _ROOT_DIR / Path("config_files/config_lorem_ipsum.yaml")
+    config_dict = load_app_config_dict(dummy_config_path)
+    return AppConfig.model_validate(config_dict)
 
 
 @pytest.fixture
 def dummy_data_path(tmpdir) -> Path:
-    source_dummy_data_path = _ROOT_DIR / Path("./data/lorem_ipsum.jsonl")
-    dummy_data_path = Path(tmpdir, source_dummy_data_path.name)
-    dummy_data_path.write_text(source_dummy_data_path.read_text())
+    source_raw_dummy_data_path = _ROOT_DIR / Path("./data/lorem_ipsum.jsonl")
+    dummy_data_path = Path(tmpdir, source_raw_dummy_data_path.name)
+    dummy_data_path.write_text(source_raw_dummy_data_path.read_text())
     index_path = Path(tmpdir, f"{dummy_data_path.stem}.idx.pkl")
     index_path.unlink(missing_ok=True)
-
     return dummy_data_path
--- tests/test_main.py
+++ tests/test_main.py
@@ -1,23 +1,16 @@
 from unittest.mock import MagicMock
 
-from llm_gym.__main__ import Main, load_app_config_dict
+from llm_gym.__main__ import Main
 from llm_gym.checkpointing.checkpointing import CheckpointingIF
-from llm_gym.config.config import AppConfig
 
 
-def test_e2e_training_run_wout_ckpt(monkeypatch, dummy_data_path, dummy_config_path):
+def test_e2e_training_run_wout_ckpt(monkeypatch, dummy_data_path, dummy_config):
     # patch in env variables
-    monkeypatch.setenv("RANK", "0")
-    monkeypatch.setenv("LOCAL_RANK", "0")
-    monkeypatch.setenv("WORLD_SIZE", "1")
     monkeypatch.setenv("MASTER_ADDR", "localhost")
     monkeypatch.setenv("MASTER_PORT", "9948")
 
-    # load and run test config
-    config_dict = load_app_config_dict(dummy_config_path)
-    config = AppConfig.model_validate(config_dict)
-    config.training.train_dataloader.config.dataset.config.raw_data_path = dummy_data_path
-    main = Main(config)
+    dummy_config.training.train_dataloader.config.dataset.config.raw_data_path = dummy_data_path
+    main = Main(dummy_config)
     mocked_checkpointing = MagicMock(spec=CheckpointingIF)
     main.gym.checkpointing = mocked_checkpointing
     main.run()
