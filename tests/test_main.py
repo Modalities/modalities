bed22301d4208a5bc96d9a56224452d760e9a00d
Luzian Hahn <145655920+luzian-hahn@users.noreply.github.com>
2023-12-14 17:03:56 +0100

    test: run e2e test only when a GPU is available
    background is that the internally used FSPRunner requires GPU-Support

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,9 +1,19 @@
 from unittest.mock import MagicMock
 
+import pytest
+import torch.cuda
+
 from llm_gym.__main__ import Main
 from llm_gym.checkpointing.checkpointing import CheckpointingIF
 
 
+def no_gpu_available() -> bool:
+    return not torch.cuda.is_available()
+
+
+@pytest.mark.skipif(
+    no_gpu_available(), reason="This e2e test verifies a GPU-Setup and uses components, which do not support CPU-only."
+)
 def test_e2e_training_run_wout_ckpt(monkeypatch, indexed_dummy_data_path, dummy_config):
     # patch in env variables
     monkeypatch.setenv("MASTER_ADDR", "localhost")
