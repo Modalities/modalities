27be1ec33cca18e0ab66ab1e54745870d7505ec0
Max Luebbering <2804731+le1nux@users.noreply.github.com>
2023-12-18 10:52:42 +0100

    fix: fixed test test_checkpoint_strategy_k

--- tests/checkpointing/test_checkpoint_strategies.py
+++ tests/checkpointing/test_checkpoint_strategies.py
@@ -7,25 +7,27 @@ from src.llm_gym.checkpointing.checkpointing_strategies import SaveKMostRecentCh
 
 
 @pytest.mark.parametrize(
-    "k, saved_batch_id_checkpoints, global_train_batch_id, checkpoints_to_delete",
+    "k, saved_batch_id_checkpoints, checkpoints_to_delete, save_current",
     [
-        (2, [1, 2], 100, [2]),
-        # Test case 1: k value is 2. New checkpoint is created and the last one (in the example: [2]) is deleted.
-        (0, [1, 2], 100, []),  # Test case 2: k value is 0. No deletion of checkpoints.
-        (2, [1], 100, []),
-        # Test case 1: k value is 2, but there are currently only one checkpoint. Hence, no deletion.
-    ])
+        # k value is 2. New checkpoint is created and the last one (in the example: [2]) is deleted.
+        (2, [1, 2], [2], True),
+        # k value is 0. No deletion of checkpoints.
+        (0, [], [], False),
+        # k value is 2, but there are currently only one checkpoint. Hence, no deletion.
+        (2, [1], [], True),
+        # k value is -1, therefore we want to keep all checkpoints without any deletion
+        (-1, [3, 2, 1], [], True),
+    ],
+)
 def test_checkpoint_strategy_k(
-        k: int,
-        saved_batch_id_checkpoints: List[int],
-        global_train_batch_id: int,
-        checkpoints_to_delete: List[int]
+    k: int, saved_batch_id_checkpoints: List[int], checkpoints_to_delete: List[int], save_current: bool
 ) -> None:
+    global_train_batch_id = 100
     checkpoint_strategy = SaveKMostRecentCheckpointsStrategy(k=k)
     checkpoint_strategy.saved_batch_id_checkpoints = saved_batch_id_checkpoints
     checkpoint_instruction = checkpoint_strategy.get_checkpoint_instruction(
-        global_train_batch_id=global_train_batch_id,
-        num_batches=5
+        global_train_batch_id=global_train_batch_id, num_batches=5
     )
 
     assert checkpoint_instruction.checkpoints_to_delete == checkpoints_to_delete
+    assert checkpoint_instruction.save_current == save_current
