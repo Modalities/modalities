eb0766ec3329a57b27b1f279907d78ff3fee0e00
Max Luebbering <2804731+le1nux@users.noreply.github.com>
2023-12-18 10:08:29 +0100

    test: reformatted code and refactored test_delete_checkpoint

--- tests/checkpointing/test_checkpoint_execution_functions.py
+++ tests/checkpointing/test_checkpoint_execution_functions.py
@@ -1,4 +1,3 @@
-from typing import List
 import torch.nn as nn
 from torch.distributed.fsdp import (
     FullyShardedDataParallel as FSDP,
@@ -8,22 +7,17 @@ from torch.distributed.fsdp import (
 )
 from pathlib import Path
 
-from src.llm_gym.checkpointing.checkpointing_execution import FSDPToDiscCheckpointing, CheckpointingEntityType
+from src.llm_gym.checkpointing.checkpointing_execution import FSDPToDiscCheckpointing
 import pytest
 
 
 @pytest.mark.skip
-def dummy_method(
-        module: nn.Module,
-        flag: bool
-) -> FSDP:
+def dummy_method(module: nn.Module, flag: bool) -> FSDP:
     raise NotImplementedError
 
 
 @pytest.mark.skip
-def is_empty_directory(
-        folder_path: str
-) -> bool:
+def is_empty_directory(folder_path: str) -> bool:
     path = Path(folder_path)
     return not any(path.iterdir())
 
@@ -31,36 +25,36 @@ def is_empty_directory(
 CONTENT = "model"
 
 
-def test_get_paths_to_delete(
-        tmp_path  # pytest temp path
-):
+def test_get_paths_to_delete(tmp_path):  # pytest temp path
     d = tmp_path / "folder"
     d.mkdir()
-    p = d / '<experiment_id>-200-101.bin'
+    p = d / "<experiment_id>-200-101.bin"
     p.write_text(CONTENT)
 
     checkpointing = FSDPToDiscCheckpointing(
-        checkpoint_path=d,
-        checkpointing_rank=0,
-        experiment_id=str(1),
-        global_rank=0,
-        model_wrapping_fn=dummy_method
+        checkpoint_path=d, checkpointing_rank=0, experiment_id=str(1), global_rank=0, model_wrapping_fn=dummy_method
     )
     files_paths_to_delete = checkpointing._get_paths_to_delete(batch_id=100)
     assert len(files_paths_to_delete) != 0
 
 
-def test_delete_checkpoint(tmp_path):
-    d = tmp_path / "folder"
-    d.mkdir()
-    p = d / '<experiment_id>-200-101.bin'
-    p.write_text(CONTENT)
-    p = d / '<experiment_id>-model-101.bin'
-    p.write_text(CONTENT)
+def test_delete_checkpoint(tmpdir):
+    experiment_id = "2023-11-17-01:35:09"
+    directory = Path(tmpdir)
+
+    optimizer_path = directory / f"{experiment_id}-optimizer-101.bin"
+    optimizer_path.write_text(CONTENT)
+
+    model_path = directory / f"{experiment_id}-model-101.bin"
+    model_path.write_text(CONTENT)
+
     # "<experiment_id>-<enitity>-<step>.bin"
-    flagger = is_empty_directory(d.__str__())
-    checkpointing = FSDPToDiscCheckpointing(checkpoint_path=d, checkpointing_rank=0, experiment_id=str(1),
-                                            global_rank=0,
-                                            model_wrapping_fn=dummy_method)
+    checkpointing = FSDPToDiscCheckpointing(
+        checkpoint_path=directory,
+        checkpointing_rank=0,
+        experiment_id=experiment_id,
+        global_rank=0,
+        model_wrapping_fn=dummy_method,
+    )
     checkpointing._delete_checkpoint(batch_id=100)
-    assert is_empty_directory(d.__str__()) == True
+    assert is_empty_directory(directory.__str__())
