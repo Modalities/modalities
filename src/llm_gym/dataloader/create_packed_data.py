82817d26a92de8ac38ab2cc7766478926a8e77aa
Luzian Hahn <145655920+luzian-hahn@users.noreply.github.com>
2023-12-22 12:19:48 +0100

    refactor: remove feature to configure byte sizes in packed Datasets
    decided against this, since the default value of 8 bytes to define the amount of encoded tokens is already larger than the known parts of the public web

--- src/llm_gym/dataloader/create_packed_data.py
+++ src/llm_gym/dataloader/create_packed_data.py
@@ -12,6 +12,14 @@ from llm_gym.dataloader.large_file_lines_reader import LargeFileLinesReader
 
 
 class PackedDataGenerator:
+    # amount of bytes to represent tokens as integers.
+    # If the vocabulary exceeds 2^(8*`size_in_bytes`), this requires adaptation.
+    TOKEN_SIZE_IN_BYTES = 4
+    # amount of bytes to represent number of all tokens in dataset.
+    # If the amount exceeds 2^(8*`header_size_in_bytes`), this requires adaptation.
+    # Decided to keep this constant, since a size of 8 bytes requires more data than the internet currently provides
+    HEAD_SIZE_IN_BYTES = 8
+
     def __init__(
         self,
         src_path: Path,
@@ -19,8 +27,6 @@ class PackedDataGenerator:
         index_path: Path = None,
         jq_pattern: str = ".text",
         max_number_of_tokens: int = None,
-        size_in_bytes: int = 4,
-        header_size_in_bytes: int = 8,
     ):
         """
         Reads in a jsonl file and the corresponding index file and packs dataset file for LLM training.
@@ -34,21 +40,15 @@ class PackedDataGenerator:
         :param jq_pattern: jq-pattern applied on every jsonl-entry. Results are afterwards tokenized and packed
         :param max_number_of_tokens: Limit the total amount of tokens in the packed dataset.
                                      If not specified, the whole data is packed into the dataset.
-        :param size_in_bytes: amount of bytes to represent tokens as integers.
-                              If the vocabulary exceeds 2^(8*`size_in_bytes`), this requires adaptation.
-        :param header_size_in_bytes: amount of bytes to represent number of all tokens in dataset.
-                                     If the amount exceeds 2^(8*`header_size_in_bytes`), this requires adaptation.
         """
         self.src_path = src_path
         self.tokenizer = tokenizer
         self.jq_filter = jq.compile(jq_pattern)
         self.max_tokens = max_number_of_tokens
-        self.size_in_bytes = size_in_bytes
-        self.header_size_in_bytes = header_size_in_bytes
 
         self._reader = LargeFileLinesReader(src_path, index_path=index_path)
         self._total_num_of_tokens = 0
-        self._curr_offset = self.header_size_in_bytes
+        self._curr_offset = self.HEAD_SIZE_IN_BYTES
         self._index_list = []
 
     def _default_destination_path(self, destination_path: Path = None) -> Path:
@@ -69,11 +69,11 @@ class PackedDataGenerator:
             raise ValueError(f"file already exists at destination path '{dst_path}'.")
 
         encoded_eos_token = self.tokenizer(self.tokenizer.eos_token)["input_ids"][0]
-        encoded_eos_token_as_bytes = encoded_eos_token.to_bytes(self.size_in_bytes, byteorder="big")
+        encoded_eos_token_as_bytes = encoded_eos_token.to_bytes(self.TOKEN_SIZE_IN_BYTES, byteorder="big")
         with dst_path.open("wb") as f:
             # allocate first self.header_size_in_bytes bytes for header (encodes length of data section)
             # not possible to prepend header after determining size of data section
-            f.write((0).to_bytes(self.header_size_in_bytes, byteorder="big"))
+            f.write((0).to_bytes(self.HEAD_SIZE_IN_BYTES, byteorder="big"))
 
             # write data section (tokens)
             for idx, line in tqdm(enumerate(self._reader)):
@@ -93,11 +93,11 @@ class PackedDataGenerator:
 
     def _update_data_length_in_pre_allocated_header(self, dst_path: Path):
         start_of_index_in_bytes = self._index_list[-1][0] + self._index_list[-1][1]
-        length_of_byte_encoded_data_section = start_of_index_in_bytes - self.header_size_in_bytes
-        header_content = length_of_byte_encoded_data_section.to_bytes(self.header_size_in_bytes, byteorder="big")
+        length_of_byte_encoded_data_section = start_of_index_in_bytes - self.HEAD_SIZE_IN_BYTES
+        header_content = length_of_byte_encoded_data_section.to_bytes(self.HEAD_SIZE_IN_BYTES, byteorder="big")
         header_content = np.frombuffer(header_content, dtype="uint8")
         # write the header content to the packed dataset file
-        m = np.memmap(dst_path, mode="r+", offset=0, shape=(self.header_size_in_bytes,))
+        m = np.memmap(dst_path, mode="r+", offset=0, shape=(self.HEAD_SIZE_IN_BYTES,))
         m[:] = header_content[:]
 
     def _process_line(self, eos_token_as_bytes: bytes, f: IO, line: str):
@@ -107,15 +107,15 @@ class PackedDataGenerator:
             raise ValueError("Received empty sample...")
         token_idx = 0
         for token in tokens:
-            token_as_bytes = token.to_bytes(self.size_in_bytes, byteorder="big")
+            token_as_bytes = token.to_bytes(self.TOKEN_SIZE_IN_BYTES, byteorder="big")
             f.write(token_as_bytes)
             self._total_num_of_tokens += 1
             if self._total_num_of_tokens == self.max_tokens:
-                segment_length = (token_idx + 1) * self.size_in_bytes
+                segment_length = (token_idx + 1) * self.TOKEN_SIZE_IN_BYTES
                 self._index_list.append((self._curr_offset, segment_length))
                 raise StopIteration
             token_idx += 1
         f.write(eos_token_as_bytes)
-        segment_length = (token_idx + 1) * self.size_in_bytes
+        segment_length = (token_idx + 1) * self.TOKEN_SIZE_IN_BYTES
         self._index_list.append((self._curr_offset, segment_length))
         self._curr_offset += segment_length
