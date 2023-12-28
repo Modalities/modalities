75238cbbc44fb019409bbf82d16837c01a0c1c6e
Max Luebbering <2804731+le1nux@users.noreply.github.com>
2023-12-28 14:47:46 +0100

    test: added improved testing to ResumableSampler

--- tests/dataloader/test_samplers.py
+++ tests/dataloader/test_samplers.py
@@ -6,9 +6,12 @@ from llm_gym.dataloader.samplers import ResumableSampler
 
 def test_resumable_sampler(resumable_sampler: ResumableSampler):
     existing_sampler: SequentialSampler = resumable_sampler.existing_sampler
-    values_1 = [i for i in resumable_sampler]
-    values_2 = existing_sampler.data_source[::-1][resumable_sampler.start_index :]
-    assert values_1 == values_2
+    indices_1 = [i for i in resumable_sampler]
+    indices_2 = [i for i in existing_sampler][resumable_sampler.start_index :]
+
+    data_source = existing_sampler.data_source[resumable_sampler.start_index :]
+    assert indices_1 == indices_2
+    assert indices_1 != data_source
 
 
 def test_resumable_batch_sampler(resumable_batch_sampler: ResumableSampler):
