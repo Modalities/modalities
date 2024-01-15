b49396d0ce0998359fe8cccaea843bfc3d6eb64f
Luzian Hahn <luzian.hahn@iis.fraunhofer.de>
2024-01-15 12:41:53 +0100

    fix: deactivate WandB per default
    It breaks the tests and is not configurable for ppl without WandB-Accounts

--- src/modalities/__main__.py
+++ src/modalities/__main__.py
@@ -31,7 +31,7 @@ from modalities.logging_broker.subscriber_impl.batch_progress_subscriber import 
     DummyProgressSubscriber,
     RichProgressSubscriber,
 )
-from modalities.logging_broker.subscriber_impl.results_subscriber import WandBEvaluationResultSubscriber
+from modalities.logging_broker.subscriber_impl.results_subscriber import RichResultSubscriber
 from modalities.loss_functions import Loss
 from modalities.models.gpt2.huggingface_model import HuggingFaceModel
 from modalities.resolver_register import ResolverRegister
@@ -353,14 +353,7 @@ class Main:
                 train_split_num_samples=train_split_lengths,
                 eval_splits_num_samples=eval_split_lengths,
             )
-            evaluation_result_subscriber = WandBEvaluationResultSubscriber(
-                num_ranks=config.training.world_size,
-                project=config.wandb.project_name,
-                experiment_id=self.experiment_id,
-                mode=config.wandb.mode,
-                dir=config.wandb.dir,
-                experiment_config=config,
-            )
+            evaluation_result_subscriber = RichResultSubscriber(num_ranks=config.training.world_size)
             message_broker.add_subscriber(
                 subscription=MessageTypes.EVALUATION_RESULT, subscriber=evaluation_result_subscriber
             )
