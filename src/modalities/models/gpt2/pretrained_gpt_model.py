abd6cb79f1b038ca5e65bd22bc0583c7e42a7e31
rrutmann <97447451+rrutmann@users.noreply.github.com>
2024-01-25 13:06:01 +0100

    feat: Pass model to load to PretrainedGPTModel

--- src/modalities/models/gpt2/pretrained_gpt_model.py
+++ src/modalities/models/gpt2/pretrained_gpt_model.py
@@ -5,21 +5,21 @@ from transformers import PreTrainedModel
 
 from modalities.config.config import PretrainedGPTConfig
 from modalities.models.gpt2.gpt2_model import GPT2LLM
+import torch.nn as nn
 
 
 class PretrainedGPTModel(PreTrainedModel):
     config_class = PretrainedGPTConfig
 
-    def __init__(self, config: PretrainedGPTConfig):
+    def __init__(self, config: PretrainedGPTConfig, model: nn.Module = None):
         super().__init__(config)
         # TODO offloading the parameters like this is ugly
-        self.model: GPT2LLM = GPT2LLM(**dict(config.config))
+        if model is None:
+            self.model: GPT2LLM = GPT2LLM(**dict(config.config))
+        else:
+            self.model = model
 
     def forward(self, tensor):
         model_input = {"input_ids": tensor}
         model_forward_output: Dict[str, torch.Tensor] = self.model.forward(model_input)
         return model_forward_output[self.config.config.prediction_key]
-
-
-if __name__ == "__main__":
-    ...
