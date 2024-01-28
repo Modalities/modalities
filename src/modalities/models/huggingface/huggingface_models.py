4d6a5ffb18ab590e6b6bf088ca0bf36408823c7a
Max Luebbering <le1nux@users.noreply.github.com>
2024-01-28 22:01:17 +0100

    feat: fully implemented HuggingFacePretrainedModel with respective configuration

--- src/modalities/models/huggingface/huggingface_models.py
+++ src/modalities/models/huggingface/huggingface_models.py
@@ -1,14 +1,13 @@
+from enum import Enum
 from pathlib import Path
 from typing import Any, Dict, List, Optional
 
 import torch
-import torch.nn as nn
-from accelerate.utils.dataclasses import FullyShardedDataParallelPlugin
-from pydantic import BaseModel
-from transformers import AutoModelForCausalLM, AutoModelForMaskedLM
+from pydantic import BaseModel, validator
+from transformers import AutoModelForCausalLM, AutoModelForMaskedLM, AutoTokenizer
 
-from modalities.config.lookup_types import LookupEnum
 from modalities.models.model import NNModel
+from modalities.util import parse_enum_by_name
 
 # Huggingface Model dependencies
 #
@@ -23,23 +22,31 @@ from modalities.models.model import NNModel
 #           <- LlamaForSequenceClassification    The LLaMa transformer with a sequence classif. head on top (lin. layer)
 
 
-class HuggingFaceModelTypes(LookupEnum):
+class HuggingFaceModelTypes(Enum):
     AutoModelForCausalLM = AutoModelForCausalLM
     AutoModelForMaskedLM = AutoModelForMaskedLM
 
 
-class HuggingFaceModelConfig(BaseModel):
+class HuggingFacePretrainedModelConfig(BaseModel):
     model_type: HuggingFaceModelTypes
-    pretrained_model_name_or_path: Path
+    model_name: Path
+    prediction_key: str
+    sample_key: str
     model_args: Optional[Any] = None
     kwargs: Optional[Any] = None
+
+    @validator("model_type", pre=True, always=True)
+    def parse_sharding_strategy_by_name(cls, name):
+        return parse_enum_by_name(name=name, enum_type=HuggingFaceModelTypes)
 
 
 class HuggingFacePretrainedModel(NNModel):
     def __init__(
         self,
         model_type: HuggingFaceModelTypes,
-        pretrained_model_name_or_path: Path,
+        model_name: str,
+        prediction_key: str,
+        sample_key: str,
         model_args: Optional[Any] = None,
         kwargs: Optional[Any] = None,
     ):
@@ -48,30 +55,27 @@ class HuggingFacePretrainedModel(NNModel):
             model_args = []
         if kwargs is None:
             kwargs = {}
+        self.prediction_key = prediction_key
+        self.sample_key = sample_key
 
         # TODO this would be perfect for a factory design, however the resovler register currently does not
         # support functions instead of classes within enums.
-        self.huggingface_model = model_type.value.from_pretrained(pretrained_model_name_or_path, *model_args, **kwargs)
+        self.huggingface_model = model_type.value.from_pretrained(model_name, *model_args, **kwargs)
 
     def forward(self, inputs: Dict[str, torch.Tensor]) -> Dict[str, torch.Tensor]:
-        return self.huggingface_model.forward(**inputs)
+        output = self.huggingface_model.forward(inputs[self.sample_key])
+        return {self.prediction_key: output.logits}
 
     @property
     def fsdp_block_names(self) -> List[str]:
         return self.huggingface_model._no_split_modules
 
 
-def get_fsdp_block_from_huggingface_model(model: HuggingFacePretrainedModel) -> List[nn.Module]:
-    fsdp_block_types = []
-    for cls_block_name in model.fsdp_block_names:
-        fsdp_block_types.append(FullyShardedDataParallelPlugin.get_module_class_from_name(model, cls_block_name))
-    return fsdp_block_types
-
-
 if __name__ == "__main__":
-    # tokenizer = AutoTokenizer.from_pretrained("epfl-llm/meditron-7b")
+    tokenizer = AutoTokenizer.from_pretrained("epfl-llm/meditron-7b")
     model = HuggingFacePretrainedModel(
-        model_type=HuggingFaceModelTypes.AutoModelForCausalLM, pretrained_model_name_or_path="epfl-llm/meditron-7b"
+        model_type=HuggingFaceModelTypes.AutoModelForCausalLM,
+        model_name="epfl-llm/meditron-7b",
+        prediction_key="logits",
     )
     print(model)
-    print(get_fsdp_block_from_huggingface_model(model))
