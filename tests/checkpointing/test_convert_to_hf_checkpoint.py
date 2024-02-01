f59734237124f01743e96e38d4fc2fe51adc5a59
rrutmann <97447451+rrutmann@users.noreply.github.com>
2024-02-01 15:33:56 +0100

    test(models): End-to-end test for checkpoint conversion adjusted to refactoring

--- tests/checkpointing/test_convert_to_hf_checkpoint.py
+++ tests/checkpointing/test_convert_to_hf_checkpoint.py
@@ -1,4 +1,5 @@
 import os
+from pathlib import Path
 
 import pytest
 import torch
@@ -12,7 +13,7 @@ from tests.conftest import torch_distributed_cleanup
 
 @pytest.fixture
 def checkpoint_dir():
-    return os.path.join(os.path.dirname(os.path.dirname(__file__)), "data", "checkpoint", "checkpoint_for_testing")
+    return Path(os.path.join(os.path.dirname(os.path.dirname(__file__)), "data", "checkpoint", "checkpoint_for_testing"))
 
 
 @pytest.fixture
@@ -25,32 +26,34 @@ def config(config_path, monkeypatch):
     monkeypatch.setenv("RANK", "0")
     monkeypatch.setenv("LOCAL_RANK", "0")
     monkeypatch.setenv("WORLD_SIZE", "1")
-    monkeypatch.setenv("CUDA_VISIBLE_DEVICES", "")
-    monkeypatch.setenv("MASTER_ADDR", "localhost")
-    monkeypatch.setenv("MASTER_PORT", "9948")
-    torch_distributed_cleanup()
     config_dict = load_app_config_dict(config_path)
     pydantic_config = AppConfig.model_validate(config_dict)
     return pydantic_config
 
 
-def test_convert_to_hf_checkpoint(tmp_path, config):
+@pytest.fixture
+def device():
+    return "cpu"
+
+
+def test_convert_to_hf_checkpoint(tmp_path, config, checkpoint_dir, device):
     # load test checkpoint
     main = Main(config)
-    main.load_and_convert_checkpoint(tmp_path)
-    wrapped_model = main.wrapped_model
+    main.load_and_convert_checkpoint(checkpoint_dir, tmp_path)
+    model = main.model
     # register config and model
     AutoConfig.register("modalities_gpt2", HuggingFaceModelConfig)
     AutoModelForCausalLM.register(HuggingFaceModelConfig, HuggingFaceModel)
-    wrapped_model.eval()
+    model.eval()
+    model = model.to(device)
     # check that model before and after loading return the same output
     test_tensor = torch.randint(10, size=(5, 10))
-    test_tensor = test_tensor.to(wrapped_model.compute_device)
-    output_before_loading = wrapped_model.forward({"input_ids": test_tensor})["logits"]
+    test_tensor = test_tensor.to(device)
+    output_before_loading = model.forward({"input_ids": test_tensor})["logits"]
     # load saved model
-    loaded_model = AutoModelForCausalLM.from_pretrained(tmp_path, torch_dtype=wrapped_model.module.lm_head.weight.dtype)
-    loaded_model = loaded_model.to(wrapped_model.compute_device)
-    assert loaded_model.dtype == wrapped_model.module.lm_head.weight.dtype
+    loaded_model = AutoModelForCausalLM.from_pretrained(tmp_path, torch_dtype=model.lm_head.weight.dtype)
+    loaded_model = loaded_model.to(device)
+    assert loaded_model.dtype == model.lm_head.weight.dtype
     loaded_model.eval()
     output_after_loading = loaded_model.forward(test_tensor)
     assert (output_after_loading == output_before_loading).all()
