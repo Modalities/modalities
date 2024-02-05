8ad989b3d82b08f7e8a2b065e313d5b848bae6b0
rrutmann <97447451+rrutmann@users.noreply.github.com>
2024-02-05 11:31:28 +0100

    feat(HuggingFaceModel): Add ability to generate text with HF models  - Overwrite prepare_inputs_for_generation  - Add dataclass ModalitiesModelOutput  - Add several required parameters to forward

--- src/modalities/models/gpt2/huggingface_model.py
+++ src/modalities/models/gpt2/huggingface_model.py
@@ -1,11 +1,15 @@
-from typing import Dict
+
+from dataclasses import dataclass
+from typing import Dict, Optional, Callable, Union, Any, Tuple
 
 import torch
 import torch.nn as nn
-from transformers import PreTrainedModel
+from transformers import PreTrainedModel, GenerationConfig, LogitsProcessorList, StoppingCriteriaList
 
 from modalities.config.config import HuggingFaceModelConfig
 from modalities.models.gpt2.gpt2_model import GPT2LLM
+from transformers.generation.utils import GenerateOutput
+from transformers.utils import ModelOutput
 
 
 class HuggingFaceModel(PreTrainedModel):
@@ -19,7 +23,79 @@ class HuggingFaceModel(PreTrainedModel):
         else:
             self.model = model
 
-    def forward(self, tensor: torch.Tensor):
-        model_input = {"input_ids": tensor}
+    def forward(
+            self,
+            input_ids: torch.Tensor,
+            attention_mask: Optional[torch.Tensor] = None,
+            return_dict: Optional[bool] = False,
+            output_attentions: Optional[bool] = False,
+            output_hidden_states: Optional[bool] = False,
+    ):
+        if output_attentions or output_hidden_states:
+            raise NotImplementedError
+        model_input = {"input_ids": input_ids, 'attention_mask': attention_mask}
         model_forward_output: Dict[str, torch.Tensor] = self.model.forward(model_input)
-        return model_forward_output[self.config.config.prediction_key]
+        if return_dict:
+            return ModalitiesModelOutput(**model_forward_output)
+        else:
+            return model_forward_output[self.config.config.prediction_key]
+
+    def prepare_inputs_for_generation(
+            self,
+            input_ids: torch.LongTensor,
+            attention_mask=None,
+            **kwargs) -> Dict[str, Any]:
+        """
+        Implement in subclasses of :class:`~transformers.PreTrainedModel` for custom behavior to prepare inputs in the
+        generate method.
+        """
+        return {
+            "input_ids": input_ids,
+            "attention_mask": attention_mask,
+        }
+
+
+@dataclass
+class ModalitiesModelOutput(ModelOutput):
+    """
+    Base class for model's outputs that may also contain a past key/values (to speed up sequential decoding).
+
+    Args:
+        last_hidden_state (`torch.FloatTensor` of shape `(batch_size, sequence_length, hidden_size)`):
+            Sequence of hidden-states at the output of the last layer of the model.
+
+            If `past_key_values` is used only the last hidden-state of the sequences of shape `(batch_size, 1,
+            hidden_size)` is output.
+        past_key_values (`tuple(tuple(torch.FloatTensor))`, *optional*, returned when `use_cache=True` is passed or when `config.use_cache=True`):
+            Tuple of `tuple(torch.FloatTensor)` of length `config.n_layers`, with each tuple having 2 tensors of shape
+            `(batch_size, num_heads, sequence_length, embed_size_per_head)`) and optionally if
+            `config.is_encoder_decoder=True` 2 additional tensors of shape `(batch_size, num_heads,
+            encoder_sequence_length, embed_size_per_head)`.
+
+            Contains pre-computed hidden-states (key and values in the self-attention blocks and optionally if
+            `config.is_encoder_decoder=True` in the cross-attention blocks) that can be used (see `past_key_values`
+            input) to speed up sequential decoding.
+        hidden_states (`tuple(torch.FloatTensor)`, *optional*, returned when `output_hidden_states=True` is passed or when `config.output_hidden_states=True`):
+            Tuple of `torch.FloatTensor` (one for the output of the embeddings, if the model has an embedding layer, +
+            one for the output of each layer) of shape `(batch_size, sequence_length, hidden_size)`.
+
+            Hidden-states of the model at the output of each layer plus the optional initial embedding outputs.
+        attentions (`tuple(torch.FloatTensor)`, *optional*, returned when `output_attentions=True` is passed or when `config.output_attentions=True`):
+            Tuple of `torch.FloatTensor` (one for each layer) of shape `(batch_size, num_heads, sequence_length,
+            sequence_length)`.
+
+            Attentions weights after the attention softmax, used to compute the weighted average in the self-attention
+            heads.
+        cross_attentions (`tuple(torch.FloatTensor)`, *optional*, returned when `output_attentions=True` and `config.add_cross_attention=True` is passed or when `config.output_attentions=True`):
+            Tuple of `torch.FloatTensor` (one for each layer) of shape `(batch_size, num_heads, sequence_length,
+            sequence_length)`.
+
+            Attentions weights of the decoder's cross-attention layer, after the attention softmax, used to compute the
+            weighted average in the cross-attention heads.
+    """
+    logits: torch.FloatTensor = None
+    # last_hidden_state: torch.FloatTensor = None
+    # past_key_values: Optional[Tuple[Tuple[torch.FloatTensor]]] = None
+    hidden_states: Optional[Tuple[torch.FloatTensor]] = None
+    attentions: Optional[Tuple[torch.FloatTensor]] = None
+    # cross_attentions: Optional[Tuple[torch.FloatTensor]] = None
