15d049dbf160dfe209404df98d7c184a77bf1881
Julian Spravil <julian.spravil@gmx.de>
2024-02-08 16:06:08 +0100

    test: add e2e test for coca

--- tests/models/coca/test_coca.py
+++ tests/models/coca/test_coca.py
@@ -2,15 +2,15 @@ from pathlib import Path
 
 import torch
 
-from modalities.__main__ import load_app_config_dict
+from modalities.__main__ import Main, load_app_config_dict
+from modalities.config.config import AppConfig
 from modalities.models.coca.coca_model import AttentionalPooling, CoCa, CoCaConfig
-
-_ROOT_DIR = Path(__file__).parents[1]
+from tests.conftest import _ROOT_DIR
 
 
 def test_coca():
     # Create model
-    config_file_path = _ROOT_DIR / Path("coca/coca_config.yaml")
+    config_file_path = _ROOT_DIR / Path("tests/models/coca/coca_config.yaml")
     config_dict = load_app_config_dict(config_file_path=config_file_path)
     coca_config = CoCaConfig.model_validate(config_dict)
     model = CoCa(**dict(coca_config))
@@ -47,3 +47,22 @@ def test_attn_pool():
     dummy_queries = torch.randn(1, 257, 768)
     out = model(dummy_vision_embed, dummy_queries)
     assert out.shape == (1, 257, 768)
+
+
+def test_e2e_coca_training_run_without_checkpoint(monkeypatch):
+    monkeypatch.setenv("RANK", "0")
+    monkeypatch.setenv("LOCAL_RANK", "0")
+    monkeypatch.setenv("WORLD_SIZE", "1")
+    monkeypatch.setenv("MASTER_ADDR", "localhost")
+    monkeypatch.setenv("MASTER_PORT", "9948")
+
+    # Load config
+    dummy_config_path = _ROOT_DIR / Path("config_files/config_example_coca.yaml")
+    config_dict = load_app_config_dict(dummy_config_path)
+    dummy_config = AppConfig.model_validate(config_dict)
+
+    # Disable checkpointing
+    dummy_config.checkpointing.checkpointing_strategy.config.k = 0
+
+    main = Main(dummy_config)
+    main.run()
