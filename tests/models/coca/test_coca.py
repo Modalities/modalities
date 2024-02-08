d755bb1f65bc024c3c1b76ec619540d4f543454d
Julian Spravil <julian.spravil@gmx.de>
2024-02-08 15:45:32 +0100

    test: add backwards test

--- tests/models/coca/test_coca.py
+++ tests/models/coca/test_coca.py
@@ -8,17 +8,31 @@ from modalities.models.coca.coca_model import AttentionalPooling, CoCa, CoCaConf
 _ROOT_DIR = Path(__file__).parents[1]
 
 
-def test_coca_forward():
+def test_coca():
+    # Create model
     config_file_path = _ROOT_DIR / Path("coca/coca_config.yaml")
     config_dict = load_app_config_dict(config_file_path=config_file_path)
     coca_config = CoCaConfig.model_validate(config_dict)
-    model = CoCa(coca_config)
+    model = CoCa(**dict(coca_config))
+
+    # Create dummy inputs
     dummy_input_image = torch.randn(1, 3, 224, 224)
     dummy_input_text = torch.randint(
         0, coca_config.text_decoder_config.vocab_size, (1, coca_config.multimodal_decoder_config.block_size)
     )
     dummy_input = dict(images=dummy_input_image, input_ids=dummy_input_text)
+
+    # Create optimizer
+    optimizer = torch.optim.SGD(model.parameters(), lr=0.001, momentum=0.9)
+
+    # Run one training step
+    optimizer.zero_grad()
     out = model(dummy_input)
+    loss = out["logits"].sum()
+    loss.backward()
+    optimizer.step()
+
+    # Test outputs
     assert "logits" in out
     assert "vision_cls" in out
     assert "text_cls" in out
