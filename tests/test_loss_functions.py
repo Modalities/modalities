d388d210f4f6cd083abc5b3b3606b824b6ba40ca
Sogol Haghighat <sogol.haghighat@iais.fraunhofer.de>
2024-02-09 17:37:35 +0100

    test: adapt test_nce_loss_correctness to uni and bidirectional loss

--- tests/test_loss_functions.py
+++ tests/test_loss_functions.py
@@ -7,7 +7,7 @@ from modalities.loss_functions import NCELoss, nce_loss
 
 @pytest.fixture
 def dummy_result_batch() -> InferenceResultBatch:
-    predictions = {"embedding": torch.FloatTensor(1024, 512).uniform_(-50, 50)}
+    predictions = {"embedding": torch.rand(1024, 512)}
     targets = {"target": torch.zeros(1024, 512)}
     batch_dim = 1024
     result_batch = InferenceResultBatch(targets, predictions, batch_dim)
@@ -21,7 +21,7 @@ def test_asymm_NCELoss_is_zero(dummy_result_batch, key):
     assert loss_func(dummy_result_batch) <= 10e-6
 
 
-# calculating nce_loss for two randomly generated batch of embeddings --> 1.1300 (pre-calculated manually)
+# calculating nce_loss for two randomly generated batch of embeddings (manually calculated)
 @pytest.mark.parametrize(
     "embedding1,embedding2",
     [
@@ -32,5 +32,7 @@ def test_asymm_NCELoss_is_zero(dummy_result_batch, key):
     ],
 )
 def test_nce_loss_correctness(embedding1, embedding2):
-    loss = nce_loss(embedding1, embedding2, device="cpu", is_asymmetric=True, temperature=1.0)
-    assert loss == pytest.approx(1.1300, 0.0001)
+    unidirectional_loss = nce_loss(embedding1, embedding2, device="cpu", is_asymmetric=True, temperature=1.0)
+    bidirectional_loss = nce_loss(embedding1, embedding2, device="cpu", is_asymmetric=False, temperature=1.0)
+    assert unidirectional_loss == pytest.approx(1.1300, 0.0001)
+    assert bidirectional_loss == pytest.approx(2.2577, 0.0001)
