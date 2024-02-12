c115b2b3b64a689cbb0a868aaaf1c1789e9da343
Max Luebbering <le1nux@users.noreply.github.com>
2024-02-12 14:41:21 +0100

    refactor: moved component factory into parent module

--- src/modalities/config/component_factory.py
+++ src/modalities/config/component_factory.py
@@ -1,18 +1,8 @@
-from pathlib import Path
 from typing import Any, Dict, List, Union
 
-from omegaconf import OmegaConf
 from pydantic import RootModel
 
-from modalities.config.hierarchical_instantiation.configs import (
-    CompConfigABC,
-    CompVConfig,
-    CompWConfig,
-    CompXConfig,
-    CompYConfig,
-    CompZConfig,
-    ReferenceConfig,
-)
+from tests.config.hierarchical_instantiation.configs import CompConfigABC
 
 
 class ComponentFactory:
@@ -137,24 +127,3 @@ class ComponentFactory:
         component_type = component_config.type_hint.value
         component = component_type(**component_config.model_dump(exclude=["type_hint"]))
         return component
-
-
-if __name__ == "__main__":
-
-    def load_app_config_dict(config_file_path: Path) -> Dict:
-        cfg = OmegaConf.load(config_file_path)
-        config_dict = OmegaConf.to_container(cfg, resolve=True)
-        return config_dict
-
-    config_file_path = Path(
-        "/raid/s3/opengptx/max_lue/modalities/src/modalities/config/hierarchical_instantiation/config.yaml"
-    )
-    comp_config_types = Union[CompVConfig, CompWConfig, CompXConfig, CompYConfig, CompZConfig, ReferenceConfig]
-    component_names = ["comp_z_1", "comp_x_1", "comp_y_1"]
-
-    config_dict = load_app_config_dict(config_file_path=config_file_path)
-
-    components = ComponentFactory.build_config(
-        config_dict=config_dict, config_types=comp_config_types, component_names=component_names
-    )
-    print(components)
