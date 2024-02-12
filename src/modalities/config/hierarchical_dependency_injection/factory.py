45f7ff4149b13c3cb45789b8584933ba9932574d
Max Luebbering <le1nux@users.noreply.github.com>
2024-02-12 14:19:24 +0100

    refactor: removed legacy code and added comments to component factory.

--- src/modalities/config/hierarchical_dependency_injection/factory.py
+++ src/modalities/config/hierarchical_dependency_injection/factory.py
@@ -47,10 +47,11 @@ class ComponentFactory:
             # if the entities are top level components, we return the component,
             # as it must have been built already via a referencing component
             if len(traversal_path) > 0 and traversal_path[-1] in top_level_components:
-                return top_level_components[traversal_path[-1]], top_level_components
-
+                entity_key = traversal_path[-1]
+                return top_level_components[entity_key], top_level_components
+            # if it is not a component that has been built already, we need to build it.
+            # We first traverse the config for possible sub components that need to build beforehand.
             materialized_component_config = {}
-            # before materializing the component, we need to materialize the sub components
             for sub_entity_key, sub_component_config_dict in current_component_config.items():
                 materialized_component_config[sub_entity_key], top_level_components = ComponentFactory._build_component(
                     current_component_config=sub_component_config_dict,
@@ -59,10 +60,7 @@ class ComponentFactory:
                     top_level_components=top_level_components,
                     traversal_path=traversal_path + [sub_entity_key],
                 )
-
-            if len(traversal_path) == 0:
-                return materialized_component_config, top_level_components
-
+            # After building all the sub components, we can now build the actual component
             # if the config is component_config then we instantiate the component
             if ComponentFactory._is_component_config(config_dict=current_component_config):
                 # instantiate component config
@@ -72,6 +70,10 @@ class ComponentFactory:
                 # instantiate component
                 component = ComponentFactory._instantiate_component(component_config=current_component_config)
                 print(" -> ".join(traversal_path) + ":", component)
+
+                # if the component is a top level component, then we add it to the top level components dictionary
+                # to make sure that we don't build it again. Building it again would mean that we work by-value
+                # instead of by reference.
                 if len(traversal_path) == 1:
                     entity_key = traversal_path[-1]
                     top_level_components[entity_key] = component
@@ -88,6 +90,9 @@ class ComponentFactory:
                         top_level_components=top_level_components,
                         traversal_path=[referenced_entity_key],
                     )
+                    # we add the newly build reference config to the top level components dict
+                    # so that we don't instantiate it again when we reach the respective component config
+                    # in the subsequent config traversal
                     top_level_components[referenced_entity_key] = materialized_referenced_component
                 print(" -> ".join(traversal_path) + ": ", f"--ref--> {top_level_components[referenced_entity_key]}")
                 return top_level_components[referenced_entity_key], top_level_components
@@ -108,15 +113,9 @@ class ComponentFactory:
             return materialized_component_configs, top_level_components
 
         else:
+            # we return the raw sub config if the sub config is not a dictionary or a list
+            # i.e., just a "scalar" value (e.g., string, int, etc.), since we don't have to build it.
             return current_component_config, top_level_components
-
-    @staticmethod
-    def _build_reference_component(
-        current_component_config: Union[Dict, List, Any],
-        component_config: Union[Dict, List, Any],
-        config_types,
-    ) -> Any:
-        pass
 
     @staticmethod
     def _is_component_config(config_dict: Dict) -> bool:
