f7dfe3123ded20435c217091a6eeae6aa4a080d4
Max Luebbering <le1nux@users.noreply.github.com>
2024-02-16 20:04:54 +0100

    refactor: Refactored CheckpointingFactory

--- src/modalities/checkpointing/checkpointing_factory.py
+++ src/modalities/checkpointing/checkpointing_factory.py
@@ -1,36 +1,17 @@
-from modalities.checkpointing.checkpointing import (
-    Checkpointing,
-    CheckpointingExecutionIF,
-    CheckpointingIF,
-    CheckpointingStrategyIF,
-)
-from modalities.config.config import CheckpointingConfig
-from modalities.config.resolver_register import ResolverRegister
+from modalities.checkpointing.checkpointing import CheckpointingExecutionIF
+from modalities.checkpointing.checkpointing_execution import FSDPToDiscCheckpointing
 from modalities.running_env.fsdp.fsdp_running_env import RunningEnv
 
 
-class CheckpointingFactory:
+class CheckpointingExecutionFactory:
     @staticmethod
-    def get_checkpointing(
-        resolvers: ResolverRegister,
-        config: CheckpointingConfig,
-        running_env: RunningEnv,
-        experiment_id: str,
-        num_ranks: int,
-    ) -> CheckpointingIF:
-        checkpointing_strategy: CheckpointingStrategyIF = resolvers.build_component_by_config(
-            config=config.checkpointing_strategy, extra_kwargs={}
+    def get_fsdp_to_disc_checkpointing(
+        checkpoint_path: str, global_rank: int, running_env: RunningEnv, experiment_id: str
+    ) -> CheckpointingExecutionIF:
+        fsdp_to_disc_checkpointing = FSDPToDiscCheckpointing(
+            checkpoint_path=checkpoint_path,
+            global_rank=global_rank,
+            experiment_id=experiment_id,
+            model_wrapping_fn=running_env.wrap_model,
         )
-
-        checkpointing_execution: CheckpointingExecutionIF = resolvers.build_component_by_config(
-            config=config.checkpointing_execution,
-            extra_kwargs={"experiment_id": experiment_id, "model_wrapping_fn": running_env.wrap_model},
-        )
-
-        checkpointing = Checkpointing(
-            checkpointing_strategy=checkpointing_strategy,
-            checkpointing_execution=checkpointing_execution,
-            num_ranks=num_ranks,
-        )
-
-        return checkpointing
+        return fsdp_to_disc_checkpointing
