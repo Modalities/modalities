d446260353c2e290e3fe2654062689bd289151ea
Max Luebbering <le1nux@users.noreply.github.com>
2024-02-16 20:03:54 +0100

    feat: added new configs in separate file for now

---
+++ src/modalities/config/config_new.py
@@ -0,0 +1,104 @@
+from pathlib import Path
+from typing import Annotated, Any
+
+from pydantic import BaseModel, GetCoreSchemaHandler, PositiveInt, conint
+from pydantic_core import core_schema
+
+from modalities.checkpointing.checkpointing_execution import CheckpointingExecutionIF
+from modalities.checkpointing.checkpointing_strategies import CheckpointingStrategyIF
+from modalities.config.lookup_types import LookupEnum
+from modalities.running_env.running_env import RunningEnv
+
+
+class PydanticRunningEnvIF:
+    @classmethod
+    def __get_pydantic_core_schema__(
+        cls,
+        _source_type: Any,
+        _handler: GetCoreSchemaHandler,
+    ) -> core_schema.CoreSchema:
+        # see: https://docs.pydantic.dev/latest/concepts/types/#handling-third-party-types
+        return core_schema.json_or_python_schema(
+            json_schema=core_schema.is_instance_schema(RunningEnv),
+            python_schema=core_schema.is_instance_schema(RunningEnv),
+            # serialization=core_schema.plain_serializer_function_ser_schema(
+            #     lambda instance: instance.x
+            # ),
+        )
+
+
+class PydanticCheckpointingStrategyIF:
+    @classmethod
+    def __get_pydantic_core_schema__(
+        cls,
+        _source_type: Any,
+        _handler: GetCoreSchemaHandler,
+    ) -> core_schema.CoreSchema:
+        # see: https://docs.pydantic.dev/latest/concepts/types/#handling-third-party-types
+        return core_schema.json_or_python_schema(
+            json_schema=core_schema.is_instance_schema(CheckpointingStrategyIF),
+            python_schema=core_schema.is_instance_schema(CheckpointingStrategyIF),
+            # serialization=core_schema.plain_serializer_function_ser_schema(
+            #     lambda instance: instance.x
+            # ),
+        )
+
+
+class PydanticCheckpointingExecutionIF:
+    @classmethod
+    def __get_pydantic_core_schema__(
+        cls,
+        _source_type: Any,
+        _handler: GetCoreSchemaHandler,
+    ) -> core_schema.CoreSchema:
+        # see: https://docs.pydantic.dev/latest/concepts/types/#handling-third-party-types
+        return core_schema.json_or_python_schema(
+            json_schema=core_schema.is_instance_schema(CheckpointingExecutionIF),
+            python_schema=core_schema.is_instance_schema(CheckpointingExecutionIF),
+            # serialization=core_schema.plain_serializer_function_ser_schema(
+            #     lambda instance: instance.x
+            # ),
+        )
+
+
+PydanticRunningEnvType = Annotated[RunningEnv, PydanticRunningEnvIF]
+PydanticCheckpointingStrategyIFType = Annotated[CheckpointingStrategyIF, PydanticCheckpointingStrategyIF]
+PydanticCheckpointingExecutionIFType = Annotated[CheckpointingExecutionIF, PydanticCheckpointingExecutionIF]
+
+
+class PassType(LookupEnum):
+    BY_VALUE = "by_value"
+    BY_REFERENCE = "by_reference"
+
+
+class ReferenceConfig(BaseModel):
+    instance_key: str
+    pass_type: PassType
+
+
+class CLMCrossEntropyLossConfig(BaseModel):
+    target_key: str
+    prediction_key: str
+
+
+# Checkpointing
+
+
+class SaveEveryKStepsCheckpointingStrategyConfig(BaseModel):
+    k: PositiveInt
+
+
+class SaveKMostRecentCheckpointsStrategyConfig(BaseModel):
+    k: conint(ge=-1)
+
+
+class FSDPToDiscCheckpointingConfig(BaseModel):
+    checkpoint_path: Path
+    global_rank: conint(ge=0)
+    experiment_id: str
+    running_env: PydanticRunningEnvType
+
+
+class CheckpointingConfig(BaseModel):
+    checkpointing_strategy: PydanticCheckpointingStrategyIFType
+    checkpointing_execution: PydanticCheckpointingExecutionIFType
