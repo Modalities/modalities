b324c3f5520ebfe5caf8db0dd55a5aa4ac5177d8
Max Luebbering <le1nux@users.noreply.github.com>
2024-02-19 14:41:37 +0100

    refactor: refactored dataloader and its factory

--- src/modalities/dataloader/dataloader_factory.py
+++ src/modalities/dataloader/dataloader_factory.py
@@ -1,82 +1,35 @@
+from typing import Callable, Optional
+
+from torch.utils.data import BatchSampler
 from torch.utils.data.dataset import Dataset
 
-from modalities.config.config import DataLoaderConfig, DatasetConfig
-from modalities.config.resolver_register import ResolverRegister
 from modalities.dataloader.dataloader import LLMDataLoader
-from modalities.dataloader.open_gptx_dataset.open_gptx_dataset import OpenGPTXMMapDataset
 from modalities.dataloader.samplers import ResumableBatchSampler
-
-
-class OpenGPTXDatasetWrapper(Dataset):
-    def __init__(self, open_gptx_dataset: OpenGPTXMMapDataset, num_samples: int) -> None:
-        super().__init__()
-        self.open_gptx_dataset = open_gptx_dataset
-        self.num_samples = num_samples
-
-    def __len__(self):
-        return self.num_samples
-
-    def __getitem__(self, idx: int):
-        if self.num_samples > idx:
-            return self.open_gptx_dataset.__getitem__(idx)
-        else:
-            raise ValueError("num_samples <= idx")
 
 
 class DataloaderFactory:
     @staticmethod
     def get_dataloader(
-        resolvers: ResolverRegister, config: DataLoaderConfig, skip_num_batches: int = 0
+        dataloader_tag: str,
+        dataset: Dataset,
+        batch_sampler: BatchSampler,
+        collate_fn: Callable,
+        num_workers: int,
+        pin_memory: bool,
+        shuffle: bool,
+        skip_num_batches: Optional[int] = 0,
     ) -> LLMDataLoader:
-        # TODO: replace this with dynamic nested object instantiation. (More details: Different Dataloaders require
-        #  different objects in their constructors. the resolvers should be able to provide the necessary complex
-        #  objects automatically, without us manually creating this complex factory.)
-        additional_init_payload = {}
-        if hasattr(config.config.dataset.config, "tokenizer"):
-            tokenizer = resolvers.build_component_by_config(config=config.config.dataset.config.tokenizer)
-            tokenizer.pad_token = tokenizer.eos_token
-            additional_init_payload.update(tokenizer=tokenizer)
+        if skip_num_batches > 0:
+            batch_sampler = ResumableBatchSampler(start_index=skip_num_batches, underlying_batch_sampler=batch_sampler)
 
-        dataset = resolvers.build_component_by_config(
-            config=config.config.dataset, extra_kwargs=additional_init_payload
+        dataloader = LLMDataLoader(
+            dataloader_tag=dataloader_tag,
+            batch_sampler=batch_sampler,
+            dataset=dataset,
+            collate_fn=collate_fn,
+            num_workers=num_workers,
+            pin_memory=pin_memory,
+            shuffle=shuffle,
         )
 
-        # BUG: Sometimes the dataset genereated by the OpenGPTXMMap implementation has too many samples.
-        # This is a workaround to fix the dataset to the size, as specified in the config!
-        # TODO: Fix the OpenGPTX implementation and get rid of this hack.
-        if isinstance(config.config.dataset.config, DatasetConfig.OpenGPTXMMapDatasetConfig):
-            dataset = OpenGPTXDatasetWrapper(
-                open_gptx_dataset=dataset, num_samples=config.config.dataset.config.num_samples
-            )
-
-        collator = resolvers.build_component_by_config(config=config.config.collate_fn)
-        sampler = resolvers.build_component_by_config(
-            config=config.config.batch_sampler.config.sampler, extra_kwargs=dict(dataset=dataset)
-        )
-
-        batch_sampler = resolvers.build_component_by_config(
-            config=config.config.batch_sampler,
-            extra_kwargs=dict(
-                sampler=sampler,
-            ),
-        )
-
-        resumable_batch_sampler = ResumableBatchSampler(
-            start_index=skip_num_batches, underlying_batch_sampler=batch_sampler
-        )
-
-        dataloader = resolvers.build_component_by_config(
-            config=config,
-            extra_kwargs=dict(
-                dataset=dataset,
-                batch_sampler=resumable_batch_sampler,
-                collate_fn=collator,
-            ),
-        )
-
-        # TODO we should have this check rather in the gym. Here, it is clear that
-        # we are using the LLMDataLoader
-        assert isinstance(
-            dataloader, LLMDataLoader
-        ), f"Dataloader Class must use the {LLMDataLoader.__name__}-Interface"
         return dataloader
