0d27684c78fabb0494d0d6350168e4167321c3b3
Julian Spravil <julian.spravil@gmx.de>
2024-02-26 13:52:57 +0100

    refactor: replace asserts with value errors

--- src/modalities/models/coca/collator.py
+++ src/modalities/models/coca/collator.py
@@ -9,8 +9,13 @@ from modalities.batch import DatasetBatch
 class CoCaCollator:
     def __init__(self, sample_keys: List[str], target_keys: List[str], text_sample_key: str, text_target_key: str):
         self.device: torch.device = field(default_factory=lambda: torch.device("cpu"))
-        assert text_sample_key in sample_keys, f"{text_sample_key} is not part of sample keys {sample_keys}"
-        assert text_target_key not in target_keys, f"{text_target_key} should not be part of target keys {target_keys}"
+        if text_sample_key not in sample_keys:
+            raise ValueError(f"{text_sample_key} is not part of sample keys {sample_keys}")
+        if text_target_key in target_keys:
+            raise ValueError(
+                f"{text_target_key} should not be part of target keys {target_keys}, "
+                f"because {text_target_key} will generated based on {text_sample_key}"
+            )
         self.sample_keys = sample_keys
         self.target_keys = target_keys
         self.text_sample_key = text_sample_key
