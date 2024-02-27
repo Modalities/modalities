bdd779dd9a7e1f351316b0a393bdb5f9fce0ced7
Julian Spravil <julian.spravil@gmx.de>
2024-02-27 08:39:09 +0100

    test: add test for vision transformer

---
+++ tests/models/vision_transformer/test_vision_transformer.py
@@ -0,0 +1,33 @@
+from pathlib import Path
+
+import torch
+
+from modalities.__main__ import load_app_config_dict
+from modalities.models.vision_transformer.vision_transformer_model import VisionTransformer, VisionTransformerConfig
+from tests.conftest import _ROOT_DIR
+
+
+def test_vision_transformer():
+    # Create model
+    config_file_path = _ROOT_DIR / Path("tests/models/vision_transformer/vision_transformer_config.yaml")
+    config_dict = load_app_config_dict(config_file_path=config_file_path)
+    config = VisionTransformerConfig.model_validate(config_dict)
+    model = VisionTransformer(**dict(config))
+
+    # Create dummy inputs
+    dummy_input_image = torch.randn(1, 3, 224, 224)
+    dummy_input = dict(images=dummy_input_image)
+
+    # Create optimizer
+    optimizer = torch.optim.SGD(model.parameters(), lr=0.001, momentum=0.9)
+
+    # Run one training step
+    optimizer.zero_grad()
+    out = model(dummy_input)
+    loss = out["logits"].sum()
+    loss.backward()
+    optimizer.step()
+
+    # Test outputs
+    assert "logits" in out
+    assert out["logits"].shape == (1, 1000)
