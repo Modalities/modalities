77adc42dd8531e741b0c20685e84a29551ed9737
Julian Spravil <julian.spravil@gmx.de>
2024-03-08 18:43:42 +0100

    fix: attention tests

--- tests/nn/test_attention.py
+++ tests/nn/test_attention.py
@@ -1,19 +1,19 @@
 import pytest
 import torch
 
-from modalities.nn.attention import Attention
+from modalities.nn.attention import AttentionType, MultiHeadAttention
 
 
-@pytest.mark.parametrize("is_causal", [True, False])
-def test_attention_forward(is_causal):
-    model = Attention(n_embd=64, n_head=8, is_causal=is_causal)
+@pytest.mark.parametrize("attention_type", [AttentionType.CAUSAL_ATTENTION, AttentionType.NON_CAUSAL_ATTENTION])
+def test_attention_forward(attention_type):
+    model = MultiHeadAttention(n_embd=64, n_head=8, attention_type=attention_type)
     dummy_input = torch.randn(1, 256, 64)
     out = model(dummy_input)
     assert out.shape == (1, 256, 64)
 
 
 def test_attention_with_cross_attention_forward():
-    model = Attention(n_embd=64, n_head=8, use_cross_attention=True)
+    model = MultiHeadAttention(n_embd=64, n_head=8, attention_type=AttentionType.CROSS_ATTENTION)
     dummy_input = torch.randn(1, 256, 64)
     dummy_context = torch.randn(1, 16, 64)
     out = model(dummy_input, context=dummy_context)
