415b0a6a0d3bd589f35724f6a661c13b487fd6dd
Luzian Hahn <luzian.hahn@iis.fraunhofer.de>
2024-03-11 11:12:53 +0100

    test: add attention forward pass test for GQA

---
+++ tests/models/test_attention.py
@@ -0,0 +1,40 @@
+import pytest
+import torch
+
+from modalities.models.gpt2.gpt2_model import AttentionConfig, AttentionType, CausalSelfAttention
+
+
+@pytest.mark.parametrize(
+    "n_head_q, n_head_kv, n_embd, att_type, successful",
+    [
+        (4, 4, 32, AttentionType.DEFAULT_ATTENTION, True),
+        (8, 2, 32, AttentionType.DEFAULT_ATTENTION, True),
+        (9, 8, 32, AttentionType.DEFAULT_ATTENTION, False),
+        (8, 3, 32, AttentionType.DEFAULT_ATTENTION, False),
+    ],
+)
+def test_grouped_query_attention_forward(n_head_q, n_head_kv, n_embd, att_type, successful):
+    batch_size = 2
+    block_size = 10
+    embedding_shape = (batch_size, block_size, n_embd)
+    embedded_input_seq = torch.rand(size=embedding_shape, dtype=torch.float32)
+
+    def attention_forward_pass(att_type, block_size, embedded_input_seq, n_embd, n_head_kv, n_head_q):
+        attention_layer = CausalSelfAttention(
+            n_head_q=n_head_q,
+            n_head_kv=n_head_kv,
+            n_embd=n_embd,
+            attention=AttentionConfig(attention_type=att_type, scaling_factor=3),
+            bias=False,
+            dropout=False,
+            block_size=block_size,
+        )
+        output_tensor: torch.Tensor = attention_layer(embedded_input_seq)
+        return output_tensor
+
+    if not successful:
+        with pytest.raises(Exception):
+            attention_forward_pass(att_type, block_size, embedded_input_seq, n_embd, n_head_kv, n_head_q)
+    else:
+        output_tensor = attention_forward_pass(att_type, block_size, embedded_input_seq, n_embd, n_head_kv, n_head_q)
+        assert output_tensor.size() == embedding_shape
