c3242e34397bbbe2fe3d1cfc3a167877c71ec288
Felix Stollenwerk <felix.stollenwerk@ai.se>
2024-03-12 14:04:36 +0100

    test: causal self-attention type equality

--- tests/models/test_causal_self_attention.py
+++ tests/models/test_causal_self_attention.py
@@ -1,3 +1,5 @@
+from copy import deepcopy
+
 import pytest
 import torch
 
@@ -56,3 +58,33 @@ def test_forward_pass_success(n_head_q, n_head_kv, n_embd, attention_type, succe
         embedded_input_seq = _get_random_input_seq(embedding_shape)
         output_tensor = attention_layer(embedded_input_seq)
         assert output_tensor.shape == embedding_shape
+
+
+@pytest.mark.parametrize(
+    "n_head_q, n_head_kv, n_embd",
+    [
+        (4, 4, 32),
+        (8, 2, 32),
+    ],
+)
+def test_attention_types_equality(n_head_q, n_head_kv, n_embd):
+    batch_size = 2
+    block_size = 10
+    embedding_shape = (batch_size, block_size, n_embd)
+    embedded_input_seq = _get_random_input_seq(embedding_shape)
+
+    attention_layer_args = {
+        "n_head_q": n_head_q,
+        "n_head_kv": n_head_kv,
+        "n_embd": n_embd,
+        "attention_type": AttentionType.DEFAULT_ATTENTION,
+        "block_size": block_size,
+    }
+
+    attention_layer_default = _get_random_attention_layer(**attention_layer_args)
+    attention_layer_flash = deepcopy(attention_layer_default)
+    attention_layer_flash.flash = True
+
+    output_tensor_default = attention_layer_default(embedded_input_seq)
+    output_tensor_flash = attention_layer_flash(embedded_input_seq)
+    torch.testing.assert_close(output_tensor_default, output_tensor_flash)
