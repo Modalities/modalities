56b88aef06c004b1b272109edf130dd7b928a185
Max Luebbering <le1nux@users.noreply.github.com>
2024-03-13 17:12:52 +0100

    test: removed LayerNorm tests and improved RMSLayerNorm tests

--- tests/models/components/test_layer_norms.py
+++ tests/models/components/test_layer_norms.py
@@ -2,22 +2,16 @@ import numpy as np
 import pytest
 import torch
 import torch.nn as nn
-from torch.nn import functional as F
 
-from modalities.models.components.layer_norms import RMSLayerNorm, ZLayerNorm
+from modalities.models.components.layer_norms import RMSLayerNorm
 
 
 @pytest.fixture
 def rms_layer_norm() -> RMSLayerNorm:
     norm = RMSLayerNorm(ndim=3, epsilon=1e-6)
     weight_tensor = torch.Tensor([1, 2, 3])
-    norm.weight = nn.Parameter(weight_tensor)
-    return norm
-
-
-@pytest.fixture
-def z_layer_norm() -> ZLayerNorm:
-    norm = ZLayerNorm(ndim=3, bias=True, epsilon=1e-6)
+    norm.gain = nn.Parameter(weight_tensor)
+    norm.bias_tensor = nn.Parameter(torch.ones(3))
     return norm
 
 
@@ -25,23 +19,7 @@ def test_rms_layer_norm_forward(rms_layer_norm):
     x = torch.Tensor([0.1, 0.2, 0.3])
     output = rms_layer_norm(x)
     ref_x = x / np.sqrt((0.1**2 + 0.2**2 + 0.3**2) / 3 + 1e-6)
-    ref_tensor = ref_x * rms_layer_norm.weight
-
-    assert output.shape == x.shape
-    assert all(output == ref_tensor)
-
-
-def test_z_layer_norm_forward(z_layer_norm):
-    x = torch.Tensor([0.1, 0.2, 0.3])
-    output = z_layer_norm(x)
-    ndim = z_layer_norm.weight.shape[0]
-    ref_tensor = F.layer_norm(
-        input=x,
-        normalized_shape=z_layer_norm.weight.shape,
-        weight=nn.Parameter(torch.ones(ndim)),
-        bias=nn.Parameter(torch.zeros(ndim)) if z_layer_norm.bias_tensor is not None else None,
-        eps=z_layer_norm.epsilon,
-    )
+    ref_tensor = ref_x * rms_layer_norm.gain + torch.tensor([1, 1, 1])
 
     assert output.shape == x.shape
     assert all(output == ref_tensor)
