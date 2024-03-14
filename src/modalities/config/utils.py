e807e676cb6452c229d4a5e7a649ee2e85ad5492
fromm-m <michael.fromm.91@googlemail.com>
2024-03-14 10:50:20 +0100

    fix: fix linting #2

--- src/modalities/config/utils.py
+++ src/modalities/config/utils.py
@@ -2,6 +2,7 @@ from typing import Any, Dict
 
 from pydantic import BaseModel
 
+
 def convert_base_model_config_to_dict(config: BaseModel) -> Dict[Any, Any]:
-    """"Converts non-recursively a Pydantic BaseModel to a dictionary."""
+    """ "Converts non-recursively a Pydantic BaseModel to a dictionary."""
     return {key: getattr(config, key) for key in config.model_dump().keys()}
