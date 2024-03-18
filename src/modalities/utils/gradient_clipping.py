35bf777bc06efb252ebbf75cdcee576bf69cf63e
Timm Ruland <timm.heine.ruland@iais.fraunhofer.de>
2024-03-18 14:18:43 +0100

    docs(utilities): Added explanation comments in build_gradient_clipper().

--- src/modalities/utils/gradient_clipping.py
+++ src/modalities/utils/gradient_clipping.py
@@ -10,8 +10,10 @@ def build_gradient_clipper(
     gradient_clipping_mode: GradientClippingMode, gradient_clipping_threshold: float
 ) -> Callable[[NNModel], None]:
     if gradient_clipping_mode == GradientClippingMode.P2_NORM:
+        # Always return None to satisfy the Callable[[NNModel], None] interface.
         return lambda model: (clip_grad_norm_(model.parameters(), gradient_clipping_threshold, 2), None)[-1]
     if gradient_clipping_mode == GradientClippingMode.MAX_NORM:
+        # Always return None to satisfy the Callable[[NNModel], None] interface.
         return lambda model: (clip_grad_norm_(model.parameters(), gradient_clipping_threshold, "inf"), None)[-1]
     if gradient_clipping_mode == GradientClippingMode.VALUE:
         return lambda model: clip_grad_value_(model.parameters(), gradient_clipping_threshold)
