617fe7ad8cd0faede54ef81a1067853daa9534b0
Max <le1nux@users.noreply.github.com>
2024-04-11 08:54:44 +0000

    chore: removed unused lookup enum

--- src/modalities/config/look_up_enum.py
+++
@@ -1,8 +0,0 @@
-from enum import Enum
-
-
-class LookupEnum(Enum):
-    @classmethod
-    def _missing_(cls, value: str) -> type:
-        """constructs Enum by member name, if not constructable by value"""
-        return cls.__dict__[value]
