c26670a963a8827369c9eada893a29fb51e1a870
Nicolo' Brandizzi <nicofirst1@gmail.com>
2024-04-15 11:30:28 +0200

    add text only preparation

--- benchmarks/dataformats/preparation.py
+++ benchmarks/dataformats/preparation.py
@@ -7,8 +7,9 @@ import webdataset as wds
 from src.modalities.dataloader.dataset import PackedMemMapDataset
 from pathlib import Path
 
+
 @profile(runs=1)
-def prepare_webdataset(json_path, output_dir):
+def prepare_webdataset_coco(json_path, output_dir):
 
     shard_writer = wds.ShardWriter(f"{output_dir}/%06d.tar", maxcount=10000)
 
@@ -34,10 +35,27 @@ def prepare_webdataset(json_path, output_dir):
             shard_writer.write(sample)
         shard_writer.close()
 
+
+@profile(runs=1)
+def prepare_webdataset_cc(json_path, output_dir):
+
+    shard_writer = wds.ShardWriter(f"{output_dir}/%06d.tar", maxcount=10000)
+
+    with open(json_path, 'r') as f:
+        for line in f:
+            data = json.loads(line)
+            sample = {
+                "text": data['text'],
+            }
+            shard_writer.write(sample)
+        shard_writer.close()
+
+def prepare_memmap(json_path, output_dir):
+    pass
     
 
-if __name__ == "__main__":
 
+def raid_main():
     opts = get_common_opts(sys.argv[1:])
 
     if not opts.json_file:
@@ -81,8 +99,64 @@ if __name__ == "__main__":
         else:
             raise Exception("Directory already exists")
 
-    web_data = prepare_webdataset(json_path=json_file, output_dir=web_out_dir)
+    web_data = prepare_webdataset_coco(json_path=json_file, output_dir=web_out_dir)
         
      ########################################################
     # Memmap preparation
     #########################################################
+
+
+def barnard_main():
+    #opts = get_common_opts(sys.argv[1:])
+
+    if not opts.json_file:
+        raise Exception("Json file not provided")
+
+    json_file = Path(opts.json_file)
+    
+    # check if the file exists
+    if not json_file.exists():
+        raise Exception("Json file does not exist")
+    
+    
+   
+    ########################################################
+    # Webdataset preparation
+    ########################################################
+
+    if not opts.webout:
+        raise Exception("Output directory not provided")
+     
+    # create the output directory
+    web_out_dir = Path(opts.webout)
+
+
+    # check if the dir  exists
+    if not web_out_dir.exists():
+        os.makedirs(web_out_dir)
+    else:
+        # prompt the user to delete the directory
+        print(f"Directory {web_out_dir} already exists. Do you want to delete it? [y/n]")
+        response = input()
+        if response == "y":
+            os.system(f"rm -rf {web_out_dir}")
+            os.makedirs(web_out_dir)
+        else:
+            raise Exception("Directory already exists")
+
+    web_data = prepare_webdataset_cc(json_path=json_file, output_dir=web_out_dir)
+        
+     ########################################################
+    # Memmap preparation
+    #########################################################
+
+
+if __name__ == "__main__":
+    opts = get_common_opts(sys.argv[1:])
+
+    opts.json_file="/data/horse/ws/s6690609-gptx_traindata/data_filtered_deduplicated/it/oscar/2021-39_filtered_deduplicated/2021-39_filtered_deduplicated_it_part_077_of_200.jsonl"
+    opts.webout="/home/nibr274g/webdata"
+
+
+    
+    barnard_main()
