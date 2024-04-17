1a0e03d66f74fa0dcc1ce26f0e4cb89b0fbd0aec
Mehdi <ali-mehdi@live.de>
2024-04-17 19:06:57 +0000

    fix: fixed the fsdp warmstart test its now running

--- tests/end2end_tests/test_fsdp_warmstart.py
+++ tests/end2end_tests/test_fsdp_warmstart.py
@@ -38,23 +38,54 @@ class SaveAllResultSubscriberConfig(BaseModel):
 # )
 class TestWarmstart:
     def test_warm_start(self):
-        with tempfile.TemporaryDirectory():
+        with tempfile.TemporaryDirectory() as temp_dir:
+            # config for two steps model
             gpt2_two_steps_config_file_path = Path("tests/end2end_tests/gpt2_two_steps.yaml")
             gpt2_two_steps_config_dict = load_app_config_dict(gpt2_two_steps_config_file_path)
-            # gpt2_two_steps_config_dict["checkpointing"]["config"]["checkpointing_execution"]
-            # ["config"]["checkpoint_path"]
-            # = temporary_checkpoint_folder_path
-            # gpt2_two_steps_config_dict["settings"]["experiment_id"] = "0"
+            
+            # adopt the checkpoint path
+            gpt2_two_steps_config_dict["checkpointing"]["config"]["checkpointing_execution"]["config"]["checkpoint_path"]= str(Path(temp_dir))
+            gpt2_two_steps_config_dict["settings"]["experiment_id"] = "0"
 
-            main_obj = Main(gpt2_two_steps_config_dict, gpt2_two_steps_config_file_path)
+            # config for one step model
+            gpt2_warm_start_from_step_1_config_file_path = Path("tests/end2end_tests/gpt2_warm_start_from_step_1.yaml")
+            gpt2_warm_start_from_step_1_dict = load_app_config_dict(gpt2_two_steps_config_file_path)
+
+            # adopt the checkpoint path
+            gpt2_warm_start_from_step_1_dict["checkpointing"]["config"]["checkpointing_execution"]["config"]["checkpoint_path"]= str(Path(temp_dir)) + "/0/"
+            gpt2_two_steps_config_dict["wrapped_model"]["config"]["checkpoint_path"] = str(Path(temp_dir)) + "/0/"
+
+
+            main_obj_1 = Main(gpt2_two_steps_config_dict, gpt2_two_steps_config_file_path)
             with CudaEnv(process_group_backend=ProcessGroupBackendType.nccl):
-                main_obj.add_custom_component(
+                main_obj_1.add_custom_component(
                     component_key="results_subscriber",
                     variant_key="save_all",
                     custom_component=SaveAllResultSubscriber,
                     custom_config=SaveAllResultSubscriberConfig,
                 )
-                components_1 = main_obj.build_components(components_model_type=TrainingComponentsInstantiationModel)
-                main_obj.run(components_1)
+                components_1 = main_obj_1.build_components(components_model_type=TrainingComponentsInstantiationModel)
+                main_obj_1.run(components_1)
+            
+            main_obj_2 = Main(gpt2_warm_start_from_step_1_dict, gpt2_warm_start_from_step_1_config_file_path)
+            with CudaEnv(process_group_backend=ProcessGroupBackendType.nccl):
+                main_obj_2.add_custom_component(
+                    component_key="results_subscriber",
+                    variant_key="save_all",
+                    custom_component=SaveAllResultSubscriber,
+                    custom_config=SaveAllResultSubscriberConfig,
+                )
+                components_2 = main_obj_2.build_components(components_model_type=TrainingComponentsInstantiationModel)
+                main_obj_2.run(components_2)
+            
             messages_1: List[Message[EvaluationResultBatch]] = components_1.evaluation_subscriber.message_list
-            assert messages_1[-1].payload.losses is None
+            messages_2: List[Message[EvaluationResultBatch]] = components_2.evaluation_subscriber.message_list
+
+            key="CLMCrossEntropyLoss interval average"
+            assert messages_1[-1].payload.losses[key] == messages_2[-1].payload.losses[key] 
+            # messages_1: List[Message[EvaluationResultBatch]] = components_1.evaluation_subscriber.message_list
+            # expected_values = {'CLMCrossEntropyLoss interval average': 10.7891}
+            # for key, expected in expected_values.items():
+            #     assert key in messages_1[-1].payload.losses, f"Key '{key}' not found in observed values"
+            #     observed = messages_1[-1].payload.losses[key]
+            #     assert observed == expected, f"AssertionError: {observed} != {expected} for key '{key}'"
