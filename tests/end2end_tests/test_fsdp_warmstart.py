8a71e6324e0a5cd2c02bb1b282e68753a5b89048
Max Luebbering <le1nux@users.noreply.github.com>
2024-04-18 20:19:54 +0200

    test: end2end test for warmstart are running now

--- tests/end2end_tests/test_fsdp_warmstart.py
+++ tests/end2end_tests/test_fsdp_warmstart.py
@@ -1,7 +1,12 @@
+import json
+import os
 import tempfile
 from pathlib import Path
 from typing import List
 
+import pytest
+import torch
+import torch.distributed as dist
 from pydantic import BaseModel
 
 from modalities.__main__ import Main, load_app_config_dict
@@ -20,6 +25,8 @@ from modalities.running_env.cuda_env import CudaEnv
 # CUDA_VISIBLE_DEVICES=0,1 torchrun --rdzv-endpoint localhost:29502 --nnodes 1 --nproc_per_node 2 \
 #   $(which pytest) path/to/test_fsdp_to_disc_checkpointing.py
 
+# NOTE that we can only run one test at time due to NCCL issues with multiple tests in parallel.
+# You can specify the test to run with the -k flag, e.g.: -k test_warm_start
 
 _ROOT_DIR = Path(__file__).parents[1]
 
@@ -41,34 +48,55 @@ class TrainDataloaderInstantiationModel(BaseModel):
     train_dataloader: PydanticLLMDataLoaderIFType
 
 
-# @pytest.mark.skipif(
-#     "RANK" not in os.environ or torch.cuda.device_count() < 2,
-#     reason="This e2e test requires 2 GPUs and a torchrun distributed environment.",
-# )
+@pytest.mark.skipif(
+    "RANK" not in os.environ or torch.cuda.device_count() < 2,
+    reason="This e2e test requires 2 GPUs and a torchrun distributed environment.",
+)
 class TestWarmstart:
+    @staticmethod
+    def get_loss_scores(messages: List[Message[EvaluationResultBatch]], loss_key: str) -> List[float]:
+        return [message.payload.losses[loss_key].item() for message in messages]
+
     def test_warm_start(self):
         with tempfile.TemporaryDirectory() as temp_dir:
             # config for two steps model
-            gpt2_two_steps_config_file_path = Path("tests/end2end_tests/gpt2_two_steps.yaml")
+            gpt2_two_steps_config_file_path = Path("tests/end2end_tests/gpt2_train_num_steps_8.yaml")
             gpt2_two_steps_config_dict = load_app_config_dict(gpt2_two_steps_config_file_path)
 
             # adopt the checkpoint path
+            checkpoint_path = temp_dir  # "/raid/s3/opengptx/max_lue/modalities/data/checkpoints"
+            experiment_id_1 = "0"
             gpt2_two_steps_config_dict["checkpointing"]["config"]["checkpointing_execution"]["config"][
                 "checkpoint_path"
-            ] = str(Path(temp_dir))
-            gpt2_two_steps_config_dict["settings"]["experiment_id"] = "0"
+            ] = checkpoint_path
+            gpt2_two_steps_config_dict["checkpointing"]["config"]["checkpointing_execution"]["config"][
+                "experiment_id"
+            ] = experiment_id_1
+            gpt2_two_steps_config_dict["settings"]["paths"]["checkpointing_path"] = checkpoint_path
+            gpt2_two_steps_config_dict["settings"]["experiment_id"] = experiment_id_1
+            loss_values_1_path = checkpoint_path + "/0/loss_scores.txt"
 
             # config for one step model
-            gpt2_warm_start_from_step_1_config_file_path = Path("tests/end2end_tests/gpt2_warm_start_from_step_1.yaml")
+            gpt2_warm_start_from_step_1_config_file_path = Path("tests/end2end_tests/gpt2_warm_start_from_step_4.yaml")
             gpt2_warm_start_from_step_1_dict = load_app_config_dict(gpt2_warm_start_from_step_1_config_file_path)
 
             # adopt the checkpoint path
+            experiment_id_2 = "1"
             gpt2_warm_start_from_step_1_dict["checkpointing"]["config"]["checkpointing_execution"]["config"][
                 "checkpoint_path"
-            ] = (str(Path(temp_dir)) + "/0/")
-            gpt2_two_steps_config_dict["wrapped_model"]["config"]["checkpoint_path"] = str(Path(temp_dir)) + "/0/"
+            ] = checkpoint_path
+            gpt2_warm_start_from_step_1_dict["checkpointing"]["config"]["checkpointing_execution"]["config"][
+                "experiment_id"
+            ] = experiment_id_2
+            gpt2_warm_start_from_step_1_dict["wrapped_model"]["config"]["checkpoint_path"] = (
+                checkpoint_path + "/0/eid_0-model-num_steps_4.bin"
+            )
+            gpt2_warm_start_from_step_1_dict["settings"]["paths"]["checkpointing_path"] = checkpoint_path
+            gpt2_warm_start_from_step_1_dict["settings"]["experiment_id"] = experiment_id_2
+            loss_values_2_path = checkpoint_path + "/1/loss_scores.txt"
 
             main_obj_1 = Main(gpt2_two_steps_config_dict, gpt2_two_steps_config_file_path)
+
             with CudaEnv(process_group_backend=ProcessGroupBackendType.nccl):
                 main_obj_1.add_custom_component(
                     component_key="results_subscriber",
@@ -79,8 +107,16 @@ class TestWarmstart:
                 components_1 = main_obj_1.build_components(components_model_type=TrainingComponentsInstantiationModel)
                 main_obj_1.run(components_1)
 
-            main_obj_2 = Main(gpt2_warm_start_from_step_1_dict, gpt2_warm_start_from_step_1_config_file_path)
-            with CudaEnv(process_group_backend=ProcessGroupBackendType.nccl):
+                # we collect the loss values from rank 0 and store them in the temporary experiment folder
+                rank_1 = dist.get_rank()
+                if rank_1 == 0:
+                    messages_1: List[Message[EvaluationResultBatch]] = components_1.evaluation_subscriber.message_list
+                    loss_scores_1 = TestWarmstart.get_loss_scores(messages_1, "CLMCrossEntropyLoss interval average")
+                    with open(loss_values_1_path, "w") as f:
+                        json.dump(loss_scores_1, f)
+
+                main_obj_2 = Main(gpt2_warm_start_from_step_1_dict, gpt2_warm_start_from_step_1_config_file_path)
+
                 main_obj_2.add_custom_component(
                     component_key="results_subscriber",
                     variant_key="save_all",
@@ -90,25 +126,36 @@ class TestWarmstart:
                 components_2 = main_obj_2.build_components(components_model_type=TrainingComponentsInstantiationModel)
                 main_obj_2.run(components_2)
 
-            messages_1: List[Message[EvaluationResultBatch]] = components_1.evaluation_subscriber.message_list
-            messages_2: List[Message[EvaluationResultBatch]] = components_2.evaluation_subscriber.message_list
+                # we collect the loss values from rank 0 for the warmstart model
+                # and store them in the temporary experiment folder
+                rank_2 = dist.get_rank()
+                if rank_2 == 0:
+                    messages_2: List[Message[EvaluationResultBatch]] = components_2.evaluation_subscriber.message_list
+                    loss_scores_2 = TestWarmstart.get_loss_scores(messages_2, "CLMCrossEntropyLoss interval average")
+                    with open(loss_values_2_path, "w") as f:
+                        json.dump(loss_scores_2, f)
 
-            assert (
-                messages_1[-1].payload.losses["CLMCrossEntropyLoss last batch"].item()
-                == messages_2[-1].payload.losses["CLMCrossEntropyLoss last batch"].item()
-            )
-            # messages_1: List[Message[EvaluationResultBatch]] = components_1.evaluation_subscriber.message_list
-            # expected_values = {'CLMCrossEntropyLoss interval average': 10.7891}
-            # for key, expected in expected_values.items():
-            #     assert key in messages_1[-1].payload.losses, f"Key '{key}' not found in observed values"
-            #     observed = messages_1[-1].payload.losses[key]
-            #     assert observed == expected, f"AssertionError: {observed} != {expected} for key '{key}'"
+                    # read the losses from disc
+                    # note that the temporary directory is only correct for the rank 0.
+                    # rank 1 has a different one and we don't store anything in there
+                    with open(loss_values_1_path, "r") as f:
+                        loaded_loss_values_1 = json.load(f)
+
+                    with open(loss_values_2_path, "r") as f:
+                        loaded_loss_values_2 = json.load(f)
+
+                    # we check if the losses for the model from scratch
+                    # and the warm start model have the same loss values
+                    global_num_seen_steps = gpt2_warm_start_from_step_1_dict["settings"]["training"][
+                        "global_num_seen_steps"
+                    ]
+                    assert loaded_loss_values_1[global_num_seen_steps:] == loaded_loss_values_2
 
     def test_warmstart_dataloader(self):
-        gpt2_two_steps_config_file_path = Path("tests/end2end_tests/gpt2_two_steps.yaml")
+        gpt2_two_steps_config_file_path = Path("tests/end2end_tests/gpt2_train_num_steps_8.yaml")
         gpt2_two_steps_config_dict = load_app_config_dict(gpt2_two_steps_config_file_path)
 
-        gpt2_warm_start_from_step_1_config_file_path = Path("tests/end2end_tests/gpt2_warm_start_from_step_1.yaml")
+        gpt2_warm_start_from_step_1_config_file_path = Path("tests/end2end_tests/gpt2_warm_start_from_step_4.yaml")
         gpt2_warm_start_from_step_1_dict = load_app_config_dict(gpt2_two_steps_config_file_path)
 
         main_obj_1 = Main(gpt2_two_steps_config_dict, gpt2_two_steps_config_file_path)
@@ -125,7 +172,6 @@ class TestWarmstart:
             dataloader_1: LLMDataLoader = components_1.train_dataloader
             dl_1_samples = [s for s in dataloader_1]
 
-        with CudaEnv(process_group_backend=ProcessGroupBackendType.nccl):
             main_obj_2.add_custom_component(
                 component_key="results_subscriber",
                 variant_key="save_all",
@@ -139,5 +185,12 @@ class TestWarmstart:
             # fast forward the first dataloader
             num_skip_steps = dataloader_2.fast_forward_batch_id
 
+            # iterate through both sample lists from the dataloaders
+            # and assert the equality of the samples
             for i in range(len(dataloader_2)):
-                assert dl_1_samples[i + num_skip_steps]["input_ids"].equal(dl_2_samples[i]["input_ids"])
+                assert dl_1_samples[i + num_skip_steps].samples["input_ids"].equal(dl_2_samples[i].samples["input_ids"])
+
+                dl_1_samples[i + num_skip_steps].samples["input_ids"][-1] = 0
+                assert not (
+                    dl_1_samples[i + num_skip_steps].samples["input_ids"].equal(dl_2_samples[i].samples["input_ids"])
+                )
