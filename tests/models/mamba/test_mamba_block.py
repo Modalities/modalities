b4409d419fb5b3187f387b50f080802e8dbc97c2
ajude <alex.jude@iais.fraunhofer.de>
2024-04-24 16:03:12 +0200

    test(mamba): Added tests for mamba.

--- tests/models/mamba/test_mamba_block.py
+++ tests/models/mamba/test_mamba_block.py
@@ -1,6 +1,6 @@
 import torch
 
-from modalities.models.mamba.mamba_block import MambaBlock
+from modalities.models.mamba.mamba_block import MambaBlock, Block
 import pytest
 from mamba_ssm.utils.generation import InferenceParams
 
@@ -46,8 +46,18 @@ def ssm_state(batch_size, expansion_factor, d_model, d_state):
 
 
 @pytest.fixture()
-def model(d_model, d_state, d_conv, expansion_factor):
-    return MambaBlock(d_model=d_model, d_state=d_state, d_conv=d_conv, expand=expansion_factor)
+def layer_idx():
+    return 0
+
+
+@pytest.fixture()
+def mamba_block(d_model, d_state, d_conv, expansion_factor, layer_idx):
+    return MambaBlock(d_model=d_model, d_state=d_state, d_conv=d_conv, expand=expansion_factor, layer_idx=layer_idx)
+
+
+@pytest.fixture()
+def block(d_model, mamba_block):
+    return Block(dim=d_model, mixer_cls=MambaBlock)
 
 
 @pytest.fixture()
@@ -57,46 +67,55 @@ def hidden_states(d_model,
 
 
 @pytest.mark.skipif(not torch.cuda.is_available(), reason="We need cuda to run Mamba.")
-def test_forward(batch_size,
-                 sequence_length,
-                 d_model,
-                 d_state,
-                 d_conv,
-                 expansion_factor,
-                 model):
+def test_mamba_block_forward(batch_size,
+                             sequence_length,
+                             d_model,
+                             d_state,
+                             d_conv,
+                             expansion_factor,
+                             mamba_block):
     x = torch.randn(batch_size, sequence_length, d_model).to("cuda")
-    model = model.to("cuda")
-    y = model(x)
+    mamba_block = mamba_block.to("cuda")
+    y = mamba_block(x)
     assert y.shape == x.shape
 
 
 @pytest.mark.skipif(not torch.cuda.is_available(), reason="We need cuda to run Mamba.")
+def test_block_forward(hidden_states, block):
+    block = block.to("cuda")
+    hidden_states = hidden_states.to("cuda")
+    computed_hidden_states, computed_residuals = block(hidden_states)
+    assert (hidden_states == computed_residuals).all()
+    assert hidden_states.shape == computed_hidden_states.shape
+    assert (hidden_states != computed_hidden_states).any()
+
+
 def test_get_states_from_cache(conv_state,
-                                  ssm_state,
-                                  batch_size,
-                                  expansion_factor,
-                                  d_model,
-                                  d_state,
-                                  d_conv,
-                                  model
-                                  ):
+                               ssm_state,
+                               batch_size,
+                               expansion_factor,
+                               d_model,
+                               d_state,
+                               d_conv,
+                               mamba_block,
+                               layer_idx
+                               ):
     inference_params = InferenceParams(max_seqlen=16, max_batch_size=3, seqlen_offset=0, batch_size_offset=0,
-                                       key_value_memory_dict={7: (conv_state, ssm_state)},
+                                       key_value_memory_dict={layer_idx: (conv_state, ssm_state)},
                                        lengths_per_sample=None)
-    model = model.to("cuda")
-    computed_conv_state, computed_ssm_state = model._get_states_from_cache(inference_params, batch_size)
+    computed_conv_state, computed_ssm_state = mamba_block._get_states_from_cache(inference_params, batch_size)
     assert (conv_state == computed_conv_state).all()
     assert (ssm_state == computed_ssm_state).all()
 
 
 @pytest.mark.skipif(not torch.cuda.is_available(), reason="We need cuda to run Mamba.")
-def test_step(conv_state, ssm_state, model, hidden_states):
+def test_step(conv_state, ssm_state, mamba_block, hidden_states):
     device = "cuda"
-    model = model.to(device)
+    mamba_block = mamba_block.to(device)
     hidden_states = hidden_states.to(device)
     conv_state = conv_state.to(device)
     ssm_state = ssm_state.to(device)
-    computed_hidden_states, computed_conv_state, computed_ssm_state = model.step(
+    computed_hidden_states, computed_conv_state, computed_ssm_state = mamba_block.step(
         hidden_states=hidden_states.detach().clone(),
         conv_state=conv_state.detach().clone(),
         ssm_state=ssm_state.detach().clone())
@@ -108,12 +127,11 @@ def test_step(conv_state, ssm_state, model, hidden_states):
     assert (computed_ssm_state != ssm_state).any()
 
 
-def test_allocate_inference_cache(model, batch_size, sequence_length, conv_state, ssm_state):
+def test_allocate_inference_cache(mamba_block, batch_size, sequence_length, conv_state, ssm_state):
     device = "cuda"
-    model.to(device)
-    computed_conv_state, computed_ssm_state = model.allocate_inference_cache(batch_size=batch_size, max_seqlen=sequence_length,
-                                                           dtype=torch.float32)
+    mamba_block.to(device)
+    computed_conv_state, computed_ssm_state = mamba_block.allocate_inference_cache(batch_size=batch_size,
+                                                                                   max_seqlen=sequence_length,
+                                                                                   dtype=torch.float32)
     assert (computed_conv_state == torch.zeros(conv_state.shape).to(device)).all()
     assert (computed_ssm_state == torch.zeros(ssm_state.shape).to(device)).all()
-
-
---
+++ tests/models/mamba/test_mamba_model.py
@@ -0,0 +1,110 @@
+import pytest
+import torch
+from torch import nn
+
+from modalities.models.mamba.mamba_model import MixerModel, _init_weights, MambaLLM, create_block
+
+
+@pytest.fixture()
+def batch_size():
+    return 2
+
+
+@pytest.fixture()
+def vocab_size():
+    return 1024
+
+
+@pytest.fixture()
+def sequence_length():
+    return 64
+
+
+@pytest.fixture()
+def n_layer():
+    return 2
+
+
+@pytest.fixture()
+def d_model():
+    return 12
+
+
+@pytest.fixture()
+def ssm_config():
+    return {}
+
+
+@pytest.fixture()
+def mixer_model(d_model, n_layer, vocab_size):
+    return MixerModel(d_model, n_layer, vocab_size)
+
+
+@pytest.fixture()
+def prediction_key():
+    return "logits"
+
+
+@pytest.fixture()
+def mamba_llm(d_model, n_layer, vocab_size, prediction_key):
+    return MambaLLM(d_model=d_model, n_layer=n_layer, vocab_size=vocab_size, ssm_cfg={}, rms_norm=True,
+                    residual_in_fp32=False, fused_add_norm=False, pad_vocab_size_multiple=1, tie_embeddings=False,
+                    prediction_key=prediction_key)
+
+
+@pytest.fixture()
+def linear_layer():
+    return nn.Linear(in_features=16, out_features=24)
+
+
+@pytest.fixture()
+def embedding_layer(vocab_size, d_model):
+    return nn.Embedding(num_embeddings=vocab_size, embedding_dim=d_model)
+
+
+@pytest.mark.skipif(not torch.cuda.is_available(), reason="We need cuda to run Mamba.")
+def test_mixer_model_forward(batch_size, sequence_length, vocab_size, mixer_model, d_model):
+    x = torch.randint(size=(batch_size, sequence_length), high=vocab_size).to("cuda")
+    mixer_model = mixer_model.to("cuda")
+    y = mixer_model(x)
+    assert y.shape == (batch_size, sequence_length, d_model)
+    assert y.shape != x.shape
+
+
+@pytest.mark.skipif(not torch.cuda.is_available(), reason="We need cuda to run Mamba.")
+def test_mixer_model_allocate_inference_cache(batch_size, sequence_length, mixer_model, n_layer):
+    mixer_model = mixer_model.to("cuda")
+    computed_inference_cache = mixer_model.allocate_inference_cache(batch_size, sequence_length)
+    assert len(computed_inference_cache) == n_layer
+
+
+def test__init_weights(linear_layer, embedding_layer, n_layer):
+    _init_weights(linear_layer, n_layer)
+    assert int(linear_layer.bias.sum()) == 0
+
+    embedding_layer_weights_before = embedding_layer.weight.clone().detach()
+    _init_weights(embedding_layer, n_layer)
+    embedding_layer_weights_after = embedding_layer.weight
+    assert (embedding_layer_weights_before != embedding_layer_weights_after).any()
+
+
+def test_mamba_llm_forward(mamba_llm, batch_size, sequence_length, vocab_size, prediction_key):
+    mamba_llm = mamba_llm.to("cuda")
+    x = torch.randint(size=(batch_size, sequence_length), high=vocab_size).to("cuda")
+    inputs = {"input_ids": x}
+    y = mamba_llm(inputs)
+    assert prediction_key in y.keys()
+    assert y[prediction_key].shape == (batch_size, sequence_length, vocab_size)
+
+
+def test__create_block(d_model):
+    test_block = create_block(d_model=d_model)
+    assert test_block.norm.normalized_shape[0] == d_model
+    assert test_block.mixer.d_model == d_model
+
+
+def test_tie_weights(mamba_llm):
+    assert (mamba_llm.lm_head.weight != mamba_llm.backbone.embedding.weight).any()
+    mamba_llm.tie_embeddings = True
+    mamba_llm.tie_weights()
+    assert (mamba_llm.lm_head.weight == mamba_llm.backbone.embedding.weight).all()
