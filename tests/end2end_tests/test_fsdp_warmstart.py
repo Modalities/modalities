1ee4d19edea8449465e71e83e9d0fc6369c19a87
Max Luebbering <le1nux@users.noreply.github.com>
2024-04-29 18:28:22 +0200

    fix: failing tests

--- tests/end2end_tests/test_fsdp_warmstart.py
+++ tests/end2end_tests/test_fsdp_warmstart.py
@@ -11,11 +11,8 @@ from pydantic import BaseModel
 
 from modalities.__main__ import Main, load_app_config_dict
 from modalities.batch import EvaluationResultBatch
-from modalities.config.config import (
-    ProcessGroupBackendType,
-    PydanticLLMDataLoaderIFType,
-    TrainingComponentsInstantiationModel,
-)
+from modalities.config.config import ProcessGroupBackendType, PydanticLLMDataLoaderIFType
+from modalities.config.instantiation_models import TrainingComponentsInstantiationModel
 from modalities.dataloader.dataloader import LLMDataLoader
 from modalities.logging_broker.messages import Message
 from modalities.logging_broker.subscriber import MessageSubscriberIF
