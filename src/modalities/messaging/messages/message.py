32c6647f1f8e15238df1cd0bde024cd253726f59
Max Luebbering <le1nux@users.noreply.github.com>
2024-05-10 18:56:06 +0200

    refactor: removed unused MessageTypes

--- src/modalities/messaging/messages/message.py
+++ src/modalities/messaging/messages/message.py
@@ -5,12 +5,9 @@ from typing import Generic, TypeVar
 
 class MessageTypes(Enum):
     FORWARD_BACKWARD_PASS_STATE = "FORWARD_BACKWARD_PASS_STATE"
-
-    HIGH_LEVEL_PROGRESS_UPDATE = "HIGH_LEVEL_PROGRESS_UPDATE"
     BATCH_PROGRESS_UPDATE = "PROGRESS_UPDATE"
-    ERROR_MESSAGE = "ERROR_MESSAGE"
-    EVALUATION_RESULT = "EVALUATION_RESULT"
     MODEL_STATE = "MODEL_STATE"
+    EVALUATION_RESULT = "EVALUATION_RESULT"
 
 
 T = TypeVar("T")
@@ -20,7 +17,7 @@ T = TypeVar("T")
 class Message(Generic[T]):
     """An object representing a message."""
 
-    message_type: MessageTypes
+    message_type: Enum
     payload: T
     global_rank: int = 0
     local_rank: int = 0
