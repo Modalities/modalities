826ef2fafc201ff7c1690bdb518ddbb1716e96e0
Max Luebbering <le1nux@users.noreply.github.com>
2024-05-12 00:44:43 +0200

    feat: added batch progress update procesor

---
+++ src/modalities/messaging/evaluation/processors/batch_progress_update_processors.py
@@ -0,0 +1,11 @@
+from modalities.messaging.evaluation.processors.processors import LocalProcessorIF
+from modalities.messaging.evaluation.states import IntervalState
+from modalities.messaging.messages.payloads import BatchProgressUpdate
+
+
+class BatchProgressUpdateProcessor(LocalProcessorIF[BatchProgressUpdate]):
+    def __init__(self):
+        pass
+
+    def process(self, payload: BatchProgressUpdate, current_local_step_state: IntervalState):
+        current_local_step_state.meta_information = payload
