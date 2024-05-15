deefe75d0826d031806594298aec0ed51a744f00
Max Luebbering <le1nux@users.noreply.github.com>
2024-05-15 18:54:26 +0200

    refactor: first optimizer changes towards improved loss stability

--- src/modalities/optimizers/optimizer_factory.py
+++ src/modalities/optimizers/optimizer_factory.py
@@ -1,6 +1,7 @@
-from typing import Tuple
+from typing import Dict, List, Tuple
 
 import torch.nn as nn
+from torch.distributed.fsdp import FullyShardedDataParallel as FSDP
 from torch.optim import Adam, AdamW, Optimizer
 
 from modalities.checkpointing.checkpoint_loading import CheckpointLoadingIF
@@ -19,8 +20,10 @@ class OptimizerFactory:
     def get_adam_w(
         lr: float, betas: Tuple[float, float], eps: float, weight_decay: float, wrapped_model: nn.Module
     ) -> Optimizer:
-        model_parameters = wrapped_model.parameters()
-        optimizer = AdamW(params=model_parameters, lr=lr, betas=betas, eps=eps, weight_decay=weight_decay)
+        # model_parameters = wrapped_model.parameters()
+        model_parameters = get_parameter_groups(wrapped_model, weight_decay)
+        optimizer = AdamW(params=model_parameters, lr=lr, betas=betas, eps=eps)
+        # optimizer = AdamW(params=wrapped_model.parameters(), lr=lr, betas=betas, eps=eps, weight_decay=weight_decay)
         return optimizer
 
     @staticmethod
@@ -31,3 +34,33 @@ class OptimizerFactory:
             file_path=checkpoint_path, optimizer=optimizer, model=wrapped_model
         )
         return wrapped_optimizer
+
+
+@staticmethod
+def get_parameter_groups(model: FSDP, weight_decay: float) -> List[Dict[str, List[nn.Parameter] | float]]:
+    # start with all of the candidate parameters
+    param_dict = {pn: p for pn, p in model.named_parameters()}
+    # filter out those that do not require grad
+    param_dict = {pn: p for pn, p in param_dict.items() if p.requires_grad}
+    # create optim groups. Any parameters that is 2D will be weight decayed, otherwise no.
+    # i.e. all weight tensors in matmuls + embeddings decay, all biases and layernorms don't.
+    # decay_params = [p for n, p in param_dict.items() if p.dim() >= 2]
+    # nodecay_params = [p for n, p in param_dict.items() if p.dim() < 2]
+    nodecay_params = [parameter for name, parameter in param_dict.items() if "norm" in name]
+    decay_params = [parameter for name, parameter in param_dict.items() if "norm" not in name]
+
+    optim_groups = [
+        {"params": decay_params, "weight_decay": weight_decay},
+        {"params": nodecay_params, "weight_decay": 0.0},
+    ]
+    num_decay_params = sum(p.numel() for p in decay_params)
+    num_nodecay_params = sum(p.numel() for p in nodecay_params)
+    print(f"num decayed parameter tensors: {len(decay_params)}, with {num_decay_params:,} parameters")
+    print(f"num non-decayed parameter tensors: {len(nodecay_params)}, with {num_nodecay_params:,} parameters")
+    # Create AdamW optimizer and use the fused version if it is available
+    # fused_available = 'fused' in inspect.signature(torch.optim.AdamW).parameters
+    # use_fused = fused_available and device_type == 'cuda'
+    # extra_args = {} # dict(fused=True) if use_fused else dict()
+    # optimizer = torch.optim.AdamW(optim_groups, lr=learning_rate, betas=betas, **extra_args)
+    # print(f"using fused AdamW: {use_fused}")
+    return optim_groups
