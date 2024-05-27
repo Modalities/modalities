c96b8fb3dd64feac26de66bb0e174fee88d71d21
Felix Stollenwerk <felix.stollenwerk@ai.se>
2024-05-27 13:06:21 +0200

    feat(attention): support group query attention for manual and pytorch_flash

--- tests/models/test_causal_self_attention.py
+++ tests/models/test_causal_self_attention.py
@@ -7,7 +7,7 @@ from copy import deepcopy
 import pytest
 import torch
 
-from modalities.models.gpt2.gpt2_model import AttentionConfig, CausalSelfAttention
+from modalities.models.gpt2.gpt2_model import AttentionConfig, CausalSelfAttention, repeat_kv_heads
 
 torch.manual_seed(0)
 
@@ -35,23 +35,62 @@ def _get_random_attention_layer(n_head_q, n_head_kv, n_embd, block_size, attenti
     return self_attention_layer
 
 
+@pytest.mark.parametrize(
+    "n_head_q, n_head_kv, n_embd",
+    [
+        (4, 4, 32),  # MHA (multi head attention)
+        (32, 32, 768),  # MHA (multi head attention)
+        (4, 2, 32),  # GQA (group query attention)
+        (6, 2, 32),  # GQA
+        (32, 4, 768),  # GQA
+    ],
+)
+def test_repeat_kv_heads(n_head_q, n_head_kv, n_embd):
+    batch_size = 2
+    block_size = 10
+    head_dim = n_embd // n_head_q
+    AttentionConfig(qkv_transforms=[])
+
+    q = torch.rand(batch_size, n_head_q, block_size - 1, head_dim, dtype=torch.bfloat16).cuda()
+    k_in = torch.rand(batch_size, n_head_kv, block_size - 1, head_dim, dtype=torch.bfloat16).cuda()
+    v_in = torch.rand(batch_size, n_head_kv, block_size - 1, head_dim, dtype=torch.bfloat16).cuda()
+
+    k_out, v_out = repeat_kv_heads(q, k_in, v_in)
+
+    # assert that shapes are correct: (batch_size, num_heads, seq_length, head_dim)
+    assert k_out.shape == q.shape
+    assert v_out.shape == q.shape
+
+    # assert that repetitions are correct
+    if n_head_q != n_head_kv:
+        # e.g. n_head_q = 6, n_head_kv = 2
+        for i in range(0, n_head_q, n_head_q // n_head_kv):  # e.g. i = 0,3
+            for j in range(1, n_head_q // n_head_kv):  # e.g. j = 1,2
+                torch.testing.assert_close(
+                    k_out[:, i, :, :], k_out[:, i + j, :, :]
+                )  # compares i=0 vs. i+j=1,2 | i=3 vs. i+j=4,5
+                torch.testing.assert_close(
+                    v_out[:, i, :, :], v_out[:, i + j, :, :]
+                )  # compares i=0 vs. i+j=1,2 | i=3 vs. i+j=4,5
+
+
 @pytest.mark.skipif(torch.cuda.device_count() < 1, reason="This e2e test requires 1 GPU.")
 @pytest.mark.parametrize(
     "n_head_q, n_head_kv, n_embd, attention_impl, successful",
     [
         # manual
-        (4, 4, 32, "manual", True),
-        (8, 2, 32, "manual", False),  # group query attention not implemented for manual attention
+        (4, 4, 32, "manual", True),  # MHA
+        (8, 2, 32, "manual", True),  # GQA
         (9, 8, 32, "manual", False),
         (8, 3, 32, "manual", False),
         # pytorch_flash
-        (4, 4, 32, "pytorch_flash", True),
-        (8, 2, 32, "pytorch_flash", False),  # group query attention not implemented for pytorch_flash attention
+        (4, 4, 32, "pytorch_flash", True),  # MHA
+        (8, 2, 32, "pytorch_flash", True),  # GQA
         (9, 8, 32, "pytorch_flash", False),
         (8, 3, 32, "pytorch_flash", False),
         # dao_flash
-        (4, 4, 32, "dao_flash", True),
-        (8, 2, 32, "dao_flash", True),
+        (4, 4, 32, "dao_flash", True),  # MHA
+        (8, 2, 32, "dao_flash", True),  # GQA
         (9, 8, 32, "dao_flash", False),
         (8, 3, 32, "dao_flash", False),
     ],
@@ -85,15 +124,17 @@ def test_forward_pass_success(n_head_q, n_head_kv, n_embd, attention_impl, succe
     "seq_length, n_head_q, n_head_kv, head_dim, attention_impl",
     [
         # manual
-        (12, 4, 4, 32, "manual"),
-        (16, 8, 8, 16, "manual"),
+        (12, 4, 4, 32, "manual"),  # MHA
+        (12, 8, 2, 32, "manual"),  # GQA
+        (16, 8, 8, 16, "manual"),  # MHA
         # pytorch_flash
-        (12, 4, 4, 32, "pytorch_flash"),
-        (16, 8, 8, 16, "pytorch_flash"),
+        (12, 4, 4, 32, "pytorch_flash"),  # MHA
+        (12, 8, 2, 32, "pytorch_flash"),  # GQA
+        (16, 8, 8, 16, "pytorch_flash"),  # MHA
         # dao_flash
-        (12, 4, 4, 32, "dao_flash"),
-        (12, 8, 2, 32, "dao_flash"),  # group query attention
-        (16, 8, 8, 16, "dao_flash"),
+        (12, 4, 4, 32, "dao_flash"),  # MHA
+        (12, 8, 2, 32, "dao_flash"),  # GQA
+        (16, 8, 8, 16, "dao_flash"),  # MHA
     ],
 )
 def test_forward_pass_shapes(seq_length, n_head_q, n_head_kv, head_dim, attention_impl):
@@ -120,22 +161,24 @@ def test_forward_pass_shapes(seq_length, n_head_q, n_head_kv, head_dim, attentio
 @pytest.mark.parametrize(
     "n_head_q, n_head_kv, n_embd, attention_impl_1, attention_impl_2, verbose",
     [
-        # note that no group query attention is used (i.e. n_head_q == n_head_kv)
         # manual vs. pytorch_flash
-        (4, 4, 4, "manual", "pytorch_flash", True),
+        (4, 4, 4, "manual", "pytorch_flash", False),  # MHA
         (4, 4, 32, "manual", "pytorch_flash", False),
         (4, 4, 768, "manual", "pytorch_flash", False),
         (8, 8, 2048, "manual", "pytorch_flash", False),
+        (8, 2, 2048, "manual", "pytorch_flash", False),  # GQA
         # manual vs. dao_flash
-        (4, 4, 4, "manual", "dao_flash", False),
+        (4, 4, 4, "manual", "dao_flash", False),  # MQA
         (4, 4, 32, "manual", "dao_flash", False),
         (4, 4, 768, "manual", "dao_flash", False),
         (8, 8, 2048, "manual", "dao_flash", False),
+        (8, 2, 2048, "manual", "dao_flash", False),  # GQA
         # pytorch_flash vs. dao_flash
         (4, 4, 4, "pytorch_flash", "dao_flash", False),
         (4, 4, 32, "pytorch_flash", "dao_flash", False),
         (4, 4, 768, "pytorch_flash", "dao_flash", False),
         (8, 8, 2048, "pytorch_flash", "dao_flash", False),
+        (8, 2, 2048, "pytorch_flash", "dao_flash", False),  # GQA
     ],
 )
 def test_attention_implementation_approximate_equality(
@@ -174,6 +217,6 @@ def test_attention_implementation_approximate_equality(
     torch.testing.assert_close(
         output_tensor[attention_impl_1],
         output_tensor[attention_impl_2],
-        atol=2e-3,  # default for bfloat16: 1e-5
+        atol=2.5e-3,  # default for bfloat16: 1e-5
         rtol=0.016,  # default for bfloat16: 0.016
     )
