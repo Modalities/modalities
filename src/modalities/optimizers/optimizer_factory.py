6cdcbbad9ac248f784dd10ead2003bc0c887cf54
Felix Stollenwerk <felix.stollenwerk@ai.se>
2024-05-29 12:05:19 +0200

    fix(gpt2_model): apply parameter groups to adam + cleanup

--- src/modalities/optimizers/optimizer_factory.py
+++ src/modalities/optimizers/optimizer_factory.py
@@ -12,18 +12,16 @@ class OptimizerFactory:
     def get_adam(
         lr: float, betas: Tuple[float, float], eps: float, weight_decay: float, wrapped_model: nn.Module
     ) -> Optimizer:
-        model_parameters = wrapped_model.parameters()
-        optimizer = Adam(params=model_parameters, lr=lr, betas=betas, eps=eps, weight_decay=weight_decay)
+        model_parameter_groups = get_parameter_groups(wrapped_model, weight_decay)
+        optimizer = Adam(params=model_parameter_groups, lr=lr, betas=betas, eps=eps)
         return optimizer
 
     @staticmethod
     def get_adam_w(
         lr: float, betas: Tuple[float, float], eps: float, weight_decay: float, wrapped_model: nn.Module
     ) -> Optimizer:
-        # model_parameters = wrapped_model.parameters()
-        model_parameters = get_parameter_groups(wrapped_model, weight_decay)
-        optimizer = AdamW(params=model_parameters, lr=lr, betas=betas, eps=eps)
-        # optimizer = AdamW(params=wrapped_model.parameters(), lr=lr, betas=betas, eps=eps, weight_decay=weight_decay)
+        model_parameter_groups = get_parameter_groups(wrapped_model, weight_decay)
+        optimizer = AdamW(params=model_parameter_groups, lr=lr, betas=betas, eps=eps)
         return optimizer
 
     @staticmethod
@@ -38,14 +36,10 @@ class OptimizerFactory:
 
 @staticmethod
 def get_parameter_groups(model: FSDP, weight_decay: float) -> List[Dict[str, List[nn.Parameter] | float]]:
-    # start with all of the candidate parameters
-    param_dict = {pn: p for pn, p in model.named_parameters()}
-    # filter out those that do not require grad
-    param_dict = {pn: p for pn, p in param_dict.items() if p.requires_grad}
-    # create optim groups. Any parameters that is 2D will be weight decayed, otherwise no.
-    # i.e. all weight tensors in matmuls + embeddings decay, all biases and layernorms don't.
-    # decay_params = [p for n, p in param_dict.items() if p.dim() >= 2]
-    # nodecay_params = [p for n, p in param_dict.items() if p.dim() < 2]
+    # filter out parameters that do not require grad
+    param_dict = {pn: p for pn, p in model.named_parameters() if p.requires_grad}
+
+    # split parameters into different groups
     nodecay_params = [parameter for name, parameter in param_dict.items() if "norm" in name]
     decay_params = [parameter for name, parameter in param_dict.items() if "norm" not in name]
 
@@ -53,14 +47,11 @@ def get_parameter_groups(model: FSDP, weight_decay: float) -> List[Dict[str, Lis
         {"params": decay_params, "weight_decay": weight_decay},
         {"params": nodecay_params, "weight_decay": 0.0},
     ]
+
+    # print number of parameters per group
     num_decay_params = sum(p.numel() for p in decay_params)
     num_nodecay_params = sum(p.numel() for p in nodecay_params)
     print(f"num decayed parameter tensors: {len(decay_params)}, with {num_decay_params:,} parameters")
     print(f"num non-decayed parameter tensors: {len(nodecay_params)}, with {num_nodecay_params:,} parameters")
-    # Create AdamW optimizer and use the fused version if it is available
-    # fused_available = 'fused' in inspect.signature(torch.optim.AdamW).parameters
-    # use_fused = fused_available and device_type == 'cuda'
-    # extra_args = {} # dict(fused=True) if use_fused else dict()
-    # optimizer = torch.optim.AdamW(optim_groups, lr=learning_rate, betas=betas, **extra_args)
-    # print(f"using fused AdamW: {use_fused}")
+
     return optim_groups
