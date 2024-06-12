4cb62b1a582bee55744ac67cddd466f49a1a9845
mali-git <mehdi.ali@iais.fraunhofer.de>
2024-06-12 14:24:07 +0200

    tests: test SwiGLU

--- tests/nn/test_mlp.py
+++ tests/nn/test_mlp.py
@@ -1,5 +1,5 @@
 import torch
-
+from modalities.models.gpt2.gpt2_model import SwiGLU
 from modalities.nn.mlp import MLP
 
 
@@ -8,3 +8,12 @@ def test_mlp_forward():
     dummy_input = torch.randn(1, 10, 64)
     out = model(dummy_input)
     assert out.shape == (1, 10, 64)
+
+def test_SwiGLU_forward():
+    n_embd = 512
+    bias = True
+    model = SwiGLU(n_embd, bias)
+    input_tensor = torch.randn(1, n_embd)
+    output_tensor = model(input_tensor)
+    assert output_tensor.shape == (1, n_embd)
+
