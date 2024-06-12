f07cfdaea45e0b826a89d4f2b922b335c8a85931
mmaurya <manas.maurya@iais.fraunhofer.de>
2024-06-12 14:04:16 +0000

    fix: disable mamba imports

--- src/modalities/registry/components.py
+++ src/modalities/registry/components.py
@@ -88,8 +88,9 @@ from modalities.models.huggingface.huggingface_models import (
     HuggingFacePretrainedModel,
     HuggingFacePretrainedModelConfig,
 )
-from modalities.models.mamba.mamba_config import MambaLLMConfig
-from modalities.models.mamba.mamba_model import MambaLLM
+
+# from modalities.models.mamba.mamba_config import MambaLLMConfig
+# from modalities.models.mamba.mamba_model import MambaLLM
 from modalities.models.model_factory import ModelFactory
 from modalities.optimizers.lr_schedulers import DummyLRScheduler
 from modalities.optimizers.optimizer_factory import OptimizerFactory
@@ -117,7 +118,7 @@ class ComponentEntity:
 COMPONENTS = [
     # models
     ComponentEntity("model", "gpt2", GPT2LLM, GPT2LLMConfig),
-    ComponentEntity("model", "mamba", MambaLLM, MambaLLMConfig),
+    # ComponentEntity("model", "mamba", MambaLLM, MambaLLMConfig),
     ComponentEntity(
         "model", "huggingface_pretrained_model", HuggingFacePretrainedModel, HuggingFacePretrainedModelConfig
     ),
