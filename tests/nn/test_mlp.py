6a65cc974acdd6cfd55a4c671c9903cfe2c32b6b
mali-git <mehdi.ali@iais.fraunhofer.de>
2024-06-17 00:30:13 +0200

    test: finalize SwiGLU test

--- tests/nn/test_mlp.py
+++ tests/nn/test_mlp.py
@@ -1,5 +1,7 @@
 import torch
 
+from torch import nn
+
 from modalities.models.model import SwiGLU
 from modalities.nn.mlp import MLP
 
@@ -13,8 +15,30 @@ def test_mlp_forward():
 def test_SwiGLU_forward():
     n_embd = 512
     bias = True
-    model = SwiGLU(n_embd, bias)
-    input_tensor = torch.randn(1, n_embd)
-    output_tensor = model(input_tensor)
-    assert output_tensor.shape == (1, n_embd)
+    mlp = SwiGLU(n_embd, bias)
+
+    hidden_dim = 1536
+    assert mlp._get_hidden_dim(n_embd) == hidden_dim
+    
+    n_embd = 511
+    assert mlp._get_hidden_dim(n_embd) == hidden_dim
+    
+    n_embd = 512
+
+    # batch size x sequence length x embedding dim
+    input_tensor = torch.randn(1, 1, n_embd)
+    output_tensor = mlp(input_tensor)
+    assert output_tensor.shape == (1, 1, n_embd)
+
+
+    c_fc = nn.Linear(in_features=n_embd, out_features=hidden_dim, bias=bias)
+    c_proj = nn.Linear(in_features=n_embd,out_features=hidden_dim,bias=bias)
+    out_proj = nn.Linear(in_features=hidden_dim, out_features=n_embd,bias=bias)
+    silu = nn.SiLU()
+    mlp.c_fc = c_fc
+    mlp.c_proj = c_proj
+    mlp.out_proj = out_proj
+
+    output_tensor = mlp(input_tensor)
+    assert torch.all(output_tensor == out_proj(silu(c_fc(input_tensor)) * c_proj(input_tensor)))
 
