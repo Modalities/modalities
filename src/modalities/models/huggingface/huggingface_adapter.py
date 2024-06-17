a79516026fccb6a9eced7c4aef621ba0a2df2fc9
ajude <alex.jude@iais.fraunhofer.de>
2024-06-17 11:19:41 +0200

    feat(checkpointing):
    - Adapted HuggingFaceAdapterModel for loading the saved HF model.

--- src/modalities/models/huggingface/huggingface_adapter.py
+++ src/modalities/models/huggingface/huggingface_adapter.py
@@ -1,6 +1,7 @@
 import json
-from abc import ABC, abstractmethod
+from abc import ABC
 from dataclasses import dataclass
+from pathlib import PosixPath
 from typing import Any, Dict, Optional, Tuple
 
 import torch
@@ -8,14 +9,8 @@ import torch.nn as nn
 from transformers import PreTrainedModel, PretrainedConfig
 from transformers.utils import ModelOutput
 
-from modalities.config.config import CheckpointedModelConfig
-from modalities.checkpointing.torch.torch_checkpoint_loading import TorchCheckpointLoading
 from modalities.models.mamba.mamba_config import MambaBlockConfig, MixerModelConfig
 from modalities.models.mamba.mamba_model import MambaLLM
-from modalities.models.model_factory import ModelFactory
-
-import os
-from pathlib import PosixPath
 
 
 class HuggingFaceAdapterConfig(ABC, PretrainedConfig):
@@ -24,24 +19,14 @@ class HuggingFaceAdapterConfig(ABC, PretrainedConfig):
     def __init__(self, config_dict=None, **kwargs):
         super().__init__(**kwargs)
         self.config_dict = config_dict
-        # TODO Iterate over all dict and change PosixPath to str
+
         if self.config_dict:
-            # self.config_dict["settings"]["config_file_path"] = str(self.config_dict["settings"]["config_file_path"])
             self.convert_posixpath_to_str(self.config_dict)
 
-    # # TODO check if this is still needed
     def to_json_string(self, use_diff: bool = True) -> str:
 
         if self.config_dict:
             json_dict = {"config": self.config_dict.copy(), "model_type": self.model_type}
-            # json_dict["config"]["attention"] = {
-            #     "attention_type": self.config.attention.attention_type.value,
-            #     "scaling_factor": self.config.attention.scaling_factor,
-            # }
-            # json_dict["config"]["weight_init"] = {
-            #     "mean": self.config.weight_init.mean,
-            #     "std": self.config.weight_init.std,
-            # }
         else:
             json_dict = {}
 
@@ -65,47 +50,49 @@ class HuggingFaceModel(PreTrainedModel):
 
     def __init__(self, config: HuggingFaceAdapterConfig, model: Optional[nn.Module] = None):
         super().__init__(config)
-        # TODO pass correct model type to __init__
 
         if not model:
-            mamba_block_config = config.config["model"]["config"]["mixer_model_config"]["mamba_block_config"]
-            mamba_block_config = MambaBlockConfig(**mamba_block_config)
-            mixer_model_config = config.config["model"]["config"]["mixer_model_config"]
-            mixer_model_config = MixerModelConfig(**mixer_model_config)
-            self.model: MambaLLM = MambaLLM(**config.config["model"]["config"])
+            self.config = self.convert_config_dict_to_pydantic(config)
+            self.model: MambaLLM = MambaLLM(**self.config)
         else:
             self.model = model
 
+    def forward(
+            self,
+            input_ids: torch.Tensor,
+            attention_mask: Optional[torch.Tensor] = None,
+            return_dict: Optional[bool] = False,
+            output_attentions: Optional[bool] = False,
+            output_hidden_states: Optional[bool] = False,
+    ):
+        if output_attentions or output_hidden_states:
+            raise NotImplementedError
+        model_input = {"input_ids": input_ids, "attention_mask": attention_mask}
+        model_forward_output: Dict[str, torch.Tensor] = self.model.forward(model_input)
+        if return_dict:
+            return ModalitiesModelOutput(**model_forward_output)
+        else:
+            return model_forward_output[self.model.prediction_key]
 
-def forward(
-        self,
-        input_ids: torch.Tensor,
-        attention_mask: Optional[torch.Tensor] = None,
-        return_dict: Optional[bool] = False,
-        output_attentions: Optional[bool] = False,
-        output_hidden_states: Optional[bool] = False,
-):
-    if output_attentions or output_hidden_states:
-        raise NotImplementedError
-    model_input = {"input_ids": input_ids, "attention_mask": attention_mask}
-    model_forward_output: Dict[str, torch.Tensor] = self.model.forward(model_input)
-    if return_dict:
-        return ModalitiesModelOutput(**model_forward_output)
-    else:
-        return model_forward_output[self.model.prediction_key]
+    def prepare_inputs_for_generation(
+            self, input_ids: torch.LongTensor, attention_mask=None, **kwargs
+    ) -> Dict[str, Any]:
+        """
+        Implement in subclasses of :class:`~transformers.PreTrainedModel` for custom behavior to prepare inputs in the
+        generate method.
+        """
+        return {
+            "input_ids": input_ids,
+            "attention_mask": attention_mask,
+        }
 
+    def convert_config_dict_to_pydantic(self, config):
+        config.config["model"]["config"]["mixer_model_config"]["mamba_block_config"] = MambaBlockConfig(
+            **config.config["model"]["config"]["mixer_model_config"]["mamba_block_config"])
+        config.config["model"]["config"]["mixer_model_config"] = MixerModelConfig(
+            **config.config["model"]["config"]["mixer_model_config"])
 
-def prepare_inputs_for_generation(
-        self, input_ids: torch.LongTensor, attention_mask=None, **kwargs
-) -> Dict[str, Any]:
-    """
-    Implement in subclasses of :class:`~transformers.PreTrainedModel` for custom behavior to prepare inputs in the
-    generate method.
-    """
-    return {
-        "input_ids": input_ids,
-        "attention_mask": attention_mask,
-    }
+        return config.config["model"]["config"]
 
 
 @dataclass
