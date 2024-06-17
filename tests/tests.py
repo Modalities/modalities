563cf27bd1f5bca5fe83caf25fd8f52b68f44421
Felix Stollenwerk <felix.stollenwerk@ai.se>
2024-06-17 15:23:19 +0200

    refactor(test): use script instead of CLI for unified testing

---
+++ tests/tests.py
@@ -0,0 +1,72 @@
+import argparse
+import subprocess
+from os.path import isfile
+from pathlib import Path
+
+_ROOT_DIR = Path(__file__).parents[1]
+
+
+def main(cpu: bool = False, single_gpu: bool = False, multi_gpu: bool = False, devices: str = "0,1"):
+    """
+    Run tests on cpu, single gpu and multiple gpus
+
+    Examples:
+    modalities test:                           run CPU tests + single-GPU tests on GPU 0 + multi-GPU tests on GPUs 0/1
+    modalities test --devices 4,5:             run CPU tests + single-GPU tests on GPU 4 + multi-GPU tests on GPUs 4/5
+    modalities test --cpu:                     run CPU tests
+    modalities test --single-gpu --devices 3:  run CPU tests + single-GPU tests on GPU 3
+    modalities test --multi-gpu --devices 3,4: run multi-GPU tests on GPUs 3/4
+    """
+    # parse input
+    if not cpu and not single_gpu and not multi_gpu:  # run all tests
+        cpu, single_gpu, multi_gpu = True, True, True
+    if single_gpu:  # cpu & single_gpu are both executed together via pytest
+        cpu = True
+    try:
+        devices = [int(device) for device in devices.split(",")]  # e.g. '0,1' -> [0, 1]
+    except ValueError:
+        exit(f"ERROR! devices needs to be a string of comma-separated ints, e.g. '0,1'. Specified devices = {devices}")
+    devices = devices if len(devices) <= 2 else devices[:2]  # use max 2 devices
+
+    # check input
+    if single_gpu and len(devices) < 1:
+        exit(f"ERROR! Need at least 1 device to run single_gpu tests. Specified devices = {devices}")
+    if multi_gpu and len(devices) < 2:
+        exit(f"ERROR! Need 2 devices to run multi_gpu tests. Specified devices = {devices}")
+
+    # start
+    print(f"> TESTS ON          CPU: {cpu}")
+    print(f"> TESTS ON   SINGLE GPU: {single_gpu} " + f"(device={devices[0] if single_gpu else None})")
+    print(f"> TESTS ON MULTIPLE GPU: {multi_gpu} " + f"(devices={devices if multi_gpu else None})")
+
+    # run cpu / single-gpu tests
+    if cpu or single_gpu:
+        command_unit_tests = f"CUDA_VISIBLE_DEVICES={devices[0] if single_gpu else None} pytest"
+
+        print("\n=== RUN CPU & SINGLE-GPU TESTS ===" if single_gpu else "\n=== RUN CPU TESTS ===")
+        print(command_unit_tests)
+        subprocess.run(command_unit_tests, shell=True, capture_output=False, text=True)
+
+    # run multi-gpu tests
+    if multi_gpu:
+        run_distributed_tests_directory = _ROOT_DIR / "tests"
+        run_distributed_tests_script = _ROOT_DIR / "tests" / "run_distributed_tests.sh"
+        assert isfile(run_distributed_tests_script), f"ERROR! {run_distributed_tests_script} does not exist."
+        command_end_to_end_tests = (
+            f"cd {run_distributed_tests_directory}; bash run_distributed_tests.sh {devices[0]} {devices[1]}"
+        )
+
+        print("\n=== RUN MULTI-GPU TESTS ===")
+        print(command_end_to_end_tests)
+        subprocess.run(command_end_to_end_tests, shell=True, capture_output=False, text=True)
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Description of your program")
+    parser.add_argument("--cpu", action=argparse.BooleanOptionalAction, help="Do cpu testing.")
+    parser.add_argument("--single-gpu", action=argparse.BooleanOptionalAction, help="Do single GPU testing.")
+    parser.add_argument("--multi-gpu", action=argparse.BooleanOptionalAction, help="Do multi GPU testing.")
+    parser.add_argument("--devices", type=str, default="0,1")
+    args = vars(parser.parse_args())
+    args = {k: v if v is not None else False for k, v in args.items()}  # None -> False
+    main(**args)
