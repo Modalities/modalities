06821d6422a840f5f4584e589cb6e2c89c1d5d84
mrudat-iais <max.rudat@iais.fraunhofer.de>
2024-06-17 13:40:24 +0200

    refactor(tests): the checkpoint conversion test is now more readable

--- tests/checkpointing/test_checkpoint_conversion.py
+++ tests/checkpointing/test_checkpoint_conversion.py
@@ -56,50 +56,73 @@ def initialized_model(set_env, component_factory, config_dict):
     return components.model
 
 
-def test_entry_point_convert_pytorch_to_hf_checkpoint(
-        initialized_model,
-        config_file_path,
-        device,
-        tmp_path
-):
-    output_hf_checkpoint_dir = tmp_path / "converted_hf_checkpoint"
+@pytest.fixture()
+def checkpoint_conversion(tmp_path, initialized_model, config_file_path):
     model_file_path = tmp_path / "pytorch_model.bin"
-
     torch.save(initialized_model.state_dict(), model_file_path)
+
+    output_hf_checkpoint_dir = tmp_path / "converted_hf_checkpoint"
     checkpoint_conversion = CheckpointConversion(
         config_file_path=config_file_path,
         output_hf_checkpoint_dir=output_hf_checkpoint_dir,
     )
-
     checkpoint_conversion.config_dict["checkpointed_model"]["config"][
         "checkpoint_path"] = model_file_path
-    pytorch_model = checkpoint_conversion._setup_model()
-    hf_model = checkpoint_conversion.convert_pytorch_to_hf_checkpoint()
+    return checkpoint_conversion
 
-    # register config and model
+
+@pytest.fixture()
+def pytorch_model(checkpoint_conversion):
+    return checkpoint_conversion._setup_model()
+
+
+@pytest.fixture()
+def hf_model(checkpoint_conversion):
+    return checkpoint_conversion.convert_pytorch_to_hf_checkpoint()
+
+
+@pytest.fixture()
+def hf_model_from_checkpoint(checkpoint_conversion, pytorch_model, device):
     AutoConfig.register("modalities", HuggingFaceAdapterConfig)
     AutoModelForCausalLM.register(HuggingFaceAdapterConfig, HuggingFaceModel)
-
-    hf_model_from_checkpoint = AutoModelForCausalLM.from_pretrained(output_hf_checkpoint_dir,
+    hf_model_from_checkpoint = AutoModelForCausalLM.from_pretrained(checkpoint_conversion.output_hf_checkpoint_dir,
                                                                     torch_dtype=pytorch_model.lm_head.weight.dtype)
     hf_model_from_checkpoint = hf_model_from_checkpoint.to(device)
+    return hf_model_from_checkpoint
 
+
+@pytest.fixture()
+def test_tensor(device, size: int = 10):
+    test_tensor = torch.randint(size, size=(5, size))
+    test_tensor = test_tensor.to(device)
+    return test_tensor
+
+
+def test_hf_and_pytorch_models_are_the_same_after_init(hf_model, pytorch_model, checkpoint_conversion):
     assert hf_model.dtype == pytorch_model.lm_head.weight.dtype
     assert hf_model.__class__.__name__ == "HuggingFaceModel"
-    assert os.listdir(output_hf_checkpoint_dir)
+    assert os.listdir(checkpoint_conversion.output_hf_checkpoint_dir)
 
-    # Evaluating whether the model before and after conversion produces the same output
-    pytorch_model.eval()
-    pytorch_model = pytorch_model.to(device)
-    hf_model.eval()
-    hf_model = hf_model.to(device)
 
-    test_tensor = torch.randint(10, size=(5, 10))
-    test_tensor = test_tensor.to(device)
+def test_models_before_and_after_conversion_produce_same_output(
+        device,
+        pytorch_model,
+        hf_model,
+        hf_model_from_checkpoint,
+        test_tensor,
+):
+    pytorch_model = put_model_to_eval_mode(pytorch_model, device)
+    hf_model = put_model_to_eval_mode(hf_model, device)
+
     output_pytorch_model = pytorch_model.forward({"input_ids": test_tensor})["logits"]
-
     output_hf_model = hf_model.forward(test_tensor)
     output_hf_model_from_checkpoint = hf_model_from_checkpoint.forward(test_tensor)
 
     assert (output_hf_model == output_pytorch_model).all()
     assert (output_hf_model == output_hf_model_from_checkpoint).all()
+
+
+def put_model_to_eval_mode(model, device):
+    model.eval()
+    model = model.to(device)
+    return model
