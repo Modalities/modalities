4e579b6026b4b0d577892bf68d946af1f8aa545e
fromm-m <michael.fromm.91@googlemail.com>
2024-06-19 10:07:23 +0000

    refactor: made _get_hidden_dim for SwiGLU an static method

--- tests/nn/test_mlp.py
+++ tests/nn/test_mlp.py
@@ -1,5 +1,4 @@
 import torch
-
 from torch import nn
 
 from modalities.models.model import SwiGLU
@@ -12,17 +11,18 @@ def test_mlp_forward():
     out = model(dummy_input)
     assert out.shape == (1, 10, 64)
 
+
 def test_SwiGLU_forward():
     n_embd = 512
     bias = True
     mlp = SwiGLU(n_embd, bias)
 
     hidden_dim = 1536
-    assert mlp._get_hidden_dim(n_embd) == hidden_dim
-    
+    assert SwiGLU._get_hidden_dim(n_embd) == hidden_dim
+
     n_embd = 511
-    assert mlp._get_hidden_dim(n_embd) == hidden_dim
-    
+    assert SwiGLU._get_hidden_dim(n_embd) == hidden_dim
+
     n_embd = 512
 
     # batch size x sequence length x embedding dim
@@ -30,10 +30,9 @@ def test_SwiGLU_forward():
     output_tensor = mlp(input_tensor)
     assert output_tensor.shape == (1, 1, n_embd)
 
-
     c_fc = nn.Linear(in_features=n_embd, out_features=hidden_dim, bias=bias)
-    c_proj = nn.Linear(in_features=n_embd,out_features=hidden_dim,bias=bias)
-    out_proj = nn.Linear(in_features=hidden_dim, out_features=n_embd,bias=bias)
+    c_proj = nn.Linear(in_features=n_embd, out_features=hidden_dim, bias=bias)
+    out_proj = nn.Linear(in_features=hidden_dim, out_features=n_embd, bias=bias)
     silu = nn.SiLU()
     mlp.c_fc = c_fc
     mlp.c_proj = c_proj
@@ -41,4 +40,3 @@ def test_SwiGLU_forward():
 
     output_tensor = mlp(input_tensor)
     assert torch.all(output_tensor == out_proj(silu(c_fc(input_tensor)) * c_proj(input_tensor)))
-
