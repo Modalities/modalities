16e31abd11a053254e63d0ae6899a30f1f3fa305
Felix Stollenwerk <felix.stollenwerk@ai.se>
2024-06-25 14:59:59 +0200

    test: scaled initialization for coca

--- tests/test_initialization.py
+++ tests/test_initialization.py
@@ -76,30 +76,48 @@ GPT2_FFN_HIDDEN = 2048
 GPT2_VOCAB_SIZE = 50304
 GPT2_SEQUENCE_LENGTH = 2048
 GPT2_HIDDEN_DIM = 768
+COCA_NLAYERS = 6 + 6  # text + multimodal
 
 # parameters
-GPT2_AVG = {
-    "weight-normal": 0.0,
-    "weight-projection": 0.0,
-    "weight-norm": 1.0,
-    "bias": 0.0,
+AVG = {
+    "gpt2": {
+        "weight-normal": 0.0,
+        "weight-projection": 0.0,
+        "weight-norm": 1.0,
+        "bias": 0.0,
+    },
+    "coca": {
+        "weight-normal": 0.0,
+        "weight-projection": 0.0,
+        "weight-norm": 1.0,
+        "bias": 0.0,
+    },
 }
-GPT2_STD = {
-    "weight-normal": 0.02,
-    "weight-projection": 0.02 / np.sqrt(2 * GPT2_NLAYERS),
-    "weight-norm": 0,
-    "bias": 0,
+STD = {
+    "gpt2": {
+        "weight-normal": 0.02,
+        "weight-projection": 0.02 / np.sqrt(2 * GPT2_NLAYERS),
+        "weight-norm": 0,
+        "bias": 0,
+    },
+    "coca": {
+        "weight-normal": 0.02,
+        "weight-projection": 0.02 / np.sqrt(2 * COCA_NLAYERS),
+        "weight-norm": 0,
+        "bias": 0,
+    },
 }
 
 # number of parameters for each parameter group
-GPT2_ALL = 106374912
+ALL = {
+    "gpt2": 106374912,
+    "coca": 184502784,
+}
 GPT2_LINEAR_PROJECTION = (GPT2_HIDDEN_DIM**2 + GPT2_HIDDEN_DIM * GPT2_FFN_HIDDEN) * GPT2_NLAYERS  # 25952256
 GPT2_EMBEDDING = GPT2_HIDDEN_DIM * (GPT2_VOCAB_SIZE + GPT2_SEQUENCE_LENGTH)
-GPT2_NORM = GPT2_HIDDEN_DIM * 25  # 25 = num_layer_norms
+GPT2_NORM = GPT2_HIDDEN_DIM * (GPT2_NLAYERS * 2 + 1)  # second term = num_layer_norms = (12*2+1) = 25
 GPT2_BIAS = 89856
-GPT2_LINEAR_NO_PROJECTION = GPT2_ALL - GPT2_LINEAR_PROJECTION - GPT2_EMBEDDING - GPT2_NORM - GPT2_BIAS  # 40107264
-
-COCA_ALL = 184502784
+GPT2_LINEAR_NO_PROJECTION = ALL["gpt2"] - GPT2_LINEAR_PROJECTION - GPT2_EMBEDDING - GPT2_NORM - GPT2_BIAS  # 40107264
 
 
 @pytest.mark.skipif(
@@ -117,6 +135,18 @@ COCA_ALL = 184502784
                 "weight-projection": GPT2_LINEAR_PROJECTION,
                 "weight-norm": GPT2_NORM,
                 "bias": GPT2_BIAS,
+                "other": 0,
+            },
+        ),
+        (
+            "coca",
+            "scaled",
+            {
+                "weight-normal": 169332480,
+                "weight-projection": 14745600,
+                "weight-norm": 34560,
+                "bias": 191232,
+                "other": 198912,
             },
         ),
     ],
@@ -133,6 +163,7 @@ def test_initialization_groups(model_name, initialization, groups):
             model = _load_gpt2()
         elif model_name == "coca":
             model = _load_coca()
+        print(model)  # for debugging
 
         params = {name: parameter for name, parameter in model.named_parameters() if parameter.requires_grad}
         init = {}
@@ -144,6 +175,13 @@ def test_initialization_groups(model_name, initialization, groups):
                     if name.endswith(".weight")
                     and not name.endswith("c_proj.weight")
                     and not name.endswith("norm.weight")
+                    and not name.endswith("norm1.weight")
+                    and not name.endswith("norm2.weight")
+                    and not name.endswith("ln_1.weight")
+                    and not name.endswith("ln_2.weight")
+                    and not name.endswith("ln_3.weight")
+                    and not name.endswith("ln_4.weight")
+                    and not name.endswith("ln_f.weight")
                 ]
             )
             init["weight-projection"] = torch.cat(
@@ -158,25 +196,50 @@ def test_initialization_groups(model_name, initialization, groups):
                     torch.flatten(parameter.detach())
                     for name, parameter in params.items()
                     if name.endswith("norm.weight")
+                    or name.endswith("norm1.weight")
+                    or name.endswith("norm2.weight")
+                    or name.endswith("ln_1.weight")
+                    or name.endswith("ln_2.weight")
+                    or name.endswith("ln_3.weight")
+                    or name.endswith("ln_4.weight")
+                    or name.endswith("ln_f.weight")
                 ]
             )
             init["bias"] = torch.cat(
-                [torch.flatten(parameter.detach()) for name, parameter in params.items() if name.endswith(".bias")]
+                [
+                    torch.flatten(parameter.detach())
+                    for name, parameter in params.items()
+                    if name.endswith(".bias") and "conv" not in name
+                ]
             )
-            for key in ["weight-normal", "weight-projection", "weight-norm", "bias"]:
+            other = [
+                torch.flatten(parameter.detach())
+                for name, parameter in params.items()
+                if not name.endswith(".weight") and (not name.endswith(".bias") or "conv" in name)
+            ]
+            init["other"] = torch.cat(other) if len(other) else None
+            count_all = 0
+            for key in ["weight-normal", "weight-projection", "weight-norm", "bias", "other"]:
                 # check number of parameters in each group
-                assert len(init[key]) == groups[key], f"len(init[{key}]) = {len(init[key])} should be {groups[key]}"
+                len_init_key = len(init[key]) if init[key] is not None else 0
+                assert len_init_key == groups[key], f"len(init[{key}]) = {len_init_key} should be {groups[key]}"
+                count_all += len_init_key
 
                 # check mean and std for each group
-                avg = torch.mean(init[key])
-                std = torch.std(init[key])
-                avg_test = torch.tensor(GPT2_AVG[key], device=avg.device, dtype=avg.dtype)
-                std_test = torch.tensor(GPT2_STD[key], device=std.device, dtype=std.dtype)
-                torch.testing.assert_close(
-                    avg, avg_test, msg=f"average for {key} = {avg} should be close to {avg_test}"
-                )
-                torch.testing.assert_close(
-                    std, std_test, msg=f"standard deviation for {key} = {std} should be close to {std_test}"
-                )
+                if key != "other":
+                    avg = torch.mean(init[key])
+                    std = torch.std(init[key])
+                    avg_test = torch.tensor(AVG[model_name][key], device=avg.device, dtype=avg.dtype)
+                    std_test = torch.tensor(STD[model_name][key], device=std.device, dtype=std.dtype)
+                    torch.testing.assert_close(
+                        avg, avg_test, msg=f"average for {key} = {avg} should be close to {avg_test}"
+                    )
+                    torch.testing.assert_close(
+                        std, std_test, msg=f"standard deviation for {key} = {std} should be close to {std_test}"
+                    )
+
+            # check total number of parameters
+            assert count_all == ALL[model_name], f"total number of parameters = {count_all} should be {ALL[model_name]}"
+
         else:
             raise Exception(f"Initialization = {initialization} not implemented.")
