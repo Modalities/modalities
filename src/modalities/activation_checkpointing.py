00d19b2511a0fc98593695487835c6238353858b
mali-git <mehdi.ali@iais.fraunhofer.de>
2024-06-28 09:19:12 +0200

    fix: syntax and typo

--- src/modalities/activation_checkpointing.py
+++ src/modalities/activation_checkpointing.py
@@ -21,10 +21,10 @@ def is_module_to_apply_activation_checkpointing(submodule: torch.nn.Module, acti
 
 def apply_activation_checkpointing_inplace(model: torch.nn.Module, activation_checkpointing_modules: List[str]):
     activation_checkpointing_module_types = [get_module_class_from_name(model, m) for m in activation_checkpointing_modules]
-    if not isinstance(model, FSDP)
+    if not isinstance(model, FSDP):
         raise ValueError("activation checkpointing can only be applied to FSDP wrapped models!")
     non_reentrant_wrapper = partial(checkpoint_wrapper, checkpoint_impl=CheckpointImpl.NO_REENTRANT, debug=False)
 
     apply_activation_checkpointing(
-        model, checkpoint_wrapper_fn=non_reentrant_wrapper, check_fn=lambda submodule: is_module_to_apply_activation_checkpointing(submodule, activation_checkpointing_modules_types)
+        model, checkpoint_wrapper_fn=non_reentrant_wrapper, check_fn=lambda submodule: is_module_to_apply_activation_checkpointing(submodule, activation_checkpointing_module_types)
     )
