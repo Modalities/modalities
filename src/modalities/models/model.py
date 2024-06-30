dcafa38c6b8c1fa5a61c478c5e47c1bec1ad0bee
Max Luebbering <le1nux@users.noreply.github.com>
2024-06-30 17:20:46 +0200

    refactor: removed previous initialization code in the abstract model class

--- src/modalities/models/model.py
+++ src/modalities/models/model.py
@@ -1,27 +1,12 @@
-import math
 from abc import abstractmethod
-from enum import Enum
-from functools import partial
-from typing import Annotated, Dict, List, Optional
+from typing import Dict, List, Optional
 
 import torch
 import torch.nn as nn
-from pydantic import BaseModel, Field
 
 from modalities.batch import DatasetBatch, InferenceResultBatch
 
 WeightDecayGroups = Dict[str, List[str]]
-
-
-class ActivationType(str, Enum):
-    GELU = "gelu"
-    FUSED_SWIGLU = "fused_swiglu"
-
-
-class WeightInitializationConfig(BaseModel):
-    mean: Annotated[float, Field(strict=True, ge=0.0)]
-    std: Annotated[float, Field(strict=True, ge=0.0)] | str  # can be float or "auto"
-    type: str
 
 
 class NNModel(nn.Module):
@@ -41,42 +26,6 @@ class NNModel(nn.Module):
 
     def get_parameters(self) -> Dict[str, torch.Tensor]:
         return {name: param for name, param in self.named_parameters()}
-
-    def _init_weights(self, module: nn.Module, weight_init: WeightInitializationConfig):
-        if isinstance(module, nn.Linear):
-            torch.nn.init.normal_(module.weight, mean=weight_init.mean, std=weight_init.std)
-            if module.bias is not None:
-                torch.nn.init.zeros_(module.bias)
-        elif isinstance(module, nn.Embedding):
-            torch.nn.init.normal_(module.weight, mean=weight_init.mean, std=weight_init.std)
-
-    def initialize_weights(
-        self, weight_init: WeightInitializationConfig, number_of_layers: int, hidden_dim: Optional[int] = None
-    ):
-        # auto: choose std automatically
-        if weight_init.std == "auto":
-            assert hidden_dim is not None, "ERROR! weight_init.std = auto not implemented"
-            weight_init.std = math.sqrt(2 / (5 * hidden_dim))
-
-        # initialize weights
-        self.apply(partial(self._init_weights, weight_init=weight_init))
-
-        if weight_init.type == "plain":
-            pass  # nothing more to do
-        elif weight_init.type in ["scaled", "scaled_embed"]:
-            # apply special scaled init to the residual projections, per GPT-2 paper
-            for pn, p in self.named_parameters():
-                if pn.endswith("c_proj.weight"):
-                    torch.nn.init.normal_(
-                        p, mean=weight_init.mean, std=weight_init.std / math.sqrt(2 * number_of_layers)
-                    )
-            if weight_init.type == "scaled_embed":
-                # apply special init to embeddings, see https://arxiv.org/abs/2312.16903
-                for pn, p in self.named_parameters():
-                    if pn.endswith("wte.weight") or pn.endswith("wpe.weight"):
-                        torch.nn.init.normal_(p, mean=weight_init.mean, std=0.4)
-        else:
-            raise Exception(f"ERROR! weight_init.type = {weight_init.type} not implemented.")
 
 
 def model_predict_batch(model: nn.Module, batch: DatasetBatch) -> InferenceResultBatch:
