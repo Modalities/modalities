93a0cb4dcd53a62c21b185f52c0f1798f77a35e9
Max Luebbering <le1nux@users.noreply.github.com>
2024-07-02 21:58:29 +0200

    refactor: removed ModulewiseNormalInitialization and extended NamedParameterwiseNormalInitialization to support also plain initialisation

--- src/modalities/nn/weight_init/weight_init.py
+++ src/modalities/nn/weight_init/weight_init.py
@@ -1,8 +1,9 @@
-import warnings
+import re
 from typing import List
 
 import torch.nn as nn
 
+from modalities.nn.weight_init.parameter_name_regex_filters import RegexFilter
 from modalities.nn.weight_init.weight_init_if import WeightInitializationIF
 
 
@@ -15,52 +16,19 @@ class WeightInitializerWrapper(WeightInitializationIF):
             weight_initializer.initialize_in_place(model)
 
 
-class ModulewiseNormalInitialization(WeightInitializationIF):
-    def __init__(self, mean: float, std: float):
-        """Initializes the weights of a model by sampling from a normal distribution.
-        NOTE: This class supports the initialization of nn.Linear and nn.Embedding layers.
-        For other layer types, the initialization must be subclassed and extended
-
-        Args:
-            mean (float): mean of the normal distribution
-            std (float): standard deviation of the normal distribution
-        """
-        self.mean = mean
-        self.std = std
-
-    def _init_weights_impl(self, module: nn.Module):
-        if isinstance(module, nn.Linear):
-            nn.init.normal_(module.weight, mean=self.mean, std=self.std)
-            if module.bias is not None:
-                nn.init.zeros_(module.bias)
-        elif isinstance(module, nn.Embedding):
-            nn.init.normal_(module.weight, mean=self.mean, std=self.std)
-        else:
-            warnings.warn(
-                f"Module {module.__class__} is not of type nn.Linear or nn.Embedding. "
-                "Looking for weight and bias attributes to initialize."
-            )
-            if hasattr(module, "weight") and module.weight is not None:
-                nn.init.normal_(module.weight, mean=self.mean, std=self.std)
-
-            if hasattr(module, "bias") and module.bias is not None:
-                nn.init.zeros_(module.bias)
-
-            if not hasattr(module, "weight") and not hasattr(module, "bias"):
-                raise NotImplementedError(f"ERROR! Initialization of {module.__class__} not implemented")
-
-    def initialize_in_place(self, model: nn.Module):
-        model.apply(self._init_weights_impl)
-
-
 class NamedParameterwiseNormalInitialization(WeightInitializationIF):
-    def __init__(self, mean: float, std: float, parameter_name_suffixes: List[str]):
+    def __init__(self, mean: float, std: float, parameter_name_regexes: RegexFilter):
         self.mean = mean
         self.std = std
-        self.parameter_name_suffixes = parameter_name_suffixes
+        self.parameter_name_regexes = parameter_name_regexes
 
     def initialize_in_place(self, model: nn.Module):
-        for pn, p in model.named_parameters():
-            for parameter_name_suffix in self.parameter_name_suffixes:
-                if pn.endswith(parameter_name_suffix):
+        for parameter_name, p in model.named_parameters():
+            weight_regexes = self.parameter_name_regexes.weights
+            for weight_regex in weight_regexes:
+                if re.fullmatch(weight_regex, parameter_name):
                     nn.init.normal_(p, mean=self.mean, std=self.std)
+            bias_regexes = self.parameter_name_regexes.biases
+            for bias_regex in bias_regexes:
+                if re.fullmatch(bias_regex, parameter_name):
+                    nn.init.zeros_(p)
