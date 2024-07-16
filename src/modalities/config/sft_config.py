330314717193afb040961cd54a350614552d8f8a
Alexander Weber <alex.a.weber@gmx.de>
2024-07-16 13:27:44 +0200

    chore: extend to multiple chat templates

---
+++ src/modalities/config/sft_config.py
@@ -0,0 +1,22 @@
+from pathlib import Path
+from typing import Any, Dict, Optional
+
+from pydantic import BaseModel, FilePath
+
+
+class Settings(BaseModel):
+    src_path: FilePath
+    dst_path: Path
+    conversations_key: str
+    chat_template_key: Optional[str] = None
+
+
+class InstructionDataTransformation(BaseModel):
+    role_mapping: Dict[str, str]
+
+
+class SFTConfig(BaseModel):
+    settings: Settings
+    instruction_data_transformation: InstructionDataTransformation
+    jinja2_chat_template_files: Dict[str, FilePath]
+    chat_template_data: Dict[str, Any]
--- src/modalities/dataloader/apply_chat_template.py
+++ src/modalities/dataloader/apply_chat_template.py
@@ -1,10 +1,13 @@
 import json
 from pathlib import Path
+from typing import Any, Dict, Generator, List
 
 import jsonlines
+from jinja2 import Template
 from packaging import version
 
 from modalities.config.config import load_app_config_dict
+from modalities.config.sft_config import SFTConfig
 
 # TODO copy and adapt: src.modalities.dataloader.dataset.MemMapDataset
 # -> it reads lerge JSONL files, jq-pattern filters and tokenizes
@@ -18,36 +21,48 @@ from modalities.config.config import load_app_config_dict
 
 def apply_chat_template(config_file_path: Path):
     config_dict = load_app_config_dict(config_file_path=config_file_path)
-    instruction_data = _stream_jsonl(config_dict["settings"]["src_path"])
-    chat_template = _compile_jinja_template(config_dict["chat_template"].replace("}\n{", "}{"))
-    conversations_key = config_dict["settings"]["conversations_key"]
-    role_mapping = config_dict["instruction_data_transformation"]["role_mapping"]
-    output_file_path = config_dict["settings"]["dst_path"]
-    with open(output_file_path, "w") as output_file:
+    config = SFTConfig(**config_dict)
+    instruction_data = _stream_jsonl(config.settings.src_path)
+    chat_template_key = config.settings.chat_template_key
+    chat_templates = get_chat_templates(config.jinja2_chat_template_files)
+
+    with open(config.settings.dst_path, "w") as output_file:
         for entry in instruction_data:
-            conversation = entry[conversations_key]
-            conversation = map_roles(conversation, role_mapping)
-            chat = chat_template.render(conversation=conversation, chat_template_data=config_dict["chat_template_data"])
-            if not all(
-                special_token in chat for special_token in config_dict["chat_template_data"]["special_tokens"].values()
-            ):
+            conversation = entry[config.settings.conversations_key]
+            conversation = map_roles(conversation, config.instruction_data_transformation.role_mapping)
+            if chat_template_key in entry:
+                chat_template = chat_templates[chat_template_key]
+            else:
+                chat_template = chat_templates["default"]
+
+            chat = chat_template.render(conversation=conversation, chat_template_data=config.chat_template_data)
+            if not all(special_token in chat for special_token in config.chat_template_data["special_tokens"].values()):
                 raise ValueError("Not all special tokens are present in the chat template!")
             entry["chat"] = chat
             json.dump(entry, output_file)
             output_file.write("\n")
 
 
-def map_roles(conversation, role_mapping):
+def get_chat_templates(jinja2_chat_template_files: Dict[str, str]) -> Dict[str, Template]:
+    chat_templates = {}
+    for key, file_path in jinja2_chat_template_files.items():
+        with Path(file_path).open() as file:
+            chat_template = "".join(["".join(line.rsplit("\n", 1)) for line in file.readlines()])
+            chat_templates[key] = _compile_jinja_template(chat_template)
+    return chat_templates
+
+
+def map_roles(conversation: List[Dict[str, Any]], role_mapping: Dict[str, str]) -> List[Dict[str, Any]]:
     return [{key: role_mapping.get(value, value) for key, value in turn.items()} for turn in conversation]
 
 
-def _stream_jsonl(src_file_path):
+def _stream_jsonl(src_file_path: str) -> Generator[Dict[str, Any], None, None]:
     with jsonlines.open(src_file_path) as reader:
         for obj in reader:
             yield obj
 
 
-def _compile_jinja_template(chat_template):
+def _compile_jinja_template(chat_template: str) -> Template:
     """Code taken from
     https://github.com/huggingface/transformers/blob/v4.42.0/src/transformers/tokenization_utils_base.py#L1906
     """
