chore: fix the loss masking test and the implementation. Improve documentation.

src/modalities/models/components/collator_wrapper.py
@@ -18,7 +18,7 @@ class LossMaskingCollateFnWrapperConfig(BaseModel):
     collate_fn: PydanticCollateFnIFType
     target_keys_to_mask: List[str]
     loss_ignore_index: int
-    special_tokens: MaskingTokenConfig
+    mask_tokens: MaskingTokenConfig
     tokenizer: PydanticTokenizerIFType
 
 
@@ -28,15 +28,23 @@ class LossMaskingCollateFnWrapper(CollateFnIF):
         collate_fn: CollateFnIF,
         target_keys_to_mask: List[str],
         loss_ignore_index: int,
-        special_tokens: MaskingTokenConfig,
+        mask_tokens: MaskingTokenConfig,
         tokenizer: TokenizerWrapper,
     ):
+        """Wraps the given collate_fn and masks the target keys if not within the given special mask tokens.
+        Does not include both mask tokens into the loss. If you need a token to indicate the end of the assistant,
+        use another special token for this!
+        Works also for the continouus dataset reading, as if the "end-include-to-loss" token is detected in the front,
+        all tokens before are included to the loss.
+
+        Throws a ValueError if the mask tokens are not found in the target or if the mask tokens are the same.
+        """
         self.collate_fn = collate_fn
         self.target_keys_to_mask = target_keys_to_mask
         self.loss_ignore_index = loss_ignore_index
         self.tokenizer = tokenizer
-        self.b_mask_token_id = self.tokenizer.get_token_id(special_tokens.b_include_to_loss_token)
-        self.e_mask_token_id = self.tokenizer.get_token_id(special_tokens.e_include_to_loss_token)
+        self.b_mask_token_id = self.tokenizer.get_token_id(mask_tokens.b_include_to_loss_token)
+        self.e_mask_token_id = self.tokenizer.get_token_id(mask_tokens.e_include_to_loss_token)
 
     def __call__(self, batch: List[Dict[str, torch.Tensor]]) -> DatasetBatch:
         dataset_batch = self.collate_fn(batch)
@@ -70,21 +78,22 @@ class LossMaskingCollateFnWrapper(CollateFnIF):
                 + error_msg
                 + error_msg
             )
+
         mask = torch.zeros_like(target)
-        mask += torch.where(target != b_mask_token_id, 0, 1)
+        # we shift the mask to the right, to exclude not only the end mask token but also
+        # the begin mask token from the loss
+        mask[:, 1:] += torch.where(target != b_mask_token_id, 0, 1)[:, :-1]
         mask += torch.where(target != e_mask_token_id, 0, -1)
 
-        # in case -1 is before 1 we need to include the first tokens to the loss
+        # in case -1 (end mask token indicator) is before 1 (begin mask token indicator) we need to
+        # include the first tokens to the loss
         end_before_begin = torch.argmax(mask, dim=-1, keepdim=True) > torch.argmin(mask, dim=-1, keepdim=True)
         mask[:, 0] = end_before_begin.squeeze()
 
-        # mark all tokens beween 1 and -1 with 1
-        mask = mask.cumsum(-1)
-
-        # shift the mask to the right, to conform to the shifted target
-        mask = mask.roll(shifts=1, dims=-1)
-        mask[:, 0] = end_before_begin.squeeze()
+        # mark all tokens beween 1 (begin mask token indicator) and -1 (end mask token indicator) with 1
+        # this includes the 1, but due to the shift above, we exclude both!
+        include_to_loss_mask = mask.cumsum(-1)
 
         # apply mask: if mask is 1, keep the target, otherwise replace with loss_ignore_index
-        new_target = torch.where(mask > 0, target, loss_ignore_index)
+        new_target = torch.where(include_to_loss_mask.bool(), target, loss_ignore_index)
         return new_target