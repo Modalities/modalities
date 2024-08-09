343153fec52873a6b76ce99b7e41bbed1e6a778b
Felix Stollenwerk <felix.stollenwerk@ai.se>
2024-08-09 18:55:30 +0200

    chore: fixed cpu tests

--- src/modalities/util.py
+++ src/modalities/util.py
@@ -193,15 +193,18 @@ def get_theoretical_gpu_peak_performance(world_size: int) -> Optional[Number]:
     """
     returns theoretical gpu peak performance in units FLOPs / s for given gpu type
     """
-    device_name = torch.cuda.get_device_name()
-    if device_name.startswith("NVIDIA A100"):
-        return PEAK_PERFORMANCE["A100"] * world_size
-    elif device_name.startswith("NVIDIA H100"):
-        return PEAK_PERFORMANCE["H100"] * world_size
+    if torch.cuda.is_available() and torch.cuda.device_count() > 0:  # necessary for cpu-only tests
+        device_name = torch.cuda.get_device_name()
+        if device_name.startswith("NVIDIA A100"):
+            return PEAK_PERFORMANCE["A100"] * world_size
+        elif device_name.startswith("NVIDIA H100"):
+            return PEAK_PERFORMANCE["H100"] * world_size
+        else:
+            print(
+                f"WARNING: could not get theoretical peak performance for found device = {device_name}"
+            )  # TODO: print as warning
+            return None
     else:
-        print(
-            f"WARNING: could not get theoretical peak performance for found device = {device_name}"
-        )  # TODO: print as warning
         return None
 
 
@@ -214,18 +217,21 @@ def get_theoretical_flops_per_token(model: FSDP) -> Tuple[Optional[int], Optiona
         theoretical_flops_per_token
         sequence_length (needed to convert samples to tokens in compute_mfu)
     """
-    N = get_total_number_of_trainable_parameters(model)
-    try:
-        L = model.module.n_layer
-        T = model.module.sequence_length
-        H = model.module.n_embd
-        return 6 * N + 12 * L * T * H, T
-    except AttributeError:
+    if torch.cuda.is_available() and torch.cuda.device_count() > 0:  # necessary for cpu-only tests
+        N = get_total_number_of_trainable_parameters(model)
+        try:
+            L = model.module.n_layer
+            T = model.module.sequence_length
+            H = model.module.n_embd
+            return 6 * N + 12 * L * T * H, T
+        except AttributeError:
+            return None, None
+    else:
         return None, None
 
 
 def compute_mfu(
-    synced_num_samples_per_second: torch.Tensor,
+    num_samples_per_second: torch.Tensor,
     sequence_length: int,
     theoretical_flops_per_token: Optional[Number],
     theoretical_gpu_peak_performance: Optional[Number],
@@ -237,5 +243,5 @@ def compute_mfu(
     if theoretical_flops_per_token is None or theoretical_gpu_peak_performance is None:
         return torch.tensor(-1.0)  # needs to be float for EvaluationResultBatch
     else:
-        num_tokens_per_second = synced_num_samples_per_second * sequence_length
+        num_tokens_per_second = num_samples_per_second * sequence_length
         return num_tokens_per_second * theoretical_flops_per_token / theoretical_gpu_peak_performance
