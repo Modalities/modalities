c415b16e502b1e8d0ed6ae555b1bfdd29b80fa9b
mali-git <mehdi.ali@iais.fraunhofer.de>
2024-08-12 11:50:47 +0200

    docs: add docstring to torch_checkpoint_saving.py

--- src/modalities/checkpointing/torch/torch_checkpoint_saving.py
+++ src/modalities/checkpointing/torch/torch_checkpoint_saving.py
@@ -16,6 +16,16 @@ class TorchCheckpointSaving(CheckpointSavingExecutionABC):
         checkpoint_path: Path,
         experiment_id: str,
     ):
+        """
+        Initializes the TorchCheckpointSaving object.
+
+        Args:
+            checkpoint_path (Path): The path to save the checkpoint.
+            experiment_id (str): The ID of the experiment.
+
+        Returns:
+            None
+        """
         self.checkpoint_path = checkpoint_path
         self.experiment_id = experiment_id
 
@@ -26,10 +36,51 @@ class TorchCheckpointSaving(CheckpointSavingExecutionABC):
         model: nn.Module,
         optimizer: Optimizer,
     ):
+        """
+        Runs the checkpointing instruction for saving checkpoints.
+
+        Args:
+            checkpointing_instruction (CheckpointingInstruction): The instruction for checkpointing.
+            global_train_sample_id (int): The global train sample ID.
+            model (nn.Module): The model to be checkpointed.
+            optimizer (Optimizer): The optimizer associated with the model.
+
+        Returns:
+            None
+
+        Raises:
+            NotImplementedError: This method is not implemented yet. It is reserved for future work.
+        """
         raise NotImplementedError  # TODO Future work
 
     def _save_checkpoint(self, model: FSDP, optimizer: Optimizer, global_train_sample_id: int):
+        """
+        Save the checkpoint of the model and optimizer.
+
+        Args:
+            model (FSDP): The model to be saved.
+            optimizer (Optimizer): The optimizer to be saved.
+            global_train_sample_id (int): The global train sample ID.
+
+        Returns:
+            None
+
+        Raises:
+            NotImplementedError: This method is not implemented yet. It is reserved for future work.
+        """
         raise NotImplementedError  # TODO Future work
 
     def _delete_checkpoint(self, global_train_sample_id: int):
+        """
+        Deletes the checkpoint for the given global train sample ID.
+
+        Args:
+            global_train_sample_id (int): The global train sample ID.
+
+        Returns:
+            None
+
+        Raises:
+            NotImplementedError: This method is not implemented yet. It is reserved for future work.
+        """
         raise NotImplementedError  # TODO Future work
