de9e6532a9dd129447f4ea1e262c11725fa5d74b
mali-git <mehdi.ali@iais.fraunhofer.de>
2024-08-12 10:22:20 +0200

    chore: add docstring to checkpoint_saving_execution.py

--- src/modalities/checkpointing/checkpoint_saving_execution.py
+++ src/modalities/checkpointing/checkpoint_saving_execution.py
@@ -7,12 +7,34 @@ from modalities.checkpointing.checkpoint_saving_instruction import Checkpointing
 
 
 class CheckpointSavingExecutionABC(ABC):
+    """Abstract class for saving PyTorch model and optimizer checkpoints."""
+
     @abstractmethod
     def _save_checkpoint(self, model: nn.Module, optimizer: Optimizer, num_train_steps_done: int):
+        """
+        Saves the checkpoint of the model and optimizer.
+
+        Args:
+            model (nn.Module): The model to be saved.
+            optimizer (Optimizer): The optimizer to be saved.
+            num_train_steps_done (int): The number of training steps completed.
+
+        Raises:
+            NotImplementedError: This method is not implemented and should be overridden in a subclass.
+        """
         raise NotImplementedError
 
     @abstractmethod
     def _delete_checkpoint(self, num_train_steps_done: int):
+        """
+        Deletes the checkpoint.
+
+        Args:
+            num_train_steps_done (int): The number of training steps completed.
+
+        Raises:
+            NotImplementedError: This abstract method is not implemented and should be overridden in a subclass.
+        """
         raise NotImplementedError
 
     def run_checkpoint_instruction(
@@ -22,6 +44,15 @@ class CheckpointSavingExecutionABC(ABC):
         model: nn.Module,
         optimizer: Optimizer,
     ):
+        """
+        Runs the checkpoint instruction.
+
+        Args:
+            checkpointing_instruction (CheckpointingInstruction): The checkpointing instruction.
+            num_train_steps_done (int): The number of training steps done.
+            model (nn.Module): The model.
+            optimizer (Optimizer): The optimizer.
+        """
         if checkpointing_instruction.save_current:
             self._save_checkpoint(model=model, optimizer=optimizer, num_train_steps_done=num_train_steps_done)
 
