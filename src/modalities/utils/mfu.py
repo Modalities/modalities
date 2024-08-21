955461b9178782ba46c5abddbf36246564edc49f
mali-git <mehdi.ali@iais.fraunhofer.de>
2024-08-21 14:55:43 +0200

    docs: add docstrings

--- src/modalities/utils/mfu.py
+++ src/modalities/utils/mfu.py
@@ -20,8 +20,17 @@ PEAK_PERFORMANCE = {
 
 def _get_theoretical_gpu_peak_performance_single(precision: torch.dtype, gpu_type: str) -> Optional[Number]:
     """
-    returns theoretical gpu peak performance for #GPU=1 in units FLOPs / s for given gpu type
+    Get the theoretical peak performance of a single GPU in units FLOPs / s for given GPU type.
+
+    Args:
+        precision (torch.dtype): The precision of the GPU.
+        gpu_type (str): The type of the GPU.
+
+    Returns:
+        (Number, optional): The theoretical peak performance of the GPU if the precision and GPU type are valid,
+          otherwise None.
     """
+
     if precision in [torch.float16, torch.bfloat16] and gpu_type in PEAK_PERFORMANCE.keys():
         return PEAK_PERFORMANCE[gpu_type]
     else:
@@ -30,12 +39,21 @@ def _get_theoretical_gpu_peak_performance_single(precision: torch.dtype, gpu_typ
 
 def get_theoretical_gpu_peak_performance(model: FSDP, world_size: int) -> Optional[Number]:
     """
-    returns theoretical gpu peak performance for #GPU=world_size in units FLOPs / s for given gpu type
+    Calculates the accummulated theoretical peak performance based on all GPUs, i.e.,
+      #GPU=world_size, in units FLOPs / s for given gpu type.
+
+    Args:
+        model (FSDP): The model for which to calculate the theoretical peak performance.
+        world_size (int): The number of GPUs used in parallel.
+
+    Returns:
+        (Number, optional): The accummulated theoretical peak performance of all GPUs,
+          or None if it cannot be calculated.
     """
     if torch.cuda.is_available() and torch.cuda.device_count() > 0:  # necessary for cpu-only tests
         precision = model.mixed_precision.param_dtype
         if model.mixed_precision.reduce_dtype != precision or model.mixed_precision.buffer_dtype != precision:
-            warnings.warn("could not get theoretical gpu peak performance for given mixed precision type")
+            warnings.warn("Could not get theoretical GPU peak performance for given mixed precision type.")
             return None
         else:
             device_name = torch.cuda.get_device_name()
@@ -44,10 +62,10 @@ def get_theoretical_gpu_peak_performance(model: FSDP, world_size: int) -> Option
             elif device_name.startswith("NVIDIA H100"):
                 single_gpu_peak_performance = _get_theoretical_gpu_peak_performance_single(precision, "H100")
             else:
-                warnings.warn(f"could not get theoretical gpu peak performance for unknown device = {device_name}")
+                warnings.warn(f"Could not get theoretical gpu peak performance for unknown device = {device_name}.")
                 return None
             if single_gpu_peak_performance is None:
-                warnings.warn(f"could not get theoretical gpu peak performance for {device_name} and {precision}")
+                warnings.warn(f"Could not get theoretical gpu peak performance for {device_name} and {precision}.")
                 return None
             else:
                 return single_gpu_peak_performance * world_size
@@ -57,12 +75,19 @@ def get_theoretical_gpu_peak_performance(model: FSDP, world_size: int) -> Option
 
 def get_theoretical_flops_per_token(model: FSDP) -> Tuple[Optional[int], Optional[int]]:
     """
+    Calculates the theoretical number of floating point operations (FLOPs) per token for a given model.
     compute theoretical_flops_per_token = 6*N + 12*L*T*H
-    see App. B in the PaLM paper (https://arxiv.org/pdf/2204.02311)
+    See App. B in the PaLM paper (https://arxiv.org/pdf/2204.02311)
+
+    Args:
+        model (FSDP): The model for which to calculate the FLOPs per token.
 
     Returns:
-        theoretical_flops_per_token
-        sequence_length (needed to convert samples to tokens in compute_mfu)
+        Tuple[(int, optional), (int, optional)]: A tuple containing the theoretical FLOPs per token
+          and the sequence length.
+            - Theoretical FLOPs per token: The estimated number of FLOPs required to process each token in the model.
+            - Sequence length: The length of the input sequence. Needed to convert samples to tokens in compute_mfu.
+            If CUDA is not available, returns (None, None).
     """
     if torch.cuda.is_available() and torch.cuda.device_count() > 0:  # necessary for cpu-only tests
         N = get_total_number_of_trainable_parameters(model)
@@ -84,9 +109,26 @@ def compute_mfu(
     theoretical_gpu_peak_performance: Optional[Number],
 ) -> torch.Tensor:
     """
+    Computes the Model Flops Utilization (MFU).
+
     compute mfu = throughput * theoretical_flops_per_token / theoretical_gpu_peak_performance
     units:  [1] = [tokens/s] * [FLOPs / token]             / [FLOPs / s]
+
+    Args:
+        num_samples_per_second (torch.Tensor): The number of samples per second.
+        sequence_length (int): The length of the sequence.
+        theoretical_flops_per_token (Optional[Number]): The theoretical number of floating-point operations per token.
+        theoretical_gpu_peak_performance (Optional[Number]): The theoretical peak performance of the GPU.
+
+    Returns:
+        torch.Tensor: The computed MFU.
+
+    Note:
+        - If either `theoretical_flops_per_token` or `theoretical_gpu_peak_performance` is None,
+          the function returns -1.0.
+        - The returned value needs to be a float for EvaluationResultBatch.
     """
+
     if theoretical_flops_per_token is None or theoretical_gpu_peak_performance is None:
         return torch.tensor(-1.0)  # needs to be float for EvaluationResultBatch
     else:
