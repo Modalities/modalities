3b7d74d1cafccc26af583b75d6dfb83d978e48d4
Max Luebbering <le1nux@users.noreply.github.com>
2024-08-29 13:42:20 +0200

    refactor: removed excessive print statements

--- src/modalities/models/model_factory.py
+++ src/modalities/models/model_factory.py
@@ -2,7 +2,6 @@ from pathlib import Path
 from typing import List
 
 import torch
-import torch.distributed as dist
 import torch.nn as nn
 from torch.distributed.fsdp import FullyShardedDataParallel as FSDP
 from torch.distributed.fsdp import ShardingStrategy
@@ -11,7 +10,6 @@ from modalities.checkpointing.checkpoint_loading import CheckpointLoadingIF
 from modalities.nn.model_initialization.initialization_if import ModelInitializationIF
 from modalities.running_env.env_utils import MixedPrecisionSettings
 from modalities.running_env.fsdp.fsdp_auto_wrapper import FSDPTransformerAutoWrapPolicyFactory
-from modalities.util import get_local_number_of_trainable_parameters
 
 
 class ModelFactory:
@@ -33,10 +31,10 @@ class ModelFactory:
         mixed_precision_settings: MixedPrecisionSettings,
         sharding_strategy: ShardingStrategy,
     ) -> FSDP:
-        print(
-            f"Unsharded number of parameters on rank {dist.get_rank()}: "
-            f"{get_local_number_of_trainable_parameters(model)}"
-        )
+        # print(
+        #     f"Unsharded number of parameters on rank {dist.get_rank()}: "
+        #     f"{get_local_number_of_trainable_parameters(model)}"
+        # )
         # Here, FSDPTransformerAutoWrapPolicyFactory is hardcoded and should be passed in instead!
         # we also might want to have different auto wrap policies later...
         fsdp_auto_wrap_factory = FSDPTransformerAutoWrapPolicyFactory(model=model, block_names=block_names)
@@ -51,10 +49,10 @@ class ModelFactory:
             sync_module_states=sync_module_states,
             use_orig_params=True,
         )
-        print(
-            f"Sharded number of parameters on rank {dist.get_rank()}:"
-            f"{get_local_number_of_trainable_parameters(fsdp_model)}"
-        )
+        # print(
+        #     f"Sharded number of parameters on rank {dist.get_rank()}:"
+        #     f"{get_local_number_of_trainable_parameters(fsdp_model)}"
+        # )
 
         return fsdp_model
 
