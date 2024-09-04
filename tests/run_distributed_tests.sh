bce3a182159ec7cc4f3c8dd3b528f07792ba8088
Felix Stollenwerk <felix.stollenwerk@ai.se>
2024-09-04 11:11:02 +0200

    test: check input arguments for run_distributed_tests.sh

--- tests/run_distributed_tests.sh
+++ tests/run_distributed_tests.sh
@@ -3,15 +3,20 @@
 #######################
 ### INPUT ARGUMENTS ###
 #######################
-if [ -z "$1" ] || [ -z "$2" ]  # if one of the two input arguments does not exist
+if [ -z "$1" ] || [ -z "$2" ] || [ -z "$3" ] # if one of the three input arguments does not exist
   then
-    echo "Need to specify 2 GPU devices as arguments, e.g. bash run_distributed_tests.sh 0 1"
+    echo "Need to specify 2 GPU devices plus --cov/--no-cov as arguments, e.g. bash run_distributed_tests.sh 0 1 --cov"
     exit
 fi
-if [[ $1 =~ [^0-7] ]] || [[ $2 =~ [^0-7] ]]  # if one of the two input arguments is not an integer 0-7
+if [[ $1 =~ [^0-7] ]] || [[ $2 =~ [^0-7] ]]  # if one of the two first input arguments is not an integer 0-7
     then
-        echo "Need to specify integers 0-7 as arguments, e.g. bash run_distributed_tests.sh 0 1"
+        echo "Need to specify integers 0-7 as first two arguments, e.g. bash run_distributed_tests.sh 0 1 --cov"
         exit 
+fi
+if ! [[ $3 =~ ^(--cov|--no-cov)$ ]];  # if the third argument is neither --cov nor --no-cov
+    then 
+        echo "Need to specify --cov or --no-cov as third argument, e.g. bash run_distributed_tests.sh 0 1 --cov"
+        exit
 fi
 
 #################
@@ -19,7 +24,7 @@ fi
 #################
 DEV0=$1 
 DEV1=$2
-COVERAGE=$3 # --cov or  --no-cov
+COVERAGE=$3
 
 #############
 ### TESTS ###
