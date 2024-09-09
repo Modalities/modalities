863cac1b244ba5d0606bebd8e9025436c1c50dbb
ajude <alex.jude@iais.fraunhofer.de>
2024-09-09 11:15:40 +0200

    refactor(lora_tests): 1. Added type hints and passed keyword arguements. 2. Removed hardcoded paths in the test.

--- tests/fine_tuning/test_instruction_tuning_data_conversion.py
+++ tests/fine_tuning/test_instruction_tuning_data_conversion.py
@@ -1,15 +1,14 @@
+# from modalities.dataloader.apply_chat_template import split_and_apply_chat_template
+import json
+import os
 from pathlib import Path
 
 import pytest
-from modalities.__main__ import entry_point_data_create_raw_index, entry_point_pack_encoded_data
+from transformers import GPT2Tokenizer
+
 from modalities.dataloader.create_index import IndexGenerator
 from modalities.dataloader.large_file_lines_reader import LargeFileLinesReader
-from transformers import GPT2Tokenizer
-from modalities.dataloader.apply_chat_template import apply_chat_template
-from pydantic import FilePath
-import json
-
-# todo remove hard coded paths
+from tests.conftest import _ROOT_DIR
 
 
 @pytest.fixture
@@ -19,7 +18,12 @@ def tokenizer_path() -> str:
 
 @pytest.fixture
 def apply_chat_template_config_path() -> str:
-    return "/raid/s3/opengptx/maxr/ogptx/modalities/config_files/data_preparation/apply_chat_template_config_for_lora_test.yaml"
+    return _ROOT_DIR / Path("config_files/data_preparation/apply_chat_template_config.yaml")
+
+
+@pytest.fixture
+def index_path() -> Path:
+    return _ROOT_DIR / Path("data/lorem_ipsum_sft_converted.74d2208.idx")
 
 
 @pytest.fixture
@@ -27,7 +31,17 @@ def pbin_config_path() -> str:
     return "/raid/s3/opengptx/maxr/ogptx/modalities/config_files/data_preparation/packed_chat_dataset_config_for_lora_test.yaml"
 
 
-def test_if_include_to_loss_tokens_are_only_one_token(tokenizer_path):
+@pytest.fixture
+def jsonl_file_path_before_applying_chat_template() -> Path:
+    return "/raid/s3/opengptx/alignment_data/data/honey/data/en/ultrachat_200k_fastchat.jsonl"
+
+
+@pytest.fixture
+def jsonl_file_path_after_applying_chat_template() -> str:
+    return "/raid/s3/opengptx/alignment_data/data/honey/data/en/ultrachat_200k_fastchat_74d2208/ultrachat_200k_fastchat_converted_train.74d2208.jsonl"
+
+
+def test_if_include_to_loss_tokens_are_only_one_token(tokenizer_path: str):
     tokenizer = GPT2Tokenizer.from_pretrained(tokenizer_path)
     b_include_to_loss_token = "^"
     e_include_to_loss_token = "$"
@@ -39,37 +53,37 @@ def test_if_include_to_loss_tokens_are_only_one_token(tokenizer_path):
     assert len(tokenized_output["input_ids"]) == 1
 
 
-def read_first_line_from_json(file_path: str):
+def read_first_line_from_json(file_path: str) -> dict:
     with open(file_path, "r") as file:
         first_line = file.readline()
         first_line_data = json.loads(first_line)
     return first_line_data
 
 
-def test_conversation_into_prompt_conversion(apply_chat_template_config_path):
-    # apply_chat_template(Path(apply_chat_template_config_path))
-    original_path = "/raid/s3/opengptx/alignment_data/data/honey/data/en/ultrachat_200k_fastchat.jsonl"
-    dst_path = "/raid/s3/opengptx/alignment_data/data/honey/data/en/ultrachat_200k_fastchat_converted.44a6969.jsonl"
-    original_first_line = read_first_line_from_json(original_path)
-    dst_first_line = read_first_line_from_json(dst_path)
+def test_conversation_into_prompt_conversion(
+        apply_chat_template_config_path: str,
+        jsonl_file_path_before_applying_chat_template: str,
+        jsonl_file_path_after_applying_chat_template: str
+):
+    # split_and_apply_chat_template(Path(apply_chat_template_config_path))
+    original_first_line = read_first_line_from_json(jsonl_file_path_before_applying_chat_template)
+    dst_first_line = read_first_line_from_json(jsonl_file_path_after_applying_chat_template)
     assert not original_first_line.get("chat")
     assert dst_first_line.get("chat")
 
 
-def test_indexing():
-    src_path = Path(
-        "/raid/s3/opengptx/alignment_data/data/honey/data/en/ultrachat_200k_fastchat_converted.44a6969.jsonl"
-    )
-    index_path = Path("/raid/s3/opengptx/maxr/ogptx/modalities/data/lorem_ipsum_sft_converted.44a6969.idx")
+def test_indexing(jsonl_file_path_after_applying_chat_template: str, index_path: Path):
+    src_path = Path(jsonl_file_path_after_applying_chat_template)
     index_path = LargeFileLinesReader.default_index_path(src_path, index_path)
     if index_path.exists():
-        raise ValueError("index already exists. delete it or specify different output folder.")
-    print(f"reading raw data from {src_path}")
-    print(f"writing index to {index_path}")
+        raise ValueError("Index already exists. delete it or specify different output folder.")
+    print(f"Reading raw data from {src_path}")
+    print(f"W index to {index_path}")
     generator = IndexGenerator(src_path)
     generator.create_index(index_path)
-    print("done")
 
+    assert os.path.exists(index_path), "Index file not generated"
 
-if __name__ == "__main__":
-    ...
+    # Removing the created index file
+    os.remove(index_path)
+    print(f"{index_path} has been deleted successfully.")
