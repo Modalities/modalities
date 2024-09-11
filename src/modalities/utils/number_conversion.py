0ced8b4c73bea543cfd35f683499d254479f429c
Max Luebbering <le1nux@users.noreply.github.com>
2024-09-11 18:19:07 +0200

    feat: added further number conversion functions

--- src/modalities/utils/number_conversion.py
+++ src/modalities/utils/number_conversion.py
@@ -22,6 +22,7 @@ class NumStepsFromNumSamplesConfig(BaseModel):
     num_ranks: Annotated[int, Field(strict=True, gt=0)]
     local_micro_batch_size: Annotated[int, Field(strict=True, gt=0)]
     global_num_samples: Annotated[int, Field(strict=True, ge=0)]
+    gradient_accumulation_steps: Annotated[int, Field(strict=True, gt=0)]
 
 
 class NumStepsFromNumTokensConfig(BaseModel):
@@ -29,28 +30,32 @@ class NumStepsFromNumTokensConfig(BaseModel):
     local_micro_batch_size: Annotated[int, Field(strict=True, gt=0)]
     global_num_tokens: Annotated[int, Field(strict=True, ge=0)]
     sequence_length: Annotated[int, Field(strict=True, gt=0)]
+    gradient_accumulation_steps: Annotated[int, Field(strict=True, gt=0)]
 
 
 class NumTokensFromNumStepsConfig(BaseModel):
     num_ranks: Annotated[int, Field(strict=True, gt=0)]
     local_micro_batch_size: Annotated[int, Field(strict=True, gt=0)]
     sequence_length: Annotated[int, Field(strict=True, gt=0)]
+    gradient_accumulation_steps: Annotated[int, Field(strict=True, gt=0)]
 
 
-class LastStepFromCheckpointPathConfig(BaseModel):
+class NumberConversionFromCheckpointPathConfig(BaseModel):
     checkpoint_path: Path
-
-
-class GlobalNumSeenTokensFromCheckpointPathConfig(BaseModel):
-    checkpoint_path: Path
-
-
-class NumStepsFromNumTokensAndCheckpointPathConfig(BaseModel):
-    checkpoint_path: Path
-    global_num_tokens: Annotated[int, Field(strict=True, ge=0)]
 
 
 class NumberConversion:
+    @staticmethod
+    def _get_checkpoint_parameter_value(pattern: str, string: str) -> int:
+        match = re.search(pattern, string)
+
+        # Extract the number of steps if a match is found
+        if match:
+            value = int(match.group(1))  # Group 1 contains the digits'
+        else:
+            raise ValueError(f"No match found for pattern {pattern} in {string}")
+        return value
+
     @staticmethod
     def get_local_num_batches_from_num_samples(
         num_ranks: int, global_num_samples: int, local_micro_batch_size: int
@@ -92,46 +97,55 @@ class NumberConversion:
         )
 
     @staticmethod
-    def get_num_steps_from_num_samples(num_ranks: int, local_micro_batch_size: int, global_num_samples: int) -> int:
+    def get_num_steps_from_num_samples(
+        num_ranks: int, local_micro_batch_size: int, global_num_samples: int, gradient_accumulation_steps: int
+    ) -> int:
         """Calculates the number of steps given the global
-        number of samples, local micro batch size and number of ranks.
+        number of samples, local micro batch size, number of ranks and gradient accumulation steps.
 
         Args:
             num_ranks (int): Global number of ranks.
             local_micro_batch_size (int): Local micro batch size on single rank.
             global_num_samples (int): Global number of samples.
+            gradient_accumulation_steps (int): Number of gradient accumulation steps.
 
         Returns:
             int: Number of steps.
         """
-        return global_num_samples // (num_ranks * local_micro_batch_size)
+        return global_num_samples // (num_ranks * local_micro_batch_size * gradient_accumulation_steps)
 
     @staticmethod
     def get_num_steps_from_num_tokens(
-        num_ranks: int, local_micro_batch_size: int, global_num_tokens: int, sequence_length: int
+        num_ranks: int,
+        local_micro_batch_size: int,
+        global_num_tokens: int,
+        sequence_length: int,
+        gradient_accumulation_steps: int,
     ) -> int:
         """Calculates the number of steps given the global
-        number of tokens, local micro batch size and number of ranks.
-        This helper function is primarily used to calculate the number of batches to
-        skip when resuming a dataloader during warmstart.
+        number of tokens, local micro batch size number of ranks and gradient accumulation steps.
 
         Args:
             num_ranks (int): Global number of ranks.
             local_micro_batch_size (int): Local micro batch size on single rank.
             global_num_tokens (int): Global number of tokens.
             sequence_length (int): Sequence length of the model.
+            gradient_accumulation_steps (int): Number of gradient accumulation steps.
 
         Returns:
             int: Number of steps.
         """
         global_num_samples = global_num_tokens // sequence_length
         return NumberConversion.get_num_steps_from_num_samples(
-            num_ranks=num_ranks, local_micro_batch_size=local_micro_batch_size, global_num_samples=global_num_samples
+            num_ranks=num_ranks,
+            local_micro_batch_size=local_micro_batch_size,
+            global_num_samples=global_num_samples,
+            gradient_accumulation_steps=gradient_accumulation_steps,
         )
 
     @staticmethod
     def get_num_tokens_from_num_steps_callable(
-        num_ranks: int, local_micro_batch_size: int, sequence_length: int
+        num_ranks: int, local_micro_batch_size: int, sequence_length: int, gradient_accumulation_steps: int
     ) -> Callable[[int], int]:
         """Returns a callable that calculates the number of global tokens given the number of steps done.
 
@@ -139,11 +153,18 @@ class NumberConversion:
             num_ranks (int): Global number of ranks.
             local_micro_batch_size (int): Local micro batch size on single rank.
             sequence_length (int): Sequence length of the model.
+            gradient_accumulation_steps (int): Number of gradient accumulation steps.
 
         Returns:
             Callable[[int], int]: Callable that calculates the number of global tokens.
         """
-        return lambda num_steps_done: num_steps_done * num_ranks * local_micro_batch_size * sequence_length
+        return (
+            lambda num_steps_done: num_steps_done
+            * num_ranks
+            * local_micro_batch_size
+            * sequence_length
+            * gradient_accumulation_steps
+        )
 
     @staticmethod
     def get_last_step_from_checkpoint_path(checkpoint_path: Path) -> int:
@@ -156,15 +177,24 @@ class NumberConversion:
             int: Last step from the checkpoint path.
         """
         # Regex pattern to match 'num_steps_' followed by digits
-        pattern = r"num_steps_(\d+)"
-        match = re.search(pattern, str(checkpoint_path))
+        pattern = r"seen_steps_(\d+)"
+        num_seen_steps = NumberConversion._get_checkpoint_parameter_value(pattern, str(checkpoint_path))
+        return num_seen_steps - 1
 
-        # Extract the number of steps if a match is found
-        if match:
-            num_steps = int(match.group(1))  # Group 1 contains the digits after 'num_steps_'
-        else:
-            raise ValueError(f"No match found for pattern {pattern} in {checkpoint_path}")
-        return num_steps - 1
+    @staticmethod
+    def get_num_seen_steps_from_checkpoint_path(checkpoint_path: Path) -> int:
+        """Returns the number of seen steps from the checkpoint path."
+
+        Args:
+            checkpoint_path (Path): Path to the checkpoint file.
+
+        Returns:
+            int: Number of seen steps from the checkpoint path.
+        """
+        # Regex pattern to match 'num_steps_' followed by digits
+        pattern = r"seen_steps_(\d+)"
+        num_seen_steps = NumberConversion._get_checkpoint_parameter_value(pattern, str(checkpoint_path))
+        return num_seen_steps
 
     @staticmethod
     def get_global_num_seen_tokens_from_checkpoint_path(checkpoint_path: Path) -> int:
@@ -177,22 +207,34 @@ class NumberConversion:
             int: Num seen tokens from the checkpoint path.
         """
         # Regex pattern to match 'num_steps_' followed by digits
-        pattern = r"num_tokens_(\d+)"
-        match = re.search(pattern, str(checkpoint_path))
-
-        # Extract the number of steps if a match is found
-        if match:
-            num_tokens = int(match.group(1))  # Group 1 contains the digits after 'num_tokens_'
-        else:
-            raise ValueError(f"No match found for pattern {pattern} in {checkpoint_path}")
-        return num_tokens
+        pattern = r"seen_tokens_(\d+)"
+        num_seen_tokens = NumberConversion._get_checkpoint_parameter_value(pattern, str(checkpoint_path))
+        return num_seen_tokens
 
     @staticmethod
-    def get_num_steps_from_num_tokens_and_checkpoint_path(checkpoint_path: Path, global_num_tokens: int) -> int:
+    def get_global_num_target_tokens_from_checkpoint_path(checkpoint_path: Path) -> int:
+        """Returns the global num target tokens from the checkpoint path.
+
+        Args:
+            checkpoint_path (Path): Path to the checkpoint file.
+
+        Returns:
+            int: Num target tokens from the checkpoint path.
+        """
+        # Regex pattern to match 'num_steps_' followed by digits
+        pattern = r"target_tokens_(\d+)"
+        num_target_tokens = NumberConversion._get_checkpoint_parameter_value(pattern, str(checkpoint_path))
+        return num_target_tokens
+
+    @staticmethod
+    def get_num_target_steps_from_checkpoint_path(checkpoint_path: Path) -> int:
         tokens_per_step = NumberConversion.get_global_num_seen_tokens_from_checkpoint_path(checkpoint_path) / (
             NumberConversion.get_last_step_from_checkpoint_path(checkpoint_path) + 1
         )
-        num_steps = global_num_tokens // tokens_per_step
-        if isinstance(num_steps, float) and not num_steps.is_integer():
-            raise ValueError(f"Number of steps calculated is not an integer. {num_steps}")
-        return int(num_steps)
+
+        global_num_target_tokens = NumberConversion.get_global_num_target_tokens_from_checkpoint_path(checkpoint_path)
+
+        num_target_steps = global_num_target_tokens // tokens_per_step
+        if isinstance(num_target_steps, float) and not num_target_steps.is_integer():
+            raise ValueError(f"Number of steps calculated is not an integer. {num_target_steps}")
+        return int(num_target_steps)
