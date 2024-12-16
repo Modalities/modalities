8e2fd79197d7f219da3be281a12d85d99a55cc72
ajude <alex.jude@iais.fraunhofer.de>
2024-12-16 11:49:34 +0100

    chore(tokenizer wrapper): Adapted a wrapper for sp tokenizer.

--- src/modalities/models/huggingface_adapters/hf_adapter.py
+++ src/modalities/models/huggingface_adapters/hf_adapter.py
@@ -1,15 +1,17 @@
 import json
 from dataclasses import dataclass
 from pathlib import PosixPath
-from typing import Any, Optional
+from typing import Any, Optional, Union, List, Tuple
 
 import torch
-from transformers import PretrainedConfig, PreTrainedModel
-from transformers.utils import ModelOutput
+from torch import TensorType
+from transformers import PretrainedConfig, PreTrainedModel, PreTrainedTokenizerBase, PreTrainedTokenizer
+from transformers.tokenization_utils_base import TextInput, PreTokenizedInput, EncodedInput, TruncationStrategy
+from transformers.utils import ModelOutput, PaddingStrategy
 
 from modalities.exceptions import ConfigError
 from modalities.models.model import NNModel
-from modalities.models.utils import ModelTypeEnum, get_model_from_config
+from modalities.models.utils import ModelTypeEnum, get_model_from_config, get_tokenizer_from_config
 
 
 class HFModelAdapterConfig(PretrainedConfig):
@@ -142,6 +144,58 @@ class HFModelAdapter(PreTrainedModel):
             "input_ids": input_ids,
             "attention_mask": attention_mask,
         }
+
+
+class HFTokenizerAdapter(PreTrainedTokenizer):
+    # config_class = HFModelAdapterConfig
+    def __init__(
+            self, config: HFModelAdapterConfig, **kwargs
+    ):
+        super().__init__(**kwargs)
+
+        self.tokenizer = get_tokenizer_from_config(config.config, "tokenizer")
+        self.vocab_size = self.tokenizer.vocab_size
+        self.eos_token = config.eos_token if hasattr(config, 'eos_token') else "</s>"
+        self.bos_token = config.bos_token if hasattr(config, 'bos_token') else "<s>"
+        self.pad_token = config.pad_token if hasattr(config, 'pad_token') else "<pad>"
+        self.init_kwargs = kwargs
+
+    @classmethod
+    def from_pretrained(cls, pretrained_model_name_or_path, *model_args, **kwargs):
+        # Load the configuration
+        config = HFModelAdapterConfig.from_pretrained(pretrained_model_name_or_path, **kwargs)
+
+        # Create a new tokenizer instance
+        tokenizer = cls(config, **kwargs)
+
+        return tokenizer
+
+    def encode(
+            self,
+            text: Union[TextInput, PreTokenizedInput, EncodedInput],
+            text_pair: Optional[Union[TextInput, PreTokenizedInput, EncodedInput]] = None,
+            add_special_tokens: bool = True,
+            padding: Union[bool, str, PaddingStrategy] = False,
+            truncation: Union[bool, str, TruncationStrategy] = None,
+            max_length: Optional[int] = None,
+            stride: int = 0,
+            return_tensors: Optional[Union[str, TensorType]] = None,
+            **kwargs,
+    ) -> List[int]:
+        return self.tokenizer.tokenize(text)
+
+    def convert_tokens_to_ids(self, token):
+        return self.tokenizer.get_token_id(token=token)
+
+    def decode(
+            self,
+            token_ids: Union[int, List[int], "np.ndarray", "torch.Tensor", "tf.Tensor"],
+            skip_special_tokens: bool = False,
+            clean_up_tokenization_spaces: bool = None,
+            **kwargs,
+    ) -> str:
+        return self.tokenizer.decode(token_ids)
+
 
 
 @dataclass
