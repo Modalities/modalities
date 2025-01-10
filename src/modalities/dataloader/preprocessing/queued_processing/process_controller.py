6f542873ea6bf4a0f7d1f426a7c363dd48009a89
Max Luebbering <le1nux@users.noreply.github.com>
2025-01-10 01:02:34 +0100

    refactor: generalized multiprocesssing system

--- src/modalities/dataloader/preprocessing/queued_processing/process_controller.py
+++ src/modalities/dataloader/preprocessing/queued_processing/process_controller.py
@@ -1,10 +1,10 @@
 import multiprocessing as mp
 from dataclasses import dataclass
-from typing import Callable
+from multiprocessing.synchronize import Event
 
 import tqdm
 
-from modalities.dataloader.preprocessing.queued_processing.queued_processing import Processor
+from modalities.dataloader.preprocessing.queued_processing.processors import Processor
 from modalities.utils.logging import get_logger
 
 
@@ -16,19 +16,50 @@ class PipelineStep:
 
 
 class ProcessController:
-    def __init__(self, pipeline_steps: list[PipelineStep], populate_jobs: Callable):
+    def __init__(self, pipeline_steps: list[PipelineStep], stop_event: Event, join_timeout: int = 5):
         """Initializes the ProcessController
         Each pipeline step contains a list of processors that retrieve the data from the input queue,
         process it and if necessary put it into the output queue of the next step.
         """
         self._pipeline_steps = pipeline_steps
-        self._populate_jobs = populate_jobs
+        self._stop_event = stop_event
+        self._join_timeout = join_timeout
+
+    def join_processors_in_step(self, step: PipelineStep):
+        """Joins the processors of a pipeline step
+        If the stop_event is set, the processors are terminated
+        """
+        # poison the input queues of the processors
+        for _ in tqdm.tqdm(step.processors, desc=f"Poisoning {step.name} processes"):
+            if step.input_queue is not None:
+                step.input_queue.put(None)
+
+        # join the processors
+        num_exits = 0
+        while num_exits < len(step.processors):
+            processor = step.processors[num_exits]
+            if self._stop_event.is_set():
+                try:
+                    processor.terminate()
+                except Exception as e:
+                    # if we can't terminate the processor, we continue with the next one
+                    get_logger().error(
+                        f"Error while terminating processor {processor.full_name}: {e}. "
+                        "Continuing with the next processor."
+                    )
+                    num_exits += 1
+                    continue
+                get_logger().info(f"Terminated processor {processor.full_name}")
+                num_exits += 1
+            else:
+                try:
+                    processor.join(timeout=self.join_timeout)
+                except TimeoutError:
+                    continue
+                get_logger().info(f"Joined processor {processor.full_name}")
+                num_exits += 1
 
     def run(self):
-        # add the jobs to the input queues
-        get_logger().info("Populating jobs")
-        self._populate_jobs()
-
         # start the processors
         for step in self._pipeline_steps:
             get_logger().info(f"Starting processors for step {step.name}")
@@ -38,9 +69,4 @@ class ProcessController:
         # wait for the processors to finish
         for step in self._pipeline_steps:
             get_logger().info(f"Stopping {step.name} processes...")
-            for _ in tqdm.tqdm(step.processors, desc=f"Poisoning {step.name} processes"):
-                step.input_queue.put(None)
-            get_logger().info(f"Waiting for processors in step {step.name} to finish")
-
-            for processor in tqdm.tqdm(step.processors, desc=f"Joining {step.name} processes"):
-                processor.join()
+            self.join_processors_in_step(step)
--- src/modalities/dataloader/preprocessing/queued_processing/processing_strategy_if.py
+++ src/modalities/dataloader/preprocessing/queued_processing/processing_strategy_if.py
@@ -1,9 +1,9 @@
 from abc import ABC
-from typing import Any
+from typing import Any, Optional
 
 
 class ProcessingStrategyIF(ABC):
-    def process(self, item: Any) -> dict[str, Any] | None:
+    def process(self, item: Optional[Any] = None) -> dict[str, Any] | None:
         raise NotImplementedError
 
     def __enter__(self):
@@ -13,4 +13,4 @@ class ProcessingStrategyIF(ABC):
         raise NotImplementedError
 
     def __exit__(self, exc_type, exc_value, traceback):
-        raise
+        raise NotImplementedError
