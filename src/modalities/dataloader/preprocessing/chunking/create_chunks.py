bc01afe2d35549908be4b387d530e8e035050612
Max Luebbering <le1nux@users.noreply.github.com>
2025-01-15 21:31:18 +0100

    feat: implemented chunking calculation

---
+++ src/modalities/dataloader/preprocessing/chunking/create_chunks.py
@@ -0,0 +1,24 @@
+import math
+
+
+class Chunking:
+    @staticmethod
+    def _get_chunk_range(num_chunks: int, num_samples: int, chunk_id: int) -> list[int]:
+        # get the maximum chunk size given the number of samples and number of chunks
+        chunk_size_complete = math.ceil(num_samples / num_chunks)
+
+        # the number of complete chunks, i.e., the chunks having the maximum chunk size
+        num_complete_chunks = num_samples % num_chunks
+        if num_complete_chunks == 0:
+            num_complete_chunks = num_chunks
+
+        start = chunk_size_complete * min(num_complete_chunks, chunk_id) + max((chunk_id - num_complete_chunks), 0) * (
+            chunk_size_complete - 1
+        )
+
+        if chunk_id < num_complete_chunks:
+            end = start + chunk_size_complete
+        else:
+            end = start + chunk_size_complete - 1
+
+        return [start, end]
---
+++ tests/dataloader/preprocessing/chunking/test_create_chunks.py
@@ -0,0 +1,77 @@
+import pytest
+
+from modalities.dataloader.preprocessing.chunking.create_chunks import Chunking
+
+
+@pytest.mark.parametrize(
+    "num_chunks, num_samples, expected_chunk_indices",
+    [
+        (5, 10, [[0, 2], [2, 4], [4, 6], [6, 8], [8, 10]]),  # num_chunks << num_samples (2 samples per chunk exactly)
+        (
+            5,
+            8,
+            [[0, 2], [2, 4], [4, 6], [6, 7], [7, 8]],
+        ),  # num_chunks << num_samples (2 samples per chunk only for the first 3 chunks, 1 otherwise)
+        (1, 10, [[0, 10]]),  # num_chunks == num_samples (all samples in one chunk)
+        (0, 10, []),  # num_chunks == 0, no chunks
+        (
+            20,
+            10,
+            [
+                [0, 1],
+                [1, 2],
+                [2, 3],
+                [3, 4],
+                [4, 5],
+                [5, 6],
+                [6, 7],
+                [7, 8],
+                [8, 9],
+                [9, 10],
+                [10, 10],
+                [10, 10],
+                [10, 10],
+                [10, 10],
+                [10, 10],
+                [10, 10],
+                [10, 10],
+                [10, 10],
+                [10, 10],
+                [10, 10],
+            ],
+        ),  # num_chunks >> num_samples (1 sample per chunk only for the first 10 chunks, 0 otherwise)
+        (
+            15,
+            6,
+            [
+                [0, 1],
+                [1, 2],
+                [2, 3],
+                [3, 4],
+                [4, 5],
+                [5, 6],
+                [6, 6],
+                [6, 6],
+                [6, 6],
+                [6, 6],
+                [6, 6],
+                [6, 6],
+                [6, 6],
+                [6, 6],
+                [6, 6],
+            ],
+        ),  # num_chunks >> num_samples (1 sample per chunk only for the first 6 chunks, 0 otherwise)
+        (
+            10,
+            1,
+            [[0, 1], [1, 1], [1, 1], [1, 1], [1, 1], [1, 1], [1, 1], [1, 1], [1, 1], [1, 1]],
+        ),  # num_chunks >> num_samples (1 sample per chunk only for the first chunk, 0 otherwise)
+        (0, 10, []),  # num_chunks == 0, no chunks
+    ],
+)
+def test__get_chunk_range(num_chunks: int, num_samples: int, expected_chunk_indices: list[list[int]]):
+    chunk_indices = [
+        Chunking._get_chunk_range(num_chunks=num_chunks, num_samples=num_samples, chunk_id=chunk_id)
+        for chunk_id in range(num_chunks)
+    ]
+    assert chunk_indices == expected_chunk_indices
