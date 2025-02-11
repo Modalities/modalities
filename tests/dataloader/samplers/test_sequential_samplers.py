9886503784c28114531af608c7205b485d0eef6f
mali-git <ali-mehdi@live.de>
2025-02-11 10:14:43 +0100

    chore: add comment

--- tests/dataloader/samplers/test_sequential_samplers.py
+++ tests/dataloader/samplers/test_sequential_samplers.py
@@ -13,11 +13,10 @@ class DummyDataset(Dataset):
 
 def test_distributed_setting():
     num_samples = 10
-    # Ensure each rank gets same samples
     dataset = DummyDataset(num_samples)
     world_size = 3
     samplers = [SequentialSampler(dataset)for _ in range(world_size)]
     
     expected_indices = list(range(num_samples))
-    
+    # Ensures that all ranks receive the exact same samples in the same order
     assert all(list(sampler) == expected_indices for sampler in samplers)
