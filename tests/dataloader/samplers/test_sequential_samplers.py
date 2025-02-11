3601a8e910dbf38bfb093cdac4d9e68b882226cf
mali-git <ali-mehdi@live.de>
2025-02-11 10:07:42 +0100

    test: test sequential sampler

---
+++ tests/dataloader/samplers/test_sequential_samplers.py
@@ -0,0 +1,23 @@
+from torch.utils.data import SequentialSampler, Dataset
+
+class DummyDataset(Dataset):
+    def __init__(self, num_samples):
+        self.data = list(range(num_samples))
+
+    def __len__(self):
+        return len(self.data)
+
+    def __getitem__(self, index):
+        return self.data[index]
+
+
+def test_distributed_setting():
+    num_samples = 10
+    # Ensure each rank gets same samples
+    dataset = DummyDataset(num_samples)
+    world_size = 3
+    samplers = [SequentialSampler(dataset)for _ in range(world_size)]
+    
+    expected_indices = list(range(num_samples))
+    
+    assert all(list(sampler) == expected_indices for sampler in samplers)
