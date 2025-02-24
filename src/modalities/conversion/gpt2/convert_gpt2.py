6564452db5505d3c061ef9236ea9afa76f262168
Timm Ruland <timm.heine.ruland@iais.fraunhofer.de>
2025-02-24 11:35:59 +0100

    refactor(huggingface): Minor code improvement.

--- src/modalities/conversion/gpt2/convert_gpt2.py
+++ src/modalities/conversion/gpt2/convert_gpt2.py
@@ -198,7 +198,7 @@ def _copy_weights_base_modules(m1: nn.Linear | nn.LayerNorm, m2: nn.Linear | nn.
 
 
 def _transfer_model_code(output_dir: str):
-    """Copies the required model code to the output directory.
+    """Copies the required model code to the output directory and replaces modalities imports.
        This allows the converted model to be used without the modalities package via:
        >>> from transformers import AutoModelForCausalLM
        >>> model = AutoModelForCausalLM.from_pretrained("path/to/converted/model", trust_remote_code=True)
@@ -206,12 +206,19 @@ def _transfer_model_code(output_dir: str):
     Args:
         output_dir (str): Directory of the converted model.
     """
+    _copy_model_files(output_dir)
+    _change_modalities_import_to_relative_import(output_dir)
+
+
+def _copy_model_files(output_dir: str):
     source_dir = os.path.dirname(__file__)
     modeling_gpt2_path = os.path.join(source_dir, "modeling_gpt2.py")
     configuration_gpt2_path = os.path.join(source_dir, "configuration_gpt2.py")
     shutil.copy(modeling_gpt2_path, output_dir)
     shutil.copy(configuration_gpt2_path, output_dir)
 
+
+def _change_modalities_import_to_relative_import(output_dir: str):
     target_modeling_file = os.path.join(output_dir, "modeling_gpt2.py")
     with open(target_modeling_file, "r") as file:
         content = file.read()
