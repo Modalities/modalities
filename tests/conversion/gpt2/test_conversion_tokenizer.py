25ba10bce2a4c11738259efcff08037d603099f3
Timm Ruland <timm.heine.ruland@iais.fraunhofer.de>
2025-02-27 15:34:32 +0100

    test(huggingface): Added detokenization test for converted tokenizer.
    Also improved text examples for tokenization test.

--- tests/conversion/gpt2/test_conversion_tokenizer.py
+++ tests/conversion/gpt2/test_conversion_tokenizer.py
@@ -10,9 +10,17 @@ from modalities.tokenization.tokenizer_wrapper import PreTrainedSPTokenizer
 def test_converted_tokenizer_produces_same_tokens_as_original(
     converted_tokenizer: LlamaTokenizer, sp_tokenizer: PreTrainedSPTokenizer, text: str
 ):
-    converted_tokens = converted_tokenizer(text)
-    sp_tokens = sp_tokenizer.tokenize(text)
-    assert converted_tokens["input_ids"] == sp_tokens
+    converted_token_ids = converted_tokenizer(text)
+    sp_token_ids = sp_tokenizer.tokenize(text)
+    assert converted_token_ids["input_ids"] == sp_token_ids
+
+
+def test_converted_tokenizer_detokenizes_same_as_original(
+    converted_tokenizer: LlamaTokenizer, sp_tokenizer: PreTrainedSPTokenizer, token_ids: list[int]
+):
+    converted_tokens = converted_tokenizer.decode(token_ids)
+    sp_tokens = sp_tokenizer.decode(token_ids)
+    assert converted_tokens == sp_tokens
 
 
 @pytest.fixture
@@ -31,6 +39,23 @@ def tokenizer_model_file() -> str:
     return "data/tokenizer/sentencepiece_dclm/en_32k_tokenizer.model"
 
 
-@pytest.fixture(params=["This is a test sentence.", "Hello, my dog is cute", "the secret phrase is ossifrage"])
+@pytest.fixture(
+    params=[
+        "<unk><s></s><pad><eod><placeholder_tok_0><placeholder_tok_1><placeholder_tok_2>",
+        "<s>Hello,\n my dog is cute",
+        "the secret phrase is ossifrage",
+    ]
+)
 def text(request: pytest.FixtureRequest) -> str:
+    return request.param
+
+
+@pytest.fixture(
+    params=[
+        [0, 1, 2, 3, 4, 5, 6, 7, 8, 9],
+        [0, 20527, 1, 20527, 2, 20527, 3, 20527, 4, 20527, 5, 20527, 6, 20527],
+        [1, 20527],
+    ]
+)
+def token_ids(request: pytest.FixtureRequest) -> list[int]:
     return request.param
