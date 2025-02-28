f3ade58eb260e0336e6c7c8c3b98e5d89f5de5d0
Timm Ruland <timm.heine.ruland@iais.fraunhofer.de>
2025-02-28 11:31:47 +0100

    feat(huggingface): Changed tokenizer model loading in conversion for transformers v5 compatibility.

--- src/modalities/conversion/gpt2/conversion_tokenizer.py
+++ src/modalities/conversion/gpt2/conversion_tokenizer.py
@@ -1,3 +1,7 @@
+import shutil
+import tempfile
+from typing import Iterable
+
 from transformers import LlamaTokenizer
 
 from modalities.tokenization.tokenizer_wrapper import PreTrainedSPTokenizer
@@ -14,15 +18,13 @@ def convert_tokenizer(tokenizer_model_path: str, output_dir: str) -> tuple[int, 
         tuple[int, int]: The bos_token_id, eos_token_id and pad_token_id of the tokenizer.
     """
     sp_tokenizer = PreTrainedSPTokenizer(tokenizer_model_path)
-    split_special_tokens = is_splitting_special_tokens(sp_tokenizer)
-    # FIXME: from_pretrained from file will not work in transformers v5 anymore:
-    # FutureWarning: Calling LlamaTokenizer.from_pretrained() with the path to a single file or url
-    #                is deprecated and won't be possible anymore in v5.
-    #                Use a model identifier or the path to a directory instead.
+    split_special_tokens = _is_splitting_special_tokens(sp_tokenizer)
+    # Setting the special tokens to None in order to let the internal SentencePiece tokenizer handle them.
     special_tokens, special_token_ids = _dummy_special_tokens()
-    hf_tokenizer = LlamaTokenizer.from_pretrained(
-        tokenizer_model_path, split_special_tokens=split_special_tokens, **special_tokens, **special_token_ids
-    )
+    for tokenizer_model_dir in _create_tokenizer_directory(tokenizer_model_path):
+        hf_tokenizer = LlamaTokenizer.from_pretrained(
+            tokenizer_model_dir, split_special_tokens=split_special_tokens, **special_tokens, **special_token_ids
+        )
     hf_tokenizer.add_bos_token = False
     hf_tokenizer.add_eos_token = False
     hf_tokenizer.legacy = True
@@ -36,7 +38,7 @@ def convert_tokenizer(tokenizer_model_path: str, output_dir: str) -> tuple[int, 
     )
 
 
-def is_splitting_special_tokens(sp_tokenizer: PreTrainedSPTokenizer) -> bool:
+def _is_splitting_special_tokens(sp_tokenizer: PreTrainedSPTokenizer) -> bool:
     if (bos := sp_tokenizer.tokenizer.bos_id()) >= 0:
         test_token_id = bos
     elif (eos := sp_tokenizer.tokenizer.eos_id()) >= 0:
@@ -48,6 +50,23 @@ def is_splitting_special_tokens(sp_tokenizer: PreTrainedSPTokenizer) -> bool:
     else:
         return False
     return len(sp_tokenizer.tokenize(sp_tokenizer.tokenizer.id_to_piece(test_token_id))) > 1
+
+
+def _create_tokenizer_directory(tokenizer_model_path: str) -> Iterable[str]:
+    """Moves the tokenizer model to a temporary directory and yields the path to the model.
+       The model is moved to a temporary directory because the from_pretrained method of
+       the LlamaTokenizer class requires a directory path instead of a file path from transformers v5 on.
+       When the returned iterator is exhausted, the temporary directory is deleted.
+
+    Args:
+        tokenizer_model_path (str): Path to the tokenizer model file.
+
+    Yields:
+        Iterable[str]: Path to the temporary directory.
+    """
+    with tempfile.TemporaryDirectory() as temp_dir:
+        shutil.copy(tokenizer_model_path, f"{temp_dir}/tokenizer.model")
+        yield temp_dir
 
 
 def _dummy_special_tokens() -> tuple[dict[str, str], dict[str, int]]:
