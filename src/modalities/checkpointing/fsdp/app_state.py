9bee05fc71ac083160c7ddea3c630bc99904798f
Max Luebbering <2804731+le1nux@users.noreply.github.com>
2025-03-06 23:44:24 +0100

    refactor: made AppState load state only once.

--- src/modalities/checkpointing/fsdp/app_state.py
+++ src/modalities/checkpointing/fsdp/app_state.py
@@ -1,6 +1,5 @@
-
-from torch.distributed.checkpoint.stateful import Stateful
 from torch.distributed.checkpoint.state_dict import get_state_dict, set_state_dict
+from torch.distributed.checkpoint.stateful import Stateful
 
 
 class AppState(Stateful):
@@ -16,22 +15,35 @@ class AppState(Stateful):
     """
 
     def __init__(self, model, optimizer=None):
-        self.model = model
-        self.optimizer = optimizer
+        self._model = model
+        self._optimizer = optimizer
+        self._is_loaded = False
+
+    @property
+    def is_loaded(self):
+        return self._is_loaded
+
+    @property
+    def model(self):
+        return self._model
+
+    @property
+    def optimizer(self):
+        return self._optimizer
 
     def state_dict(self):
-        # this line automatically manages FSDP FQN's, as well as sets the default state dict type to FSDP.SHARDED_STATE_DICT
-        model_state_dict, optimizer_state_dict = get_state_dict(self.model, self.optimizer)
-        return {
-            "model": model_state_dict,
-            "optim": optimizer_state_dict
-        }
+        # this line automatically manages FSDP FQN's, as well as sets the default
+        # state dict type to FSDP.SHARDED_STATE_DICT
+        model_state_dict, optimizer_state_dict = get_state_dict(self._model, self._optimizer)
+        return {"model": model_state_dict, "optim": optimizer_state_dict}
 
     def load_state_dict(self, state_dict):
         # sets our state dicts on the model and optimizer, now that we've loaded
+        if self._is_loaded:
+            raise RuntimeError(
+                "Cannot call load_state_dict twice on the same AppState object. " "State dict has already been loaded."
+            )
         set_state_dict(
-            self.model,
-            self.optimizer,
-            model_state_dict=state_dict["model"],
-            optim_state_dict=state_dict["optim"]
+            self._model, self._optimizer, model_state_dict=state_dict["model"], optim_state_dict=state_dict["optim"]
         )
+        self._is_loaded = True
