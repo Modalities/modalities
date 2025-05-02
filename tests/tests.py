e2816f600ab646d5d831c6e2ed15fa6f8076db1b
Max Luebbering <2804731+le1nux@users.noreply.github.com>
2025-05-02 15:52:47 +0200

    refactor: improved the usability of the tests CMD interface

--- tests/tests.py
+++ tests/tests.py
@@ -151,17 +151,43 @@ def subprocess_run(command: str) -> None:
         raise Exception(f"Subproces run failed with command {command}")
 
 
-def main(include_torchrun_tests: bool = False, devices: Optional[str] = None, test_name_filter: str = None) -> None:
+def main(
+    include_main_tests: bool = False,
+    include_torchrun_tests: bool = False,
+    include_examples: bool = False,
+    devices: Optional[str] = None,
+    main_tests_name_filter: str = None,
+) -> None:
     """
-    Run tests on cpu, single gpu and multiple gpus
+    Run tests and examples.
+    There are two types of tests:
+    - CPU and GPU (distributed and non-distributed) tests that don't require to be started via torchrun
+        (included via include_main_tests)
+    - distributed tests that require to be started via torchrun (included via include_torchrun_tests)
+    If any of the main tests is not provided with enough GPUs, it will be skipped.
+
+    There are several examples whose configs are being executed if include_examples is set to True.
+    The examples are:
+    - getting started example
+    - checkpoint conversion example (based on getting started example)
+    - warmstart example
 
     Examples:
-    python tests/tests.py: run CPU tests + single-GPU tests on GPU 0 + multi-GPU tests on GPUs 0/1
-    python tests/tests.py --devices 4,5: same as above but using devices 4/5
-    python tests/tests.py --cpu: run CPU tests
-    python tests/tests.py --single-gpu --devices 3: run CPU tests + single-GPU tests on GPU 3
-    python tests/tests.py --multi-gpu --devices 3,4: run multi-GPU tests on GPUs 3/4
+    python tests/tests.py: Run no tests or examples
+    python tests/tests.py --devices 4,5: same as above
+    python tests/tests.py --include_main_tests: run the main tests
+    python tests/tests.py --include_torchrun_tests: run the torchrun tests
+    python tests/tests.py --include_examples: run the examples
+    python tests/tests.py --include_main_tests --include_torchrun_tests --include_examples: run all tests and examples
+    python tests/tests.py --include_main_tests --include_torchrun_tests --include_examples --devices 0,1,2,3:
+        run all tests and examples on devices 0, 1, 2 and 3
     """
+    if not any([include_main_tests, include_torchrun_tests, include_examples]):
+        print(
+            f"No tests selected to run ({include_main_tests=}, {include_torchrun_tests=}, "
+            f"{include_examples=}). Exiting."
+        )
+        return
 
     if devices is None:
         device_ids = list(range(torch.cuda.device_count()))
@@ -174,74 +200,82 @@ def main(include_torchrun_tests: bool = False, devices: Optional[str] = None, te
                 f"Specified devices = {devices}"
             )
 
-    # only run tests on max 2 devices
+    # only run tests on max 4 devices
     device_ids = device_ids[:4]
+    print("> Test setup: ")
+    print(f"> {include_main_tests=}, {include_torchrun_tests=}, {include_examples=}")
+    print(f"> {device_ids=}")
 
-    # run cpu / gpu tests not requiring torchrun
-    print(f"\n=== RUN TESTS on CPU and CUDA devices {device_ids} ===")
-    command_unit_tests = (
-        f"cd {_ROOT_DIR} && CUDA_VISIBLE_DEVICES="
-        f"{','.join(map(str, device_ids)) if len(device_ids) >0 else ''} python -m pytest"
-    )
-    if test_name_filter is not None:
-        command_unit_tests += f" -k {test_name_filter}"
-    subprocess_run(command_unit_tests)
+    if include_main_tests:
+        # run cpu / gpu tests not requiring torchrun
+        print(f"\n=== RUN MAIN TESTS on CPU and CUDA devices {device_ids} ===")
+        command_unit_tests = (
+            f"cd {_ROOT_DIR} && CUDA_VISIBLE_DEVICES="
+            f"{','.join(map(str, device_ids)) if len(device_ids) >0 else ''} python -m pytest"
+        )
+        if main_tests_name_filter is not None:
+            command_unit_tests += f" -k {main_tests_name_filter}"
+        subprocess_run(command_unit_tests)
 
-    if not include_torchrun_tests:
-        print("\n=== SKIPPED torchrun tests ===")
-        print("\n=== DONE ===")
-        return
+    if len(device_ids) < 2:
+        exit("ERROR! Need at least 2 devices to run torchrun tests and examples.")
 
-    if len(device_ids) < 4:
-        exit("ERROR! Need at least 2 devices to run torchrun tests.")
-    # distributed tests
-    print("\n=== RUN TORCHRUN TESTS ===")
-    run_distributed_tests_directory = _ROOT_DIR / "tests"
-    run_distributed_tests_script = _ROOT_DIR / "tests" / "run_distributed_tests.sh"
-    assert isfile(run_distributed_tests_script), f"ERROR! {run_distributed_tests_script} does not exist."
-    command_end_to_end_tests = (
-        f"cd {run_distributed_tests_directory}; bash run_distributed_tests.sh "
-        f"{' '.join(map(str, device_ids))} --no-cov"
-    )
-    subprocess_run(command_end_to_end_tests)
+    if include_torchrun_tests:
+        # distributed tests
+        print("\n=== RUN TORCHRUN TESTS ===")
+        run_distributed_tests_directory = _ROOT_DIR / "tests"
+        run_distributed_tests_script = _ROOT_DIR / "tests" / "run_distributed_tests.sh"
+        assert isfile(run_distributed_tests_script), f"ERROR! {run_distributed_tests_script} does not exist."
+        command_end_to_end_tests = (
+            f"cd {run_distributed_tests_directory}; bash run_distributed_tests.sh "
+            f"{' '.join(map(str, device_ids[:2]))} --no-cov"
+        )
+        subprocess_run(command_end_to_end_tests)
 
-    # getting started example
-    print("\n=== RUN GETTING STARTED EXAMPLE ===")
-    run_getting_started_example_directory = _ROOT_DIR / "tutorials" / "getting_started"
-    run_getting_started_example_script = (
-        _ROOT_DIR / "tutorials" / "getting_started" / "scripts" / "run_getting_started_example.sh"
-    )
-    assert isfile(run_getting_started_example_script), f"ERROR! {run_getting_started_example_script} does not exist."
-    command_getting_started_example = f"cd {run_getting_started_example_directory}; "
-    command_getting_started_example += f"bash scripts/run_getting_started_example.sh {' '.join(map(str, device_ids))}"
-    date_of_run = datetime.now().strftime("%Y-%m-%d__%H-%M-%S")
-    subprocess_run(command_getting_started_example)
+    if include_examples:
+        # getting started example
+        print("\n=== RUN GETTING STARTED EXAMPLE ===")
+        run_getting_started_example_directory = _ROOT_DIR / "tutorials" / "getting_started"
+        run_getting_started_example_script = (
+            _ROOT_DIR / "tutorials" / "getting_started" / "scripts" / "run_getting_started_example.sh"
+        )
+        assert isfile(
+            run_getting_started_example_script
+        ), f"ERROR! {run_getting_started_example_script} does not exist."
+        command_getting_started_example = f"cd {run_getting_started_example_directory}; "
+        command_getting_started_example += (
+            f"bash scripts/run_getting_started_example.sh {' '.join(map(str, device_ids))}"
+        )
+        date_of_run = datetime.now().strftime("%Y-%m-%d__%H-%M-%S")
+        subprocess_run(command_getting_started_example)
 
-    # checkpoint conversion (based on getting started example)
-    print("\n=== RUN CHECKPOINT CONVERSION (BASED ON GETTING STARTED EXAMPLE) ===")
-    modalities_checkpoint = get_checkpoint_from_getting_started_example(run_getting_started_example_directory)
-    conversion_config_path = replace_checkpoint_in_conversion_config(
-        run_getting_started_example_directory, modalities_checkpoint
-    )
+        # checkpoint conversion (based on getting started example)
+        print("\n=== RUN CHECKPOINT CONVERSION (BASED ON GETTING STARTED EXAMPLE) ===")
+        modalities_checkpoint = get_checkpoint_from_getting_started_example(run_getting_started_example_directory)
+        conversion_config_path = replace_checkpoint_in_conversion_config(
+            run_getting_started_example_directory, modalities_checkpoint
+        )
 
-    run_conversion_script = _ROOT_DIR / "tutorials" / "getting_started" / "scripts" / "run_checkpoint_conversion.sh"
-    assert isfile(run_conversion_script), f"ERROR! {run_conversion_script} does not exist."
-    command_conversion = f"cd {run_getting_started_example_directory}; "
-    command_conversion += f"sh scripts/run_checkpoint_conversion.sh {conversion_config_path} "
-    command_conversion += f"{run_getting_started_example_directory}/checkpoints/{modalities_checkpoint.split('/')[-1]}"
-    subprocess_run(command_conversion)
+        run_conversion_script = _ROOT_DIR / "tutorials" / "getting_started" / "scripts" / "run_checkpoint_conversion.sh"
+        assert isfile(run_conversion_script), f"ERROR! {run_conversion_script} does not exist."
+        command_conversion = f"cd {run_getting_started_example_directory}; "
+        command_conversion += f"sh scripts/run_checkpoint_conversion.sh {conversion_config_path} "
+        command_conversion += (
+            f"{run_getting_started_example_directory}/checkpoints/{modalities_checkpoint.split('/')[-1]}"
+        )
+        subprocess_run(command_conversion)
 
-    check_existence_and_clear_getting_started_example_output(run_getting_started_example_directory, date_of_run)
+        check_existence_and_clear_getting_started_example_output(run_getting_started_example_directory, date_of_run)
 
-    # warmstart example
-    print("\n=== RUN WARMSTART EXAMPLE ===")
-    run_warmstart_example_directory = _ROOT_DIR / "tutorials/warmstart/scripts"
-    run_warmstart_example_script = _ROOT_DIR / "tutorials/warmstart/scripts/pre_train_and_warmstart.sh"
-    assert isfile(run_warmstart_example_script), f"ERROR! {run_warmstart_example_script} does not exist."
-    command_warmstart_example = (
-        f"cd {run_warmstart_example_directory}; sh pre_train_and_warmstart.sh {' '.join(map(str, device_ids))}"
-    )
-    subprocess_run(command_warmstart_example)
+        # warmstart example
+        print("\n=== RUN WARMSTART EXAMPLE ===")
+        run_warmstart_example_directory = _ROOT_DIR / "tutorials/warmstart/scripts"
+        run_warmstart_example_script = _ROOT_DIR / "tutorials/warmstart/scripts/pre_train_and_warmstart.sh"
+        assert isfile(run_warmstart_example_script), f"ERROR! {run_warmstart_example_script} does not exist."
+        command_warmstart_example = (
+            f"cd {run_warmstart_example_directory}; sh pre_train_and_warmstart.sh {' '.join(map(str, device_ids))}"
+        )
+        subprocess_run(command_warmstart_example)
 
     print("\n=== DONE ===")
 
@@ -249,12 +283,24 @@ def main(include_torchrun_tests: bool = False, devices: Optional[str] = None, te
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Description of your program")
     parser.add_argument(
+        "--include_main_tests",
+        action=argparse.BooleanOptionalAction,
+        help="Run the main tests on CPU and GPU.",
+        default=False,
+    )
+    parser.add_argument(
         "--include_torchrun_tests",
         action=argparse.BooleanOptionalAction,
-        help="Run tests and examples requiring torchrun env.",
+        help="Run the tests that require to be launched in a torchrun environment.",
+        default=False,
+    )
+    parser.add_argument(
+        "--include_examples",
+        action=argparse.BooleanOptionalAction,
+        help="Run the examples.",
         default=False,
     )
     parser.add_argument("--devices", type=str, default=None)
-    parser.add_argument("--test_name_filter", type=str, default=None)
+    parser.add_argument("--main_tests_name_filter", type=str, default=None)
     args = vars(parser.parse_args())
     main(**args)
