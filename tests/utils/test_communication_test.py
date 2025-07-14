a23b892f563d569f4481b72c5678b3e3e6919907
rrutmann <richardr@online.de>
2025-07-14 15:22:34 +0200

    test: Run test with multiprocessing

--- tests/utils/test_communication_test.py
+++ tests/utils/test_communication_test.py
@@ -1,21 +1,50 @@
-import os
-
 import pytest
 import torch
+import torch.multiprocessing as mp
 
 from modalities.config.config import ProcessGroupBackendType
-from modalities.running_env.cuda_env import CudaEnv
 from modalities.utils.communication_test import run_communication_test
+from tests.end2end_tests.custom_components import MultiProcessingCudaEnv
 
 
 @pytest.mark.skipif(
-    "RANK" not in os.environ or torch.cuda.device_count() < 2,
-    reason="This test requires 2 GPUs and a torchrun distributed environment.",
+    torch.cuda.device_count() < 2,
+    reason="This test requires 2 GPUs.",
 )
-def test_run_communication_test():
-    """
-    Test to ensure that the communication test runs without errors.
-    This is a simple smoke test to verify that the communication setup is functional.
-    """
-    with CudaEnv(process_group_backend=ProcessGroupBackendType.nccl):
+class TestCommunicationTest:
+    @staticmethod
+    def test_run_communication_test():
+        """
+        Test to ensure that the communication test runs without errors.
+        This is a simple smoke test to verify that the communication setup is functional.
+        """
+        world_size = 2
+        mp.spawn(
+            TestCommunicationTest._test_run_communication_test_wrapper,
+            args=(world_size,),
+            nprocs=world_size,
+            join=True,
+        )
+
+    @staticmethod
+    def _test_run_communication_test_wrapper(
+        process_id: int,
+        world_size: int,
+    ):
+        """This function is a wrapper for the communication test to run it in a multiprocessing setup."""
+        with MultiProcessingCudaEnv(
+            process_group_backend=ProcessGroupBackendType.nccl,
+            global_rank=process_id,
+            local_rank=process_id,
+            world_size=world_size,
+            rdvz_port=22356,
+        ):
+            TestCommunicationTest._test_run_communication_test_impl()
+
+    @staticmethod
+    def _test_run_communication_test_impl():
+        """
+        Implementation of the communication test.
+        This function is called by the multiprocessing wrapper.
+        """
         run_communication_test()
