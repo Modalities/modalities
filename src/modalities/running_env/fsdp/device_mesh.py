fix(logging): Correct number of parameters computation in case of pipeline parallelism.

src/modalities/running_env/fsdp/device_mesh.py
@@ -5,7 +5,6 @@ from pydantic import BaseModel, Field, model_validator
 from torch.distributed.device_mesh import DeviceMesh, init_device_mesh
 
 from modalities.exceptions import ConfigError
-from modalities.util import print_rank_0
 from modalities.utils.logger_utils import get_logger
 
 logger = get_logger("model_factory")
@@ -124,7 +123,7 @@ def get_device_mesh(
             names.append(name)
     names = tuple(names)
     device_mesh = init_device_mesh(device_type, dims, mesh_dim_names=names)
-    print_rank_0(f"{device_mesh=} | {world_size=} | {enable_loss_parallel=}")
+    logger.info(f"{device_mesh=} | {world_size=} | {enable_loss_parallel=}")
     # TODO: Torch Titan had some more checks here. We need to check if we also need those:
     # https://github.com/pytorch/torchtitan/blob/b291ad662493b63d25b038a30a915082d3617baf/torchtitan/distributed/parallel_dims.py#L86-L104
     return device_mesh

src/modalities/util.py
@@ -11,12 +11,14 @@ import torch
 import torch.distributed as dist
 import torch.nn as nn
 from pydantic import ValidationError
+from torch.distributed.device_mesh import DeviceMesh
 from torch.distributed.fsdp import FSDPModule as FSDP2
 from torch.distributed.fsdp import FullyShardedDataParallel as FSDP1
 from torch.distributed.tensor import DTensor
 from torch.types import Number
 
 from modalities.exceptions import TimeRecorderStateError
+from modalities.running_env.fsdp.device_mesh import ParallelismDegrees
 from modalities.running_env.fsdp.reducer import Reducer
 from modalities.utils.typing_utils import FSDPX
 
@@ -164,12 +166,13 @@ def get_local_number_of_trainable_parameters(model: nn.Module) -> int:
     return num_params
 
 
-def get_total_number_of_trainable_parameters(model: FSDPX) -> Number:
+def get_total_number_of_trainable_parameters(model: FSDPX, device_mesh: DeviceMesh | None) -> Number:
     """Returns the total number of trainable parameters across all ranks.
     The model must be sharded with FSDP1 or FSDP2.
 
     Args:
         model (FSDPX): The model for which to calculate the number of trainable parameters.
+        device_mesh (DeviceMesh | None): The device mesh used for distributed training.
 
     Returns:
         Number: The total number of trainable parameters across all ranks.
@@ -216,8 +219,13 @@ def get_total_number_of_trainable_parameters(model: FSDPX) -> Number:
         # >>> parameter_tensor.shape[0] * parameter_tensor.shape[1]
         # 6438912
 
-        total_num_params = sum(p.numel() for p in model.parameters() if p.requires_grad)
-        return total_num_params
+        num_params_tensor = sum(p.numel() for p in model.parameters() if p.requires_grad)
+        if device_mesh is not None and ParallelismDegrees.PP.value in device_mesh.mesh_dim_names:
+            num_params_tensor = torch.tensor(num_params_tensor).cuda()
+            pp_mesh = device_mesh[ParallelismDegrees.PP.value]
+            dist.all_reduce(num_params_tensor, op=dist.ReduceOp.SUM, group=pp_mesh.get_group())
+            return num_params_tensor.item()
+        return num_params_tensor
     else:
         raise ValueError(
             f"Model type {type(model)} is not supported. "