640c49d0ba959e76d49344f3e3a3c32f89f190f5
Max Luebbering <2804731+le1nux@users.noreply.github.com>
2025-11-13 16:02:24 +0100

    feat: wired up examples as part of the tests

--- tutorials/profiling/scripts/single_process/single_process_profiler_starter.sh
+++ tutorials/profiling/scripts/single_process/single_process_profiler_starter.sh
@@ -1,7 +1,27 @@
-#!/usr/bin/env bash
+#!/bin/sh
+set -e
 
 cd "$(dirname "$0")"
 
+remove_last_experiment_folder=false
 
-# Environment
+# Parse flags
+# -c : remove the last experiment folder after running the sweep as a cleanup step
+# this flag is useful when you just want to test the sweep (e.g., pytest) 
+# and don't want to keep the generated experiment folder
+while getopts "c" opt; do
+  case "$opt" in
+    c) remove_last_experiment_folder=true ;;
+    *) echo "Usage: $0 [-c]"; exit 1 ;;
+  esac
+done
+
 python  single_process_norm_profiling.py
+
+last_experiment_folder=$(realpath "$(ls -d ../../experiments/* | sort | tail -n 1)")
+
+
+if [ "$remove_last_experiment_folder" = true ]; then
+    echo "Cleaning up: removing last experiment folder: $last_experiment_folder"
+    rm -rf "$last_experiment_folder"
+fi
