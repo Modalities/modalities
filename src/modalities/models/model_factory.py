5f9f50e228584e6b355998ac03c4d33873a8b0ca
rrutmann <richardr@online.de>
2025-12-08 14:18:31 +0000

    fix: Initialize different weights across TP ranks

--- src/modalities/models/model_factory.py
+++ src/modalities/models/model_factory.py
@@ -578,6 +578,7 @@ class GPT2ModelFactory:
         use_meta_device: Optional[bool] = False,
         seed: Optional[int] = None,
         enforce_swiglu_hidden_dim_multiple_of: int = 256,
+        device_mesh: DeviceMesh | None = None,
     ) -> GPT2LLM:
         config = dict(
             sample_key=sample_key,
@@ -601,6 +602,7 @@ class GPT2ModelFactory:
             seed=seed,
             use_weight_tying=use_weight_tying,
             enforce_swiglu_hidden_dim_multiple_of=enforce_swiglu_hidden_dim_multiple_of,
+            device_mesh=device_mesh,
         )
         if use_meta_device and use_weight_tying:
             raise ValueError(
