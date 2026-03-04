c7bcaaa42b24d6e7596d1b52d289417090b299cb
Max Luebbering <2804731+le1nux@users.noreply.github.com>
2026-03-04 19:50:03 +0100

    refactor: we only init bias if set to true in config. added consistency check

--- src/modalities/models/gpt2/llama3_like_initialization.py
+++ src/modalities/models/gpt2/llama3_like_initialization.py
@@ -15,6 +15,7 @@ logger = get_logger(name="llama3 initialization")
 class Llama3InitializerConfig(BaseModel):
     num_layers: Annotated[int, Field(strict=True, gt=0)]
     n_embd: Annotated[int, Field(strict=True, gt=0)]
+    bias: bool
 
 
 class Llama3Initializer(ModelInitializationIF):
@@ -22,13 +23,12 @@ class Llama3Initializer(ModelInitializationIF):
     Follows weight initialization distributions and parameterization for Llama3 as described in TorchTitan.
     """
 
-    def __init__(self, num_layers: int, n_embd: int) -> None:
+    def __init__(self, num_layers: int, n_embd: int, bias: bool) -> None:
         super().__init__()
 
         self.regex_to_init = {
             # embedding weights
             r"transformer\.wte\.weight": partial(nn.init.normal_, mean=0.0, std=1),
-            r"transformer\.wpe\.weight": partial(nn.init.normal_, mean=0.0, std=1),
             # lm head weights
             r"transformer\.lm_head\.weight": partial(
                 nn.init.trunc_normal_,
@@ -45,22 +45,8 @@ class Llama3Initializer(ModelInitializationIF):
                 a=-2,
                 b=2,
             ),
-            r"transformer\.h\.\d+\.attn\.(q_attn|k_attn|v_attn)\.bias": partial(
-                nn.init.trunc_normal_,
-                mean=0.0,
-                std=0.02,
-                a=-2,
-                b=2,
-            ),
             # final attention projection in attention block
             r"transformer\.h\.\d+\.attn\.c_proj\.weight": partial(
-                nn.init.trunc_normal_,
-                mean=0.0,
-                std=0.02 / math.sqrt(2 * num_layers),
-                a=-2,
-                b=2,
-            ),
-            r"transformer\.h\.\d+\.attn\.c_proj\.bias": partial(
                 nn.init.trunc_normal_,
                 mean=0.0,
                 std=0.02 / math.sqrt(2 * num_layers),
@@ -75,7 +61,6 @@ class Llama3Initializer(ModelInitializationIF):
                 a=-2,
                 b=2,
             ),
-            r"transformer\.h\.\w+\.mlp\.(W)\.bias": nn.init.zeros_,
             r"transformer\.h\.\w+\.mlp\.(V|W_2)\.weight": partial(
                 nn.init.trunc_normal_,
                 mean=0.0,
@@ -83,14 +68,37 @@ class Llama3Initializer(ModelInitializationIF):
                 a=-2,
                 b=2,
             ),
-            r"transformer\.h\.\w+\.mlp\.(V|W_2)\.bias": nn.init.zeros_,
         }
+        if bias:
+            self.regex_to_init = {
+                **self.regex_to_init,
+                **{
+                    r"transformer\.h\.\d+\.attn\.(q_attn|k_attn|v_attn)\.bias": partial(
+                        nn.init.trunc_normal_,
+                        mean=0.0,
+                        std=0.02,
+                        a=-2,
+                        b=2,
+                    ),
+                    r"transformer\.h\.\d+\.attn\.c_proj\.bias": partial(
+                        nn.init.trunc_normal_,
+                        mean=0.0,
+                        std=0.02 / math.sqrt(2 * num_layers),
+                        a=-2,
+                        b=2,
+                    ),
+                    r"transformer\.h\.\w+\.mlp\.(W)\.bias": nn.init.zeros_,
+                    r"transformer\.h\.\w+\.mlp\.(V|W_2)\.bias": nn.init.zeros_,
+                },
+            }
 
     def initialize_in_place(self, model: nn.Module):
         self._init_by_fqn_regex(model, self.regex_to_init)
 
     @staticmethod
     def _init_by_fqn_regex(model: nn.Module, regex_to_init: dict[str, partial]):
+        hits = {k: 0 for k in regex_to_init.keys()}
+
         for parameter_name, p in model.named_parameters():
             match_count = 0
             for weight_regex in regex_to_init.keys():
@@ -98,9 +106,16 @@ class Llama3Initializer(ModelInitializationIF):
                     init_fn = regex_to_init[weight_regex]
                     init_fn(p)
                     match_count += 1
+                    hits[weight_regex] += 1
             if match_count == 0:
                 logger.warning(f"Parameter {parameter_name} did not match any regex for initialization")
             elif match_count > 1:
                 raise ValueError(
                     f"Parameter {parameter_name} matched multiple regexes for initialization, which is not allowed"
+                )
+
+        for k, count in hits.items():
+            if count == 0:
+                raise ValueError(
+                    f"Regex {k} did not match any FQNs. The model specification probably does not match LLama3."
                 )
